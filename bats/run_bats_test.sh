549c68ef539ec9626d1e90cd404c481618189dbe
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-02-15 22:44:00 +0100

    #9 - Main bash script ready for launching docker container and the bats tests

--- bats/run_bats_test.sh
+++ bats/run_bats_test.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 set -e
 
+function shutdownDockerContainer {
+    lastCommandStatus="$?"
+    echo "Shutting down docker container"
+    sudo docker stop test-postgres
+    exit $lastCommandStatus
+}
+trap shutdownDockerContainer EXIT SIGINT
+
 export DATABASE_PORT=5432
 
-sudo docker run -d --rm --name test-postgres postgres:9.6 -e POSTGRES_PASSWORD=postgres -d postgres
+sudo docker run --rm --name test-postgres -e POSTGRES_PASSWORD=postgres -d postgres:9.6
 
-psql -qtAX -U postgres -p $DATABASE_PORT -f ../db_scripts/prepare_postgresql-core_db.sql
+export PGPASSWORD=postgres
+psql -qtAX -U postgres -p $DATABASE_PORT --host=localhost -f "../db_scripts/prepare_postgresql-core_db.sql"
+
 
 #Run test
 bats -r .
+
+
+#
+# TIPS!
+# psql:
+# - To quit command line console (no-interactive mode) pass '\q' then press ENTER
+# - If docker container is still working, you can login to database by executing below commands:
+#   export PGPASSWORD=postgres
+#   psql -d postgres -U postgres -p 5432 --host=localhost
+#
