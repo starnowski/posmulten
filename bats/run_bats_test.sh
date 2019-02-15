a3f5f94894cc6cd896577251f99cf6f1ec7b1dd3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-02-16 00:06:00 +0100

    #9 - Fixed logical condition which checks if docker container database is started [skip travis]

--- bats/run_bats_test.sh
+++ bats/run_bats_test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
 
+function waitUntilDockerContainerIsReady {
+    checkCount=1
+    while : ; do
+        set +e
+        results=`psql -qtAX -U postgres -p $DATABASE_PORT --host="$DOCKER_DB_IP" -c "SELECT 1;"`
+        [[ "$?" -ne 0 && checkCount -ne 50 ]] || break
+        checkCount=$(( checkCount+1 ))
+        echo "Waiting $checkCount seconds for database to start"
+        sleep 1
+    done
+    set -e
+}
+
 function shutdownDockerContainer {
     lastCommandStatus="$?"
     echo "Shutting down docker container"
     sudo docker rm -f -v test-postgres
-    #sudo docker stop test-postgres
     exit $lastCommandStatus
 }
 trap shutdownDockerContainer EXIT SIGINT
@@ -15,9 +27,8 @@ export DATABASE_PORT=15432
 sudo docker run --rm --name test-postgres -e POSTGRES_PASSWORD=postgres_posmulten -p 127.0.0.1:15432:5432/tcp -d postgres:9.6.12
 DOCKER_DB_IP="127.0.0.1"
 
-#TODO make better condition until docker container will rise
-sleep 30
 export PGPASSWORD=postgres_posmulten
+waitUntilDockerContainerIsReady
 psql -qtAX -U postgres -p $DATABASE_PORT --host="$DOCKER_DB_IP" -f "../db_scripts/prepare_postgresql-core_db.sql"
 
 
