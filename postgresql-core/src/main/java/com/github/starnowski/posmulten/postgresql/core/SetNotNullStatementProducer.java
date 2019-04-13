ad96b3b83e9fff38a8fac89f9e160c435a40a4bd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-13 16:29:58 +0200

    #18 - Added unit tests. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
@@ -1,4 +1,8 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
 public class SetNotNullStatementProducer {
+    public String produce(String table, String column) {
+        //TODO
+        return null;
+    }
 }
--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
@@ -1,6 +1,22 @@
 package com.github.starnowski.posmulten.postgresql.core
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SetNotNullStatementProducerTest extends Specification {
+
+    def tested = new SetNotNullStatementProducer()
+
+    @Unroll
+    def "should return statement '#expectedStatement' for table '#table' and column '#column'" () {
+        expect:
+            tested.produce(table, column) == expectedStatement
+
+        where:
+            table       |   column      ||  expectedStatement
+            "users"     |   "tenant_id" ||  "ALTER TABLE users ALTER COLUMN tenant_id SET NOT NULL;"
+            "groups"    |   "tenant_id" ||  "ALTER TABLE groups ALTER COLUMN tenant_id SET NOT NULL;"
+            "users"     |   "col1" ||  "ALTER TABLE users ALTER COLUMN col1 SET NOT NULL;"
+            "groups"    |   "col1" ||  "ALTER TABLE groups ALTER COLUMN col1 SET NOT NULL;"
+    }
 }
