2d2130ccec992243d8b8c7e2c8bbb87bb2e6a6c1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-14 23:45:18 +0200

    #18 - Code styling. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
@@ -33,44 +33,44 @@ class SetNotNullStatementProducerTest extends Specification {
             ex.message == "Table name cannot be null"
 
         where:
-        column << ["xxx", "col1", "test"]
+            column << ["xxx", "col1", "test"]
     }
 
     @Unroll
     def "should throw exception of type 'IllegalArgumentException' when table name is blank, no matter if column name is correct \"#column\""()
     {
         when:
-        tested.produce(table, column)
+            tested.produce(table, column)
 
         then:
-        def ex = thrown(IllegalArgumentException.class)
+            def ex = thrown(IllegalArgumentException.class)
 
         and: "exception should have correct message"
-        ex.message == "Table name cannot be blank"
+            ex.message == "Table name cannot be blank"
 
         where:
-        table           | column
-        ""              | "col1"
-        " "             | "col1"
-        "       "       | "col1"
-        ""              | "XXXXXX"
-        " "             | "XXXXXX"
-        "       "       | "XXXXXX"
+            table           | column
+            ""              | "col1"
+            " "             | "col1"
+            "       "       | "col1"
+            ""              | "XXXXXX"
+            " "             | "XXXXXX"
+            "       "       | "XXXXXX"
     }
 
     @Unroll
     def "should throw exception of type 'IllegalArgumentException' when column name is null, no matter if table name is correct \"#table\""()
     {
         when:
-        tested.produce(table, null)
+            tested.produce(table, null)
 
         then:
-        def ex = thrown(IllegalArgumentException.class)
+            def ex = thrown(IllegalArgumentException.class)
 
         and: "exception should have correct message"
-        ex.message == "Column name cannot be null"
+            ex.message == "Column name cannot be null"
 
         where:
-        table << ["xxx", "col1", "test"]
+            table << ["xxx", "col1", "test"]
     }
 }
