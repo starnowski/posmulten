730afd5d5c841c6c4b6f4a8bbfba8f55302912fa
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-14 23:43:40 +0200

    #18 - Added implementation for test case which checks if table name is not blank. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
@@ -5,6 +5,9 @@ public class SetNotNullStatementProducer {
         if (table == null) {
             throw new IllegalArgumentException("Table name cannot be null");
         }
+        if (table.trim().isEmpty()) {
+            throw new IllegalArgumentException("Table name cannot be blank");
+        }
         if (column == null) {
             throw new IllegalArgumentException("Column name cannot be null");
         }
