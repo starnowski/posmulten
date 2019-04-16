c376507aee6d776ccfac56b0ff03aae630bba0e6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-17 00:48:25 +0200

    #22 - Added SetDefaultStatementProducer type. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetDefaultStatementProducer.java
@@ -0,0 +1,8 @@
+package com.github.starnowski.posmulten.postgresql.core;
+
+public class SetDefaultStatementProducer {
+
+    public String produce(String table, String column, String defaultValueDefinition) {
+        return null;
+    }
+}
