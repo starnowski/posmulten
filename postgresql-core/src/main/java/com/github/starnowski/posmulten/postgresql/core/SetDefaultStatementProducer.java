340393276c5faf5ce6ab055cd84dc288d41a244d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-21 10:58:12 +0200

    #22 - Added handling of invalid parameters.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetDefaultStatementProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetDefaultStatementProducer.java
@@ -3,6 +3,24 @@ package com.github.starnowski.posmulten.postgresql.core;
 public class SetDefaultStatementProducer {
 
     public String produce(String table, String column, String defaultValueDefinition) {
+        if (table == null) {
+            throw new IllegalArgumentException("Table name cannot be null");
+        }
+        if (table.trim().isEmpty()) {
+            throw new IllegalArgumentException("Table name cannot be blank");
+        }
+        if (column == null) {
+            throw new IllegalArgumentException("Column name cannot be null");
+        }
+        if (column.trim().isEmpty()) {
+            throw new IllegalArgumentException("Column name cannot be blank");
+        }
+        if (defaultValueDefinition == null) {
+            throw new IllegalArgumentException("Statement for default value cannot be null");
+        }
+        if (defaultValueDefinition.trim().isEmpty()) {
+            throw new IllegalArgumentException("Statement for default value cannot be blank");
+        }
         return "ALTER TABLE " + table + " ALTER COLUMN " + column + " SET DEFAULT " + defaultValueDefinition + ";";
     }
 }
