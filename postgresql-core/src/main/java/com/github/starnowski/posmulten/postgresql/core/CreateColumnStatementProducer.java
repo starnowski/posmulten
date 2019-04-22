921e54e25754987815cda2ca7b03336882fd7f75
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-22 09:11:53 +0200

    #30 - Added the class file (without correct implementation) [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/CreateColumnStatementProducer.java
@@ -0,0 +1,9 @@
+package com.github.starnowski.posmulten.postgresql.core;
+
+public class CreateColumnStatementProducer {
+
+        public String produce(String table, String column, String columnType){
+            //TODO
+            return null;
+        }
+}
