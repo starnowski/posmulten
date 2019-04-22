22e4cded2e0e9431aab2d9c7a2ee52e9978a1847
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-04-22 10:47:57 +0200

    #30 - Added basic implementation [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/CreateColumnStatementProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/CreateColumnStatementProducer.java
@@ -3,7 +3,6 @@ package com.github.starnowski.posmulten.postgresql.core;
 public class CreateColumnStatementProducer {
 
         public String produce(String table, String column, String columnType){
-            //TODO
-            return null;
+            return "ALTER TABLE " + table + " ADD COLUMN " + column + " " + columnType + ";";
         }
 }
