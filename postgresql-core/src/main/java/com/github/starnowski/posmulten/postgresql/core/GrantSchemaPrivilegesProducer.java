75058775712a9c61f1175929d0fd762e89aaa44f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-06-20 09:44:30 +0200

    #32 - Added implementation for main functionality.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSchemaPrivilegesProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSchemaPrivilegesProducer.java
@@ -1,10 +1,25 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class GrantSchemaPrivilegesProducer {
     public String produce(String schema, String policyTargetUsername, List<String> privileges) {
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("GRANT ");
+        Iterator<String> it = privileges.iterator();
+        while (it.hasNext()){
+            sb.append(it.next());
+            if (it.hasNext())
+            {
+                sb.append(", ");
+            }
+        }
+        sb.append(" ON SCHEMA ");
+        sb.append(schema);
+        sb.append(" TO '");
+        sb.append(policyTargetUsername);
+        sb.append("';");
+        return sb.toString();
     }
 }
