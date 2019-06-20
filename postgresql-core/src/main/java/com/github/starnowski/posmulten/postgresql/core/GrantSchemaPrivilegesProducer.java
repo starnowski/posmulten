3af24cf9f99fd5c5f91565df7a5ede016b3f974c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-06-20 09:38:11 +0200

    #32 - Added unit test scenario.

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSchemaPrivilegesProducer.java
@@ -0,0 +1,10 @@
+package com.github.starnowski.posmulten.postgresql.core;
+
+import java.util.List;
+
+public class GrantSchemaPrivilegesProducer {
+    public String produce(String schema, String policyTargetUsername, List<String> privileges) {
+
+        return null;
+    }
+}
