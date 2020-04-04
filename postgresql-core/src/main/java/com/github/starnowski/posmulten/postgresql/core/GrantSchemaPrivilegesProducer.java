8a161b0eacf147ecd89197e318f22f067b9b8294
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-04 20:34:44 +0200

    #32 - Throwing the IllegalArgumentException exception for the null schema.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSchemaPrivilegesProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSchemaPrivilegesProducer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class GrantSchemaPrivilegesProducer {
     public String produce(String schema, String policyTargetUsername, List<String> privileges) {
+        validateParameters(schema, policyTargetUsername, privileges);
         StringBuilder sb = new StringBuilder();
         sb.append("GRANT ");
         Iterator<String> it = privileges.iterator();
@@ -21,5 +22,13 @@ public class GrantSchemaPrivilegesProducer {
         sb.append(policyTargetUsername);
         sb.append("\";");
         return sb.toString();
+    }
+
+    private void validateParameters(String schema, String policyTargetUsername, List<String> privileges)
+    {
+        if (schema == null)
+        {
+            throw new IllegalArgumentException("Schema name cannot be null");
+        }
     }
 }
