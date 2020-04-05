95a6f355af0a3ef92d7ee6e0008516a9a4365e76
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-05 16:58:49 +0200

    #33 - Added method parameters. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantTablePrivilegesProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantTablePrivilegesProducer.java
@@ -1,5 +1,11 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
+import java.util.List;
+
 public class GrantTablePrivilegesProducer {
 
+    public String produce(String schema, String table, String user, List<String> privileges) {
+        //TODO
+        return null;
+    }
 }
