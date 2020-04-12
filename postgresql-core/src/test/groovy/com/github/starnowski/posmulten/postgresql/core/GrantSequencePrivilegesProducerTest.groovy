282481f1d19823e5da75fc4ff23073be37fa3c13
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-12 14:58:55 +0200

    #42 - Added basic test case. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/GrantSequencePrivilegesProducerTest.groovy
@@ -0,0 +1,23 @@
+package com.github.starnowski.posmulten.postgresql.core
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class GrantSequencePrivilegesProducerTest extends Specification {
+
+    def tested = new GrantSequencePrivilegesProducer()
+
+    @Unroll
+    def "should return granting access statement '#expectedStatement' for sequence #sequence, user '#user', schema '#schema' with 'ALL PRIVILGES'" () {
+        expect:
+            tested.produce(schema, sequence, user) == expectedStatement
+
+        where:
+            schema                  | user          | sequence      ||	expectedStatement
+            null                    | "user1"       | "primary"     || "GRANT ALL PRIVILEGES ON public.\"primary\" TO \"user1\";"
+            "public"                | "user1"       | "primary"     || "GRANT ALL PRIVILEGES ON public.\"primary\" TO \"user1\";"
+            "non_public_schema"     | "johndoe"     | "primary"     || "GRANT ALL PRIVILEGES ON non_public_schema.\"primary\" TO \"johndoe\";"
+            "non_public_schema"     | "johndoe"     | "secondary"   || "GRANT ALL PRIVILEGES ON non_public_schema.\"secondary\" TO \"johndoe\";"
+    }
+
+}
