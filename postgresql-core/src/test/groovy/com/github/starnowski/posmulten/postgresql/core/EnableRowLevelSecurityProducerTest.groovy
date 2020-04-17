07b87a1d1af33845eb2171e47caeec4a4e8f0d74
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-17 20:43:36 +0200

    #35 - Added basic test case. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/EnableRowLevelSecurityProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/EnableRowLevelSecurityProducerTest.groovy
@@ -1,6 +1,23 @@
 package com.github.starnowski.posmulten.postgresql.core
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class EnableRowLevelSecurityProducerTest extends Specification {
+
+    def tested = new EnableRowLevelSecurityProducer()
+
+    @Unroll
+    def "should return statement (#expectedStatement) that enables row level security mode for table (#table) and schema (#schema)"()
+    {
+        expect:
+            tested.produce(schema, table) == expectedStatement
+        where:
+            schema      | table     ||	expectedStatement
+            null        | "users"   ||  "ALTER TABLE \"users\" ENABLE ROW LEVEL SECURITY;"
+            null        | "posts"   ||  "ALTER TABLE \"posts\" ENABLE ROW LEVEL SECURITY;"
+            "secondary" | "users"   ||  "ALTER TABLE secondary.\"users\" ENABLE ROW LEVEL SECURITY;"
+            "secondary" | "posts"   ||  "ALTER TABLE secondary.\"posts\" ENABLE ROW LEVEL SECURITY;"
+    }
+
 }
