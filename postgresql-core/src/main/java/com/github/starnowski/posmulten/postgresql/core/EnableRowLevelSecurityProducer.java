da3f7ac7de27eef2c41f054008a825ef53a33eee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-17 20:48:43 +0200

    #35 - Added basic implementation.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/EnableRowLevelSecurityProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/EnableRowLevelSecurityProducer.java
@@ -1,4 +1,20 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
 public class EnableRowLevelSecurityProducer {
+
+    public String produce(String table, String schema)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ALTER TABLE ");
+        if (schema != null)
+        {
+            sb.append(schema);
+            sb.append(".");
+        }
+        sb.append("\"");
+        sb.append(table);
+        sb.append("\"");
+        sb.append(" ENABLE ROW LEVEL SECURITY;");
+        return sb.toString();
+    }
 }
