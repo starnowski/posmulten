57ab458daeec1731d26408b62607f99c7ce4cf37
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-18 11:56:29 +0200

    #36 - Added basic implementation.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducer.java
@@ -1,4 +1,20 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
 public class ForceRowLevelSecurityProducer {
+
+    public String produce(String table, String schema)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ALTER TABLE ");
+        if (schema != null)
+        {
+            sb.append(schema);
+            sb.append(".");
+        }
+        sb.append("\"");
+        sb.append(table);
+        sb.append("\"");
+        sb.append(" FORCE ROW LEVEL SECURITY;");
+        return sb.toString();
+    }
 }
