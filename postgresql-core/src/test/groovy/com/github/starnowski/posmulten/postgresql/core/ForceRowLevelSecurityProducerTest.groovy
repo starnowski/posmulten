04d7c5f53c83e2f64c4480574c76dc85f58797fe
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-18 11:55:29 +0200

    #36 - Added basic test case. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducerTest.groovy
@@ -1,6 +1,24 @@
 package com.github.starnowski.posmulten.postgresql.core
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ForceRowLevelSecurityProducerTest extends Specification {
+
+    def tested = new ForceRowLevelSecurityProducer()
+
+    @Unroll
+    def "should return statement (#expectedStatement) that forces row level security mode for table (#table) and schema (#schema)"()
+    {
+        expect:
+            tested.produce(table, schema) == expectedStatement
+
+        where:
+            schema      | table     ||	expectedStatement
+            null        | "users"   ||  "ALTER TABLE \"users\" FORCE ROW LEVEL SECURITY;"
+            null        | "posts"   ||  "ALTER TABLE \"posts\" FORCE ROW LEVEL SECURITY;"
+            "secondary" | "users"   ||  "ALTER TABLE secondary.\"users\" FORCE ROW LEVEL SECURITY;"
+            "secondary" | "posts"   ||  "ALTER TABLE secondary.\"posts\" FORCE ROW LEVEL SECURITY;"
+            "public"    | "posts"   ||  "ALTER TABLE public.\"posts\" FORCE ROW LEVEL SECURITY;"
+    }
 }
