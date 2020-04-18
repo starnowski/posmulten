459ccda016e02ebc7e0a00f90f87f64f99fd7282
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-18 12:11:26 +0200

    #36 - Throwing correct exceptions.

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducer.java
@@ -4,6 +4,7 @@ public class ForceRowLevelSecurityProducer {
 
     public String produce(String table, String schema)
     {
+        validateParameters(table, schema);
         StringBuilder sb = new StringBuilder();
         sb.append("ALTER TABLE ");
         if (schema != null)
@@ -16,5 +17,20 @@ public class ForceRowLevelSecurityProducer {
         sb.append("\"");
         sb.append(" FORCE ROW LEVEL SECURITY;");
         return sb.toString();
+    }
+
+    private void validateParameters(String table, String schema) {
+        if (schema != null && schema.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Schema name cannot be blank");
+        }
+        if (table == null)
+        {
+            throw new IllegalArgumentException("Table name cannot be null");
+        }
+        if (table.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Table name cannot be blank");
+        }
     }
 }
