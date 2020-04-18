8d059bd41d49a912eb5b826881a6e0354357bdf7
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-04-18 12:10:12 +0200

    #36 - Added tests that check if correct exceptions are thrown. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForceRowLevelSecurityProducerTest.groovy
@@ -21,4 +21,60 @@ class ForceRowLevelSecurityProducerTest extends Specification {
             "secondary" | "posts"   ||  "ALTER TABLE secondary.\"posts\" FORCE ROW LEVEL SECURITY;"
             "public"    | "posts"   ||  "ALTER TABLE public.\"posts\" FORCE ROW LEVEL SECURITY;"
     }
+
+    @Unroll
+    def "should throw exception of type 'IllegalArgumentException' when schema name is blank '#schema', no matter if table \"#table\" is correct"()
+    {
+        when:
+            tested.produce(table, schema)
+
+        then:
+            def ex = thrown(IllegalArgumentException.class)
+
+        and: "exception should have correct message"
+            ex.message == "Schema name cannot be blank"
+
+        where:
+            table               |   schema
+            "users"             |   ""
+            "users"             |   "   "
+            "posts"             |   ""
+            "posts"             |   "   "
+    }
+
+    @Unroll
+    def "should throw exception of type 'IllegalArgumentException' when table name is blank '#table', no matter if schema \"#schema\" is correct"()
+    {
+        when:
+            tested.produce(table, schema)
+
+        then:
+            def ex = thrown(IllegalArgumentException.class)
+
+        and: "exception should have correct message"
+            ex.message == "Table name cannot be blank"
+
+        where:
+            schema          |   table
+            "public"        |   ""
+            "public"        |   "   "
+            "secondary"     |   ""
+            "secondary"     |   "   "
+    }
+
+    @Unroll
+    def "should throw exception of type 'IllegalArgumentException' when table name is null '#table', no matter if schema \"#schema\" is correct"()
+    {
+        when:
+            tested.produce(null, schema)
+
+        then:
+            def ex = thrown(IllegalArgumentException.class)
+
+        and: "exception should have correct message"
+            ex.message == "Table name cannot be null"
+
+        where:
+            schema  << ["public", "secondary"]
+    }
 }
