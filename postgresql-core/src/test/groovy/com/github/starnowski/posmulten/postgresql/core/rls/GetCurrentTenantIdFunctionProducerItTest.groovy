942594e603290efa35738a1ff5acb8bedc33efc3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-05 13:48:49 +0200

    #49 - Added integration tests class file. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducerItTest.groovy
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.postgresql.core.rls
+
+import com.github.starnowski.posmulten.postgresql.core.TestApplication
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import spock.lang.Specification
+
+@SpringBootTest(classes = [TestApplication.class])
+class GetCurrentTenantIdFunctionProducerItTest extends Specification {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate
+
+    def tested = new GetCurrentTenantIdFunctionProducer()
+}
