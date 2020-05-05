aae79b09cc12385b95a6364000da5586fa626777
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-05 20:57:45 +0200

    #49 - Added test case that checks if a correct exception is being thrown when the passed function name is null. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducerTest.groovy
@@ -25,4 +25,29 @@ class GetCurrentTenantIdFunctionProducerTest extends Specification {
             "public"                |   "give_me_tenant"            |   "pos.tenant"                    |   "VARCHAR(32)"       ||  "CREATE OR REPLACE FUNCTION public.give_me_tenant() RETURNS VARCHAR(32) as \$\$\nSELECT current_setting('pos.tenant')\n\$\$ LANGUAGE sql\nSTABLE PARALLEL SAFE;"
             "non_public_schema"     |   "return_current_tenant"     |   "t.id"                          |   "text"              ||  "CREATE OR REPLACE FUNCTION non_public_schema.return_current_tenant() RETURNS text as \$\$\nSELECT current_setting('t.id')\n\$\$ LANGUAGE sql\nSTABLE PARALLEL SAFE;"
     }
+
+    @Unroll
+    def "should throw exception of type 'IllegalArgumentException' when function name is null, even if the rest of parameters are correct, schema #schema, tenant id property #currentTenantIdProperty, return type #returnType"()
+    {
+        when:
+        tested.produce(new GetCurrentTenantIdFunctionProducerParameters(null, testCurrentTenantIdProperty, testSchema, testReturnType))
+
+        then:
+            def ex = thrown(IllegalArgumentException.class)
+
+        and: "exception should have correct message"
+            ex.message == "Function name cannot be null"
+
+        where:
+            testSchema              |   testCurrentTenantIdProperty     |   testReturnType
+            null                    |   "c.c_ten"                       |   null
+            "public"                |   "c.c_ten"                       |   null
+            "non_public_schema"     |   "c.c_ten"                       |   null
+            null                    |   "c.c_ten"                       |   "text"
+            "public"                |   "c.c_ten"                       |   "text"
+            "non_public_schema"     |   "c.c_ten"                       |   "text"
+            null                    |   "con.tenant_id"                 |   "VARCHAR(128)"
+            "public"                |   "pos.tenant"                    |   "VARCHAR(32)"
+            "non_public_schema"     |   "t.id"                          |   "text"
+    }
 }
