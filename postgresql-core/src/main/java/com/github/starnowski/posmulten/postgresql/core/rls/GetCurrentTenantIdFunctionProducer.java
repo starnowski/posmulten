c0a7090bc056bbd89ffec09ae50247fd7ca432fd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-05 14:43:02 +0200

    #49 - Added comment. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducer.java
@@ -5,6 +5,24 @@ public class GetCurrentTenantIdFunctionProducer {
     public String produce(IGetCurrentTenantIdFunctionProducerParameters parameters)
     {
         //TODO
+        /*
+        CREATE OR REPLACE FUNCTION pg_catalog.current_setting(
+	text,
+	boolean)
+    RETURNS text
+    LANGUAGE 'internal'
+
+    COST 1
+    STABLE STRICT PARALLEL SAFE
+    ROWS 0
+AS $BODY$
+show_config_by_name_missing_ok
+$BODY$;
+         */
+        /*
+current_setting('poc.current_tenant')
+         */
+
         return null;
     }
 }
