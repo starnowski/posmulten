1548e3d3129d1832cd349a098f02a5acf1e45e13
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-06 12:50:25 +0200

    #54 - Code refactoring, removing redundant tests cases. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/GetCurrentTenantIdFunctionProducerTest.groovy
@@ -25,68 +25,6 @@ class GetCurrentTenantIdFunctionProducerTest extends AbstractFunctionFactoryTest
             "non_public_schema"     |   "return_current_tenant"     |   "t.id"                          |   "text"              ||  "CREATE OR REPLACE FUNCTION non_public_schema.return_current_tenant() RETURNS text as \$\$\nSELECT current_setting('t.id')\n\$\$ LANGUAGE sql\nSTABLE PARALLEL SAFE;"
     }
 
-    def "should throw exception of type 'IllegalArgumentException' when parameters object is null" ()
-    {
-        when:
-            tested.produce(null)
-
-        then:
-            def ex = thrown(IllegalArgumentException.class)
-
-        and: "exception should have correct message"
-            ex.message == "The parameters object cannot be null"
-    }
-
-    @Unroll
-    def "should throw exception of type 'IllegalArgumentException' when function name is null, even if the rest of parameters are correct, schema #testSchema, tenant id property #testCurrentTenantIdProperty, return type #testReturnType"()
-    {
-        when:
-            tested.produce(new GetCurrentTenantIdFunctionProducerParameters(null, testCurrentTenantIdProperty, testSchema, testReturnType))
-
-        then:
-            def ex = thrown(IllegalArgumentException.class)
-
-        and: "exception should have correct message"
-            ex.message == "Function name cannot be null"
-
-        where:
-            testSchema              |   testCurrentTenantIdProperty     |   testReturnType
-            null                    |   "c.c_ten"                       |   null
-            "public"                |   "c.c_ten"                       |   null
-            "non_public_schema"     |   "c.c_ten"                       |   null
-            null                    |   "c.c_ten"                       |   "text"
-            "public"                |   "c.c_ten"                       |   "text"
-            "non_public_schema"     |   "c.c_ten"                       |   "text"
-            null                    |   "con.tenant_id"                 |   "VARCHAR(128)"
-            "public"                |   "pos.tenant"                    |   "VARCHAR(32)"
-            "non_public_schema"     |   "t.id"                          |   "text"
-    }
-
-    @Unroll
-    def "should throw exception of type 'IllegalArgumentException' when function name is blank ('#testFunctionName'), even if the rest of parameters are correct, schema #testSchema, tenant id property #testCurrentTenantIdProperty, return type #testReturnType"()
-    {
-        when:
-            tested.produce(new GetCurrentTenantIdFunctionProducerParameters(testFunctionName, testCurrentTenantIdProperty, testSchema, testReturnType))
-
-        then:
-            def ex = thrown(IllegalArgumentException.class)
-
-        and: "exception should have correct message"
-            ex.message == "Function name cannot be blank"
-
-        where:
-            testSchema              |   testCurrentTenantIdProperty     |   testReturnType  |   testFunctionName
-            null                    |   "c.c_ten"                       |   null            |   ""
-            "public"                |   "c.c_ten"                       |   null            |   "      "
-            "non_public_schema"     |   "c.c_ten"                       |   null            |   "  "
-            null                    |   "c.c_ten"                       |   "text"          |   "            "
-            "public"                |   "c.c_ten"                       |   "text"          |   " "
-            "non_public_schema"     |   "c.c_ten"                       |   "text"          |   ""
-            null                    |   "con.tenant_id"                 |   "VARCHAR(128)"  |   "      "
-            "public"                |   "pos.tenant"                    |   "VARCHAR(32)"   |   " "
-            "non_public_schema"     |   "t.id"                          |   "text"          |   "     "
-    }
-
     @Unroll
     def "should throw exception of type 'IllegalArgumentException' when tenant id property name is null, even if the rest of parameters are correct, function name #functionName, schema #testSchema, return type #testReturnType"()
     {
@@ -135,31 +73,6 @@ class GetCurrentTenantIdFunctionProducerTest extends AbstractFunctionFactoryTest
             null                    |   "get_current_tenant"        |   "VARCHAR(128)"  | " "
             "public"                |   "get_current_tenant"        |   "VARCHAR(32)"   | ""
             "non_public_schema"     |   "get_current_tenant"        |   "text"          | "          "
-    }
-
-    @Unroll
-    def "should throw exception of type 'IllegalArgumentException' when schema is blank ('#testSchema'), even if the rest of parameters are correct, function name #functionName, tenant id property name #testCurrentTenantIdProperty, return type #testReturnType"()
-    {
-        when:
-            tested.produce(new GetCurrentTenantIdFunctionProducerParameters(functionName, testCurrentTenantIdProperty, testSchema, testReturnType))
-
-        then:
-            def ex = thrown(IllegalArgumentException.class)
-
-        and: "exception should have correct message"
-            ex.message == "Schema name cannot be blank"
-
-        where:
-            testCurrentTenantIdProperty     |   functionName                |   testReturnType  | testSchema
-            "c.c_ten"                       |   "return_current_tenant"     |   null            | ""
-            "pos.tenant"                    |   "return_current_tenant"     |   null            | "     "
-            "t.id"                          |   "return_current_tenant"     |   null            | " "
-            "c.c_ten"                       |   "return_current_tenant"     |   "text"          | "         "
-            "pos.tenant"                    |   "return_current_tenant"     |   "text"          | " "
-            "t.id"                          |   "return_current_tenant"     |   "text"          | "             "
-            "c.c_ten"                       |   "get_current_tenant"        |   "VARCHAR(128)"  | " "
-            "pos.tenant"                    |   "get_current_tenant"        |   "VARCHAR(32)"   | ""
-            "t.id"                          |   "get_current_tenant"        |   "text"          | "          "
     }
 
     @Unroll
