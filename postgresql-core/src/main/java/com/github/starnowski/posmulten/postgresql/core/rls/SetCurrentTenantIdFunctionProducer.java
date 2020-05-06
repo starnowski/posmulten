20a95e78ffda32c2e7fabdcae21ac3b92db898e9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-06 18:42:25 +0200

    #54 - Added statement builder. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/SetCurrentTenantIdFunctionProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/SetCurrentTenantIdFunctionProducer.java
@@ -3,6 +3,36 @@ package com.github.starnowski.posmulten.postgresql.core.rls;
 public class SetCurrentTenantIdFunctionProducer extends AbstractFunctionFactory<ISetCurrentTenantIdFunctionProducerParameters>{
     @Override
     protected String produceStatement(ISetCurrentTenantIdFunctionProducerParameters parameters) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE OR REPLACE FUNCTION ");
+        if (parameters.getSchema() != null)
+        {
+            sb.append(parameters.getSchema());
+            sb.append(".");
+        }
+        sb.append(parameters.getFunctionName());
+        sb.append("(");
+        if (parameters.getArgumentType() == null)
+        {
+            sb.append("text");
+        }
+        else
+        {
+            sb.append(parameters.getArgumentType());
+        }
+        sb.append(")");
+        sb.append(" RETURNS ");
+        sb.append("void");
+        sb.append(" as $$");
+        sb.append("\n");
+        sb.append("SELECT set_config('");
+        sb.append(parameters.getCurrentTenantIdProperty());
+        sb.append("', $1, false)");
+        sb.append("\n");
+        sb.append("$$ LANGUAGE sql");
+        sb.append("\n");
+        sb.append("VOLATILE");
+        sb.append(";");
+        return sb.toString();
     }
 }
