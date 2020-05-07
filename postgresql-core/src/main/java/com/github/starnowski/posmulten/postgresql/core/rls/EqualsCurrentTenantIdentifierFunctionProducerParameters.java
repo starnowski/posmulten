f89c321c3854fd5add04356fce4c40734b4d804c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-07 20:30:32 +0200

    #55 - Added parameters object. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/EqualsCurrentTenantIdentifierFunctionProducerParameters.java
@@ -0,0 +1,37 @@
+package com.github.starnowski.posmulten.postgresql.core.rls;
+
+public class EqualsCurrentTenantIdentifierFunctionProducerParameters implements IEqualsCurrentTenantIdentifierFunctionProducerParameters{
+
+    private final String functionName;
+    private final String schema;
+    private final String parameterType;
+    private final String returnCurrentTenantIdStatement;
+
+    public EqualsCurrentTenantIdentifierFunctionProducerParameters(String functionName, String schema, String parameterType, String returnCurrentTenantIdStatement) {
+        this.functionName = functionName;
+        this.schema = schema;
+        this.parameterType = parameterType;
+        this.returnCurrentTenantIdStatement = returnCurrentTenantIdStatement;
+    }
+
+    @Override
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    @Override
+    public String getSchema() {
+        return schema;
+    }
+
+    @Override
+    public String getParameterType() {
+        return parameterType;
+    }
+
+    @Override
+    public String getReturnCurrentTenantIdStatement() {
+        return returnCurrentTenantIdStatement;
+    }
+
+}
