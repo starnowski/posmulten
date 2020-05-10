287ff52281487d1cd6ac454aa7b0b577199fa7ee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-10 21:16:00 +0200

    #56 - Added the FunctionArgumentValue type. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/common/function/FunctionArgumentValue.java
@@ -0,0 +1,47 @@
+package com.github.starnowski.posmulten.postgresql.core.common.function;
+
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueEnum.REFERENCE;
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueEnum.STRING;
+
+public interface FunctionArgumentValue {
+
+    String getValue();
+
+    default FunctionArgumentValueEnum getType()
+    {
+        return STRING;
+    }
+
+    public static FunctionArgumentValue forString(String value)
+    {
+        return new DefaultFunctionArgumentValue(value, STRING);
+    }
+
+    public static FunctionArgumentValue forReference(String value)
+    {
+        return new DefaultFunctionArgumentValue(value, REFERENCE);
+    }
+
+    public static class DefaultFunctionArgumentValue implements FunctionArgumentValue {
+
+        private final String value;
+        private final FunctionArgumentValueEnum type;
+
+        public DefaultFunctionArgumentValue(String value, FunctionArgumentValueEnum type) {
+            this.value = value;
+            this.type = type;
+        }
+
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public FunctionArgumentValueEnum getType() {
+            return type;
+        }
+
+    }
+}
