6d184dc0a334eef6abda77d12ca544a18de89c12
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-11 09:20:39 +0200

    #56 - Added implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/EqualsCurrentTenantIdentifierFunctionDefinition.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/EqualsCurrentTenantIdentifierFunctionDefinition.java
@@ -2,6 +2,7 @@ package com.github.starnowski.posmulten.postgresql.core.rls.function;
 
 import com.github.starnowski.posmulten.postgresql.core.common.function.DefaultFunctionDefinition;
 import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue;
+import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueEnum;
 import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
 
 public class EqualsCurrentTenantIdentifierFunctionDefinition extends DefaultFunctionDefinition implements EqualsCurrentTenantIdentifierFunctionInvocationFactory{
@@ -12,6 +13,19 @@ public class EqualsCurrentTenantIdentifierFunctionDefinition extends DefaultFunc
 
     @Override
     public String returnEqualsCurrentTenantIdentifierFunctionInvocation(FunctionArgumentValue argumentValue) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(getFunctionReference());
+        sb.append("(");
+        if (FunctionArgumentValueEnum.STRING.equals(argumentValue.getType()))
+        {
+            sb.append("'");
+        }
+        sb.append(argumentValue.getValue());
+        if (FunctionArgumentValueEnum.STRING.equals(argumentValue.getType()))
+        {
+            sb.append("'");
+        }
+        sb.append(")");
+        return sb.toString();
     }
 }
