5592bc7a6678accc2deecbedc2d847c26869cd94
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-11 16:14:29 +0200

    #56 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/GetCurrentTenantIdFunctionProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/GetCurrentTenantIdFunctionProducer.java
@@ -1,7 +1,6 @@
 package com.github.starnowski.posmulten.postgresql.core.rls.function;
 
-import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactory;
-import com.github.starnowski.posmulten.postgresql.core.common.function.DefaultFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.function.ExtendedAbstractFunctionFactory;
 import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
 
 /**
@@ -10,7 +9,7 @@ import com.github.starnowski.posmulten.postgresql.core.common.function.IFunction
  * @see <a href="https://www.postgresql.org/docs/9.6/sql-createfunction.html">Postgres, create function</a>
  *
  */
-public class GetCurrentTenantIdFunctionProducer extends AbstractFunctionFactory<IGetCurrentTenantIdFunctionProducerParameters, GetCurrentTenantIdFunctionDefinition> {
+public class GetCurrentTenantIdFunctionProducer extends ExtendedAbstractFunctionFactory<IGetCurrentTenantIdFunctionProducerParameters, GetCurrentTenantIdFunctionDefinition> {
 
     protected void validate(IGetCurrentTenantIdFunctionProducerParameters parameters) {
         super.validate(parameters);
@@ -33,26 +32,47 @@ public class GetCurrentTenantIdFunctionProducer extends AbstractFunctionFactory<
         return new GetCurrentTenantIdFunctionDefinition(functionDefinition);
     }
 
+//    @Override
+//    protected String produceStatement(IGetCurrentTenantIdFunctionProducerParameters parameters) {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("CREATE OR REPLACE FUNCTION ");
+//        if (parameters.getSchema() != null)
+//        {
+//            sb.append(parameters.getSchema());
+//            sb.append(".");
+//        }
+//        sb.append(parameters.getFunctionName());
+//        sb.append("()");
+//        sb.append(" RETURNS ");
+//        if (parameters.getFunctionReturnType() == null)
+//        {
+//            sb.append("VARCHAR(255)");
+//        }
+//        else
+//        {
+//            sb.append(parameters.getFunctionReturnType());
+//        }
+//        sb.append(" as $$");
+//        sb.append("\n");
+//        sb.append("SELECT current_setting('");
+//        sb.append(parameters.getCurrentTenantIdProperty());
+//        sb.append("')");
+//        sb.append("\n");
+//        sb.append("$$ LANGUAGE sql");
+//        sb.append("\n");
+//        sb.append("STABLE PARALLEL SAFE");
+//        sb.append(";");
+//        return sb.toString();
+//    }
+
     @Override
-    protected String produceStatement(IGetCurrentTenantIdFunctionProducerParameters parameters) {
+    protected String prepareReturnType(IGetCurrentTenantIdFunctionProducerParameters parameters) {
+        return parameters.getFunctionReturnType() == null ? "VARCHAR(255)" : parameters.getFunctionReturnType();
+    }
+
+    @Override
+    protected String buildBodyAndMetaData(IGetCurrentTenantIdFunctionProducerParameters parameters) {
         StringBuilder sb = new StringBuilder();
-        sb.append("CREATE OR REPLACE FUNCTION ");
-        if (parameters.getSchema() != null)
-        {
-            sb.append(parameters.getSchema());
-            sb.append(".");
-        }
-        sb.append(parameters.getFunctionName());
-        sb.append("()");
-        sb.append(" RETURNS ");
-        if (parameters.getFunctionReturnType() == null)
-        {
-            sb.append("VARCHAR(255)");
-        }
-        else
-        {
-            sb.append(parameters.getFunctionReturnType());
-        }
         sb.append(" as $$");
         sb.append("\n");
         sb.append("SELECT current_setting('");
@@ -62,7 +82,6 @@ public class GetCurrentTenantIdFunctionProducer extends AbstractFunctionFactory<
         sb.append("$$ LANGUAGE sql");
         sb.append("\n");
         sb.append("STABLE PARALLEL SAFE");
-        sb.append(";");
         return sb.toString();
     }
 }
