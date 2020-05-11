ca4b62656a5140395eb1c62ef1fbde81965c14f6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-11 16:11:08 +0200

    #56 - Code refactoring. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/common/function/ExtendedAbstractFunctionFactory.java
@@ -0,0 +1,48 @@
+package com.github.starnowski.posmulten.postgresql.core.common.function;
+
+import java.util.List;
+
+public abstract class ExtendedAbstractFunctionFactory<P extends IFunctionFactoryParameters, R extends DefaultFunctionDefinition> extends AbstractFunctionFactory<P, R>{
+
+    @Override
+    protected String produceStatement(P parameters) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(buildFunctionNameAndArgumentDeclaration(parameters));
+        sb.append("\n");
+        sb.append(buildBodyAndMetaData(parameters));
+        sb.append(";");
+        return sb.toString();
+    }
+
+    protected String buildFunctionNameAndArgumentDeclaration(P parameters)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE OR REPLACE FUNCTION ");
+        sb.append(returnFunctionReference(parameters));
+        sb.append(buildArgumentDeclaration(parameters));
+        sb.append(" ");
+        sb.append(buildReturnPhrase(parameters));
+        return sb.toString();
+    }
+
+    private String buildReturnPhrase(P parameters) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("RETURNS ");
+        sb.append(prepareReturnType(parameters));
+        return sb.toString();
+    }
+
+    protected abstract String prepareReturnType(P parameters);
+
+    protected String buildArgumentDeclaration(P parameters)
+    {
+        List<IFunctionArgument> arguments = prepareFunctionArguments(parameters);
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        sb.append(prepareArgumentsPhrase(arguments));
+        sb.append(")");
+        return sb.toString();
+    }
+
+    protected abstract String buildBodyAndMetaData(P parameters);
+}
