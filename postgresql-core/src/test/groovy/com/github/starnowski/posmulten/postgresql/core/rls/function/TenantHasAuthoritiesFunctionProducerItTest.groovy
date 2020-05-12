5e71f7b18a53f16b455c8a591009f55742a553a6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-12 14:16:02 +0200

    #56 - Added integration test (not yet finished). [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducerItTest.groovy
@@ -0,0 +1,50 @@
+package com.github.starnowski.posmulten.postgresql.core.rls.function
+
+import com.github.starnowski.posmulten.postgresql.core.TestApplication
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.github.starnowski.posmulten.postgresql.core.TestUtils.isFunctionExists
+import static com.github.starnowski.posmulten.postgresql.core.TestUtils.dropFunction
+import static org.junit.Assert.assertEquals
+
+@SpringBootTest(classes = [TestApplication.class])
+class TenantHasAuthoritiesFunctionProducerItTest extends Specification {
+
+    def tested = new TenantHasAuthoritiesFunctionProducer()
+
+    @Autowired
+    JdbcTemplate jdbcTemplate
+
+    def setup()
+    {
+        StringBuilder sb = new StringBuilder()
+        sb.append("CREATE OR REPLACE FUNCTION is_tenant_starts_with_abcd(text) RETURNS BOOLEAN AS \$\$")
+        sb.append("\n")
+        sb.append("SELECT \$1 LIKE 'ABCD%'")
+        sb.append("\n")
+        sb.append("\$\$ LANGUAGE sql;")
+        jdbcTemplate.execute(sb.toString())
+        assertEquals(true, isFunctionExists(jdbcTemplate, "is_tenant_starts_with_abcd", null))
+    }
+
+    @Unroll
+    def "should create sql statement" ()
+    {
+        given:
+            def result = true
+
+        expect:
+            result == true
+        //TODO
+    }
+
+    def cleanup()
+    {
+        dropFunction(jdbcTemplate, "is_tenant_starts_with_abcd", null, "text")
+        assertEquals(false, isFunctionExists(jdbcTemplate, "is_tenant_starts_with_abcd", null))
+    }
+}
