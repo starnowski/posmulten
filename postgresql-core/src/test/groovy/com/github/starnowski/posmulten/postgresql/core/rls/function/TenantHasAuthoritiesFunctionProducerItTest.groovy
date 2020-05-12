12f65df55385d5e816942be9ea3a2d792987b4fb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-12 14:56:25 +0200

    #56 - Code refactoring. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducerItTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducerItTest.groovy
@@ -4,12 +4,19 @@ import com.github.starnowski.posmulten.postgresql.core.TestApplication
 import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueEnum
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.DataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.StatementCallback
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+
 import static com.github.starnowski.posmulten.postgresql.core.TestUtils.isFunctionExists
 import static com.github.starnowski.posmulten.postgresql.core.TestUtils.dropFunction
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forString
 import static org.junit.Assert.assertEquals
 
 @SpringBootTest(classes = [TestApplication.class])
@@ -18,6 +25,9 @@ class TenantHasAuthoritiesFunctionProducerItTest extends Specification {
     def tested = new TenantHasAuthoritiesFunctionProducer()
 
     EqualsCurrentTenantIdentifierFunctionInvocationFactory equalsCurrentTenantIdentifierFunctionInvocationFactory
+    def functionName
+    def schema
+    def functionDefinition
 
     @Autowired
     JdbcTemplate jdbcTemplate
@@ -38,19 +48,57 @@ class TenantHasAuthoritiesFunctionProducerItTest extends Specification {
     }
 
     @Unroll
-    def "should create sql statement" ()
-    {
+    def "should create function '#testFunctionName' for schema '#testSchema' (null means public) which returns expected result (#exptectedResult) for passed tenant value (#passedValue)" () {
         given:
-            def result = true
+            functionName = testFunctionName
+            schema = testSchema
+            assertEquals(false, isFunctionExists(jdbcTemplate, functionName, schema))
 
-        expect:
-            result == true
-        //TODO
+        when:
+            functionDefinition = tested.produce(new TenantHasAuthoritiesFunctionProducerParameters(testFunctionName, testSchema, equalsCurrentTenantIdentifierFunctionInvocationFactory))
+            jdbcTemplate.execute(functionDefinition.getCreateScript())
+
+        then:
+            isFunctionExists(jdbcTemplate, functionName, schema)
+
+        and: "return correct result"
+            returnSelectStatementResultAfterSettingCurrentTenantId(passedValue) == exptectedResult
+
+        where:
+            testSchema              |   testFunctionName                    |   passedValue                 || exptectedResult
+            null                    |   "tenant_has_authorities_function"   |   "ABCDE"                     ||   true
+            "public"                |   "tenant_has_authorities_function"   |   "ABCDE"                     ||   true
+            "non_public_schema"     |   "tenant_has_authorities_function"   |   "ABCDE"                     ||   true
+            null                    |   "tenant_has_privliges"              |   "ABCDE"                     ||   true
+            "public"                |   "tenant_has_privliges"              |   "ABCDE"                     ||   true
+            "non_public_schema"     |   "tenant_has_privliges"              |   "ABCDE"                     ||   true
+            null                    |   "tenant_has_authorities_function"   |   "ABEEE"                     ||   false
+            "public"                |   "tenant_has_authorities_function"   |   "ABEEE"                     ||   false
+            "non_public_schema"     |   "tenant_has_authorities_function"   |   "ABEEE"                     ||   false
+            null                    |   "tenant_has_privliges"              |   "ABEEE"                     ||   false
+            "public"                |   "tenant_has_privliges"              |   "ABEEE"                     ||   false
+            "non_public_schema"     |   "tenant_has_privliges"              |   "ABEEE"                     ||   false
+    }
+
+    def returnSelectStatementResultAfterSettingCurrentTenantId(String propertyValue, String passedValue)
+    {
+        return jdbcTemplate.execute(new StatementCallback<Boolean>() {
+            @Override
+            Boolean doInStatement(Statement statement) throws SQLException, DataAccessException {
+                statement.execute(setCurrentTenantIdFunctionDefinition.generateStatementThatSetTenant(propertyValue))
+                def selectStatement = String.format("SELECT %s;", functionDefinition.returnEqualsCurrentTenantIdentifierFunctionInvocation(forString(passedValue)))
+                ResultSet rs = statement.executeQuery(selectStatement)
+                rs.next()
+                return rs.getBoolean(1)
+            }
+        })
     }
 
     def cleanup()
     {
         dropFunction(jdbcTemplate, "is_tenant_starts_with_abcd", null, "text")
         assertEquals(false, isFunctionExists(jdbcTemplate, "is_tenant_starts_with_abcd", null))
+        jdbcTemplate.execute(functionDefinition.getDropScript())
+        assertEquals(false, isFunctionExists(jdbcTemplate, functionName, schema))
     }
 }
