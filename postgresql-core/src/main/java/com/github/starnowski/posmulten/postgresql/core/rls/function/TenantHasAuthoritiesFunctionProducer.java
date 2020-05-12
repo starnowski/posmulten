04bef53872787fcaf5673522573f1f3647f3030a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-12 15:22:19 +0200

    #56 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducer.java
@@ -1,16 +1,53 @@
 package com.github.starnowski.posmulten.postgresql.core.rls.function;
 
-import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactory;
+import com.github.starnowski.posmulten.postgresql.core.common.function.ExtendedAbstractFunctionFactory;
+import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentBuilder;
+import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionArgument;
 import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.function.metadata.MetadataPhraseBuilder;
 
-public class TenantHasAuthoritiesFunctionProducer extends AbstractFunctionFactory<ITenantHasAuthoritiesFunctionProducerParameters, TenantHasAuthoritiesFunctionDefinition> {
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forReference;
+import static com.github.starnowski.posmulten.postgresql.core.common.function.metadata.ParallelModeEnum.SAFE;
+import static com.github.starnowski.posmulten.postgresql.core.common.function.metadata.VolatilityCategoryEnum.STABLE;
+
+public class TenantHasAuthoritiesFunctionProducer extends ExtendedAbstractFunctionFactory<ITenantHasAuthoritiesFunctionProducerParameters, TenantHasAuthoritiesFunctionDefinition> {
+
+    public static final String DEFAULT_ARGUMENT_TYPE = "VARCHAR(255)";
+
     @Override
     protected TenantHasAuthoritiesFunctionDefinition returnFunctionDefinition(ITenantHasAuthoritiesFunctionProducerParameters parameters, IFunctionDefinition functionDefinition) {
         return new TenantHasAuthoritiesFunctionDefinition(functionDefinition);
     }
 
     @Override
-    protected String produceStatement(ITenantHasAuthoritiesFunctionProducerParameters parameters) {
-        return null;
+    protected List<IFunctionArgument> prepareFunctionArguments(ITenantHasAuthoritiesFunctionProducerParameters parameters) {
+        return Arrays.asList(argument(parameters.getTenantIdArgumentType()), argument(parameters.getUsingExpressionArgumentType()), argument(parameters.getCheckWithExpressionArgumentType())
+        ,argument(parameters.getTableArgumentType()), argument(parameters.getSchemaArgumentType()));
+    }
+
+    private IFunctionArgument argument(String type)
+    {
+        return FunctionArgumentBuilder.forType(type == null ? DEFAULT_ARGUMENT_TYPE : type);
+    }
+
+    @Override
+    protected String prepareReturnType(ITenantHasAuthoritiesFunctionProducerParameters parameters) {
+        return "BOOLEAN";
+    }
+
+    @Override
+    protected void enrichMetadataPhraseBuilder(ITenantHasAuthoritiesFunctionProducerParameters parameters, MetadataPhraseBuilder metadataPhraseBuilder) {
+        metadataPhraseBuilder.withVolatilityCategorySupplier(STABLE).withParallelModeSupplier(SAFE);
+    }
+
+    @Override
+    protected String buildBody(ITenantHasAuthoritiesFunctionProducerParameters parameters) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT ");
+        sb.append(parameters.getEqualsCurrentTenantIdentifierFunctionInvocationFactory().returnEqualsCurrentTenantIdentifierFunctionInvocation(forReference("$1")));
+        return sb.toString();
     }
 }
