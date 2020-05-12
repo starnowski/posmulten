75c14a8c153b5e73b071df5d83459d223c142c9d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-12 10:15:33 +0200

    #56 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/common/function/metadata/MetadataPhraseBuilder.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/common/function/metadata/MetadataPhraseBuilder.java
@@ -1,10 +1,29 @@
 package com.github.starnowski.posmulten.postgresql.core.common.function.metadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
 public class MetadataPhraseBuilder {
 
     private VolatilityCategorySupplier volatilityCategorySupplier;
 
     private ParallelModeSupplier parallelModeSupplier;
+
+    public String build()
+    {
+        List<String> metadataDefinitions = new ArrayList<>();
+        if (volatilityCategorySupplier != null)
+        {
+            metadataDefinitions.add(volatilityCategorySupplier.getVolatilityCategoryString());
+        }
+        if (parallelModeSupplier != null)
+        {
+            metadataDefinitions.add(parallelModeSupplier.getParallelModeString());
+        }
+        return metadataDefinitions.stream().collect(joining("\n"));
+    }
 
     public MetadataPhraseBuilder withParallelModeSupplier(ParallelModeSupplier parallelModeSupplier) {
         this.parallelModeSupplier = parallelModeSupplier;
