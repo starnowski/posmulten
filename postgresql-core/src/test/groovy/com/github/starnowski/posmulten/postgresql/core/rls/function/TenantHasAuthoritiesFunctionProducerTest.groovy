0c7db2b905fc7890e7575d1d8036f15cf340e8f1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-13 13:41:49 +0200

    #56 - Added unit test. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/TenantHasAuthoritiesFunctionProducerTest.groovy
@@ -2,10 +2,47 @@ package com.github.starnowski.posmulten.postgresql.core.rls.function
 
 import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactoryTest
 import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueEnum
+import spock.lang.Shared
+import spock.lang.Unroll
 
 class TenantHasAuthoritiesFunctionProducerTest extends AbstractFunctionFactoryTest {
 
     def tested = new TenantHasAuthoritiesFunctionProducer()
+
+    @Shared
+    def firstEqualsCurrentTenantIdentifierFunctionInvocationFactory = { tenant ->
+        "is_tenant_starts_with_abcd(" + (FunctionArgumentValueEnum.STRING.equals(tenant.getType()) ? ("'" + tenant.getValue() + "'") : tenant.getValue()) + ")"
+    }
+    @Shared
+    def secondEqualsCurrentTenantIdentifierFunctionInvocationFactory = { tenant ->
+        "matches_current_tenant(" + (FunctionArgumentValueEnum.STRING.equals(tenant.getType()) ? ("'" + tenant.getValue() + "'") : tenant.getValue()) + ")"
+    }
+
+    @Unroll
+    def "should generate statement that creates function for parameters object '#parametersObject' and return expected statement '#expectedStatement'" () {
+        expect:
+        tested.produce(parametersObject).getCreateScript() == expectedStatement
+
+        where:
+            parametersObject <<     [builder().withFunctionName("tenant_has_authorities").withSchema(null)
+                                            .withEqualsCurrentTenantIdentifierFunctionInvocationFactory(firstEqualsCurrentTenantIdentifierFunctionInvocationFactory)
+                                            .withTenantIdArgumentType("VARCHAR(312)")
+                                            .withPermissionCommandPolicyArgumentType("text")
+                                            .withRlsExpressionArgumentType("VARCHAR(73)")
+                                            .withTableArgumentType("text")
+                                            .withSchemaArgumentType("VARCHAR(117)")
+                                            .build()]
+            expectedStatement <<    ["CREATE OR REPLACE FUNCTION tenant_has_authorities(VARCHAR(312), text, VARCHAR(73), text, VARCHAR(117)) RETURNS BOOLEAN AS \$\$" +
+                                    "\nSELECT is_tenant_starts_with_abcd(\$1)" +
+                                    "\n\$\$ LANGUAGE sql" +
+                                    "\nSTABLE" +
+                                    "\nPARALLEL SAFE;"]
+    }
+
+    private TenantHasAuthoritiesFunctionProducerParameters.TenantHasAuthoritiesFunctionProducerParametersBuilder builder()
+    {
+        new TenantHasAuthoritiesFunctionProducerParameters.TenantHasAuthoritiesFunctionProducerParametersBuilder()
+    }
 
     @Override
     protected returnTestedObject() {
@@ -14,9 +51,6 @@ class TenantHasAuthoritiesFunctionProducerTest extends AbstractFunctionFactoryTe
 
     @Override
     protected returnCorrectParametersSpyObject() {
-        def equalsCurrentTenantIdentifierFunctionInvocationFactory = { tenant ->
-            "is_tenant_starts_with_abcd(" + (FunctionArgumentValueEnum.STRING.equals(tenant.getType()) ? ("'" + tenant.getValue() + "'") : tenant.getValue()) + ")"
-        }
-        Spy(TenantHasAuthoritiesFunctionProducerParameters, constructorArgs: ["tenant_has_authorities", "public", equalsCurrentTenantIdentifierFunctionInvocationFactory, "text", "VARCHAR(13)", "VARCHAR(512)", "VARCHAR(128)", "VARCHAR(32)"])
+        Spy(TenantHasAuthoritiesFunctionProducerParameters, constructorArgs: ["tenant_has_authorities", "public", firstEqualsCurrentTenantIdentifierFunctionInvocationFactory, "text", "VARCHAR(13)", "VARCHAR(512)", "VARCHAR(128)", "VARCHAR(32)"])
     }
 }
