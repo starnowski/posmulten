d839e34a99ab3f66c48cfff2a8ee56a688e0bb69
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-16 14:11:07 +0200

    #57 - Added basic interface for the parameters object. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/IRLSPolicyProducerParameters.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/IRLSPolicyProducerParameters.java
@@ -1,12 +1,22 @@
 package com.github.starnowski.posmulten.postgresql.core.rls;
 
+import com.github.starnowski.posmulten.postgresql.core.rls.function.TenantHasAuthoritiesFunctionInvocationFactory;
+
 public interface IRLSPolicyProducerParameters {
 
     String getPolicyName();
 
+    String getPolicyTabe();
 
+    String getPolicySchema();
+
+    String getTenantIdColumn();
 
     RLSExpressionTypeEnum getRlsExpressionType();
 
     PermissionCommandPolicyEnum getPermissionCommandPolicy();
+
+    TenantHasAuthoritiesFunctionInvocationFactory getWithCheckExpressionTenantHasAuthoritiesFunctionInvocationFactory();
+
+    TenantHasAuthoritiesFunctionInvocationFactory getUsingExpressionTenantHasAuthoritiesFunctionInvocationFactory();
 }
