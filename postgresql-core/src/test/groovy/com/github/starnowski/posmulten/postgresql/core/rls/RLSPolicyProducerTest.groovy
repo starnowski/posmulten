7fa74debe7e3264f7f572d991d521e97ec4a818b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-23 11:07:48 +0200

    #57 - Added unit tests (not yet finished). [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/RLSPolicyProducerTest.groovy
@@ -0,0 +1,85 @@
+package com.github.starnowski.posmulten.postgresql.core.rls
+
+import com.github.starnowski.posmulten.postgresql.core.rls.function.EqualsCurrentTenantIdentifierFunctionInvocationFactory
+import com.github.starnowski.posmulten.postgresql.core.rls.function.TenantHasAuthoritiesFunctionProducer
+import com.github.starnowski.posmulten.postgresql.core.rls.function.TenantHasAuthoritiesFunctionProducerParameters
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValueToStringMapper.mapToString
+import static com.github.starnowski.posmulten.postgresql.core.rls.DefaultRLSPolicyProducerParameters.builder
+import static com.github.starnowski.posmulten.postgresql.core.rls.PermissionCommandPolicyEnum.ALL
+import static org.junit.Assert.assertEquals
+
+class RLSPolicyProducerTest extends Specification {
+
+    private static final String IS_TENANT_ID_CORRECT_TEST_FUNCTION = "is_tenant_starts_with_abcd"
+    private static final String TENANT_HAS_AUTHORITIES_TEST_FUNCTION = "tenant_has_authorities_function"
+    TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory1
+    TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory2
+    def tested = new RLSPolicyProducer()
+
+    def setup()
+    {
+        EqualsCurrentTenantIdentifierFunctionInvocationFactory equalsCurrentTenantIdentifierFunctionInvocationFactory = prepareEqualsCurrentTenantIdentifierFunctionInvocationFactoryForTest()
+        TenantHasAuthoritiesFunctionProducer producer = new TenantHasAuthoritiesFunctionProducer()
+        def tenantHasAuthoritiesFunction = producer.produce(new TenantHasAuthoritiesFunctionProducerParameters(TENANT_HAS_AUTHORITIES_TEST_FUNCTION, null, equalsCurrentTenantIdentifierFunctionInvocationFactory))
+
+        tenantHasAuthoritiesFunctionInvocationFactory1 = tenantHasAuthoritiesFunction
+        tenantHasAuthoritiesFunctionInvocationFactory2 = prepareTenantHasAuthoritiesFunctionInvocationFactoryForTestFunctionThatDetermineIfTenantIdIsCorrect()
+    }
+
+    @Unroll
+    def "should create policy with name '#testPolicyName' for schema '#testSchema' (null means public) and table #testTable for grantee #grantee" ()
+    {
+        given:
+        policyName = testPolicyName
+        schema = testSchema
+        table = testTable
+        assertEquals(false, isRLSPolicyExists(jdbcTemplate, policyName, table, schema))
+        assertEquals(false, isRLSPolicyForGranteeExists(jdbcTemplate, policyName, table, schema, grantee))
+
+        when:
+        policyDefinition = tested.produce(builder().withPolicyName(policyName)
+                .withPolicySchema(schema)
+                .withPolicyTable(table)
+                .withGrantee(grantee)
+                .withPermissionCommandPolicy(ALL)
+                .withUsingExpressionTenantHasAuthoritiesFunctionInvocationFactory(tenantHasAuthoritiesFunctionInvocationFactory1)
+                .withWithCheckExpressionTenantHasAuthoritiesFunctionInvocationFactory(tenantHasAuthoritiesFunctionInvocationFactory2)
+                .build())
+        jdbcTemplate.execute(policyDefinition.getCreateScript())
+
+        then:
+        isRLSPolicyExists(jdbcTemplate, policyName, table, schema)
+        isRLSPolicyForGranteeExists(jdbcTemplate, policyName, table, schema, grantee)
+
+        where:
+        testSchema              |   testPolicyName          |   testTable       |   grantee
+        null                    |   "users_policy"          |   "users"         |   "postgresql-core-user"
+        "public"                |   "users_policy"          |   "users"         |   "postgresql-core-user"
+        "non_public_schema"     |   "users_policy"          |   "users"         |   "postgresql-core-user"
+        null                    |   "users_policy"          |   "users"         |   "postgresql-core-owner"
+        "public"                |   "users_policy"          |   "users"         |   "postgresql-core-owner"
+        "non_public_schema"     |   "users_policy"          |   "users"         |   "postgresql-core-owner"
+        null                    |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-user"
+        "public"                |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-user"
+        "non_public_schema"     |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-user"
+        null                    |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-owner"
+        "public"                |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-owner"
+        "non_public_schema"     |   "users_groups_policy"   |   "users_groups"  |   "postgresql-core-owner"
+    }
+
+    private Closure<String> prepareEqualsCurrentTenantIdentifierFunctionInvocationFactoryForTest() {
+        { tenant ->
+            IS_TENANT_ID_CORRECT_TEST_FUNCTION + "(" + mapToString(tenant) + ")"
+        }
+    }
+
+    private Closure<String> prepareTenantHasAuthoritiesFunctionInvocationFactoryForTestFunctionThatDetermineIfTenantIdIsCorrect() {
+        { tenantIdValue, permissionCommandPolicy, rlsExpressionType, table, schema ->
+            IS_TENANT_ID_CORRECT_TEST_FUNCTION + "(" + mapToString(tenantIdValue) + ")"
+        }
+    }
+
+}
