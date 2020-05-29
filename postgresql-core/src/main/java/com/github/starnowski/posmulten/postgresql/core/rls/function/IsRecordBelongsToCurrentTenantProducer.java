9f9c0d6a446e4437c3a45b2fcd3340872e90d5a3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-29 22:51:44 +0200

    #58 - Added implementation for the function arguments phrase. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducer.java
@@ -2,8 +2,16 @@ package com.github.starnowski.posmulten.postgresql.core.rls.function;
 
 import com.github.starnowski.posmulten.postgresql.core.common.function.DefaultFunctionDefinition;
 import com.github.starnowski.posmulten.postgresql.core.common.function.ExtendedAbstractFunctionFactory;
+import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionArgument;
 import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
 import com.github.starnowski.posmulten.postgresql.core.common.function.metadata.MetadataPhraseBuilder;
+import javafx.util.Pair;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.of;
 
 public class IsRecordBelongsToCurrentTenantProducer extends ExtendedAbstractFunctionFactory<AbstractIsRecordBelongsToCurrentTenantProducerParameters, DefaultFunctionDefinition> {
     @Override
@@ -24,5 +32,10 @@ public class IsRecordBelongsToCurrentTenantProducer extends ExtendedAbstractFunc
     @Override
     protected DefaultFunctionDefinition returnFunctionDefinition(AbstractIsRecordBelongsToCurrentTenantProducerParameters parameters, IFunctionDefinition functionDefinition) {
         return new DefaultFunctionDefinition(functionDefinition);
+    }
+
+    @Override
+    protected List<IFunctionArgument> prepareFunctionArguments(AbstractIsRecordBelongsToCurrentTenantProducerParameters parameters) {
+        return concat(parameters.getKeyColumnsPairsList().stream().map(Pair::getValue), of(parameters.getTenantColumnPair().getValue())).collect(toList());
     }
 }
