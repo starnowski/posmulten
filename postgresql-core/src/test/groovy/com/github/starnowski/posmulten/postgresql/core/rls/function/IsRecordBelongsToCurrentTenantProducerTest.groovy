a7c6b5ab8f8da6fe81c9ea702a33b4663e3eac2f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-05-31 19:16:45 +0200

    #58 - Added unit tests for the function invocation. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducerTest.groovy
@@ -3,6 +3,7 @@ package com.github.starnowski.posmulten.postgresql.core.rls.function
 import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactoryTest
 import spock.lang.Unroll
 
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forReference
 import static com.github.starnowski.posmulten.postgresql.core.rls.function.AbstractIsRecordBelongsToCurrentTenantProducerParameters.pairOfColumnWithType
 
 class IsRecordBelongsToCurrentTenantProducerTest extends AbstractFunctionFactoryTest {
@@ -35,6 +36,30 @@ class IsRecordBelongsToCurrentTenantProducerTest extends AbstractFunctionFactory
             "public"                |   "is_current_tenant_groups"              |   "groups"            |   "secondary"         |   "cur_ten()"                 |   pairOfColumnWithType("tenant_id", "text")       |   [pairOfColumnWithType("id", "bigint")]                                                  ||  "CREATE OR REPLACE FUNCTION public.is_current_tenant_groups(bigint, text) RETURNS BOOLEAN AS \$\$\nSELECT EXISTS (\n\tSELECT 1 FROM secondary.groups rt WHERE rt.id = \$1 AND \$2 = cur_ten()\n)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
             "secondary"             |   "is_posts_belong_to_tenant"             |   "posts"             |   "third"             |   "cur_ten()"                 |   pairOfColumnWithType("tenant", "VARCHAR")       |   [pairOfColumnWithType("uuid", "UUID")]                                                  ||  "CREATE OR REPLACE FUNCTION secondary.is_posts_belong_to_tenant(UUID, VARCHAR) RETURNS BOOLEAN AS \$\$\nSELECT EXISTS (\n\tSELECT 1 FROM third.posts rt WHERE rt.uuid = \$1 AND \$2 = cur_ten()\n)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
             "some_schema"           |   "is_user_belongs_to_current_tenant"     |   "user_info"         |   "pub"               |   "cur_ten()"                 |   pairOfColumnWithType("tenant_id", "VARCHAR")    |   [pairOfColumnWithType("uuid", "UUID"), pairOfColumnWithType("second_id", "bigint")]     ||  "CREATE OR REPLACE FUNCTION some_schema.is_user_belongs_to_current_tenant(UUID, bigint, VARCHAR) RETURNS BOOLEAN AS \$\$\nSELECT EXISTS (\n\tSELECT 1 FROM pub.user_info rt WHERE rt.uuid = \$1 AND rt.second_id = \$2 AND \$3 = cur_ten()\n)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
+    }
+
+    @Unroll
+    def "for function name '#testFunctionName' for schema '#testSchema' that compares values for columns #keyColumnsPairs and their values #keyColumnsValues, tenant column #tenantColumnPair and it value #tenantColumnValue, should generate statement that invokes function : #expectedStatement" () {
+        given:
+            IGetCurrentTenantIdFunctionInvocationFactory getCurrentTenantIdFunctionInvocationFactory =
+                    {
+                        "current_tenant()"
+                    }
+            def parameters = new IsRecordBelongsToCurrentTenantProducerParameters.Builder()
+                    .withSchema(testSchema)
+                    .withFunctionName(testFunctionName)
+                    .withRecordTableName("some_record_table")
+                    .withRecordSchemaName("some_record_schema")
+                    .withiGetCurrentTenantIdFunctionInvocationFactory(getCurrentTenantIdFunctionInvocationFactory)
+                    .withTenantColumnPair(tenantColumnPair)
+                    .withKeyColumnsPairsList(keyColumnsPairs).build()
+
+        expect:
+            tested.produce(parameters).returnIsRecordBelongsToCurrentTenantFunctionInvocation(tenantColumnValue, ) == expectedStatement
+
+        where:
+            testSchema              |   testFunctionName                        |   tenantColumnPair                                |   keyColumnsPairs                         |   tenantColumnValue           |   keyColumnsValues                || expectedStatement
+            null                    |   "is_user_belongs_to_current_tenant"     |   pairOfColumnWithType("tenant_id", "text")       |   [pairOfColumnWithType("id", "bigint")]  |   forReference("tenant_id")   |   [id: forReference("id")]        ||  "is_user_belongs_to_current_tenant(2321, id)"
     }
 
     @Override
