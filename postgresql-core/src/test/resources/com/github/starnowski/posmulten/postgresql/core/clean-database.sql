f95c14dcbbb42f40bdf90247bf347877cf171c32
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-01 23:23:29 +0200

    #58 - Test code refactoring. [skip travis]

--- postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/clean-database.sql
+++ postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/clean-database.sql
@@ -1,4 +1,4 @@
-DELETE FROM users CASCADE;
-DELETE FROM non_public_schema.users CASCADE;
 DELETE FROM comments CASCADE;
 DELETE FROM non_public_schema.comments CASCADE;
+DELETE FROM users CASCADE;
+DELETE FROM non_public_schema.users CASCADE;
