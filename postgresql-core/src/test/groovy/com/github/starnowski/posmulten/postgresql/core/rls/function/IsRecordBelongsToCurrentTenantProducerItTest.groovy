57ed1c9393abdaa07cedc8665ff00ff3d6c4d5fd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-02 16:54:23 +0200

    #58 - Added integration test (not yet finished!). [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducerItTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantProducerItTest.groovy
@@ -1,13 +1,21 @@
 package com.github.starnowski.posmulten.postgresql.core.rls.function
 
 import com.github.starnowski.posmulten.postgresql.core.TestApplication
+import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.DataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.StatementCallback
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+
 import static com.github.starnowski.posmulten.postgresql.core.TestUtils.isFunctionExists
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forNumeric
 import static com.github.starnowski.posmulten.postgresql.core.rls.function.AbstractIsRecordBelongsToCurrentTenantProducerParameters.pairOfColumnWithType
 import static org.junit.Assert.assertEquals
 
@@ -63,40 +71,59 @@ class IsRecordBelongsToCurrentTenantProducerItTest extends Specification {
     }
 
     @Unroll
-    def "for function name '#testFunctionName' for schema '#testSchema', table #recordTableName in schema #recordSchemaName that compares values for columns #keyColumnsPairs and tenant column #tenantColumnPair, should generate statement that creates function" () {
+    def "for function name '#testFunctionName' for schema '#testSchema', table #recordTableName in schema #recordSchemaName that compares values for columns #keyColumnsPairs and tenant column #tenantColumnPair, should return positve boolean result" () {
         given:
-        functionName = "is_user_belongs_to_current_tenant"
-        schema = "public"
-        assertEquals(false, isFunctionExists(jdbcTemplate, functionName, schema))
-        def parameters = new IsRecordBelongsToCurrentTenantProducerParameters.Builder()
-                .withSchema(schema)
-                .withFunctionName(functionName)
-                .withRecordTableName(recordTableName)
-                .withRecordSchemaName(recordSchemaName)
-                .withiGetCurrentTenantIdFunctionInvocationFactory(getCurrentTenantIdFunctionInvocationFactory)
-                .withTenantColumnPair(tenantColumnPair)
-                .withKeyColumnsPairsList(keyColumnsPairs).build()
+            functionName = "is_user_belongs_to_current_tenant"
+            schema = "public"
+            assertEquals(false, isFunctionExists(jdbcTemplate, functionName, schema))
+            def parameters = new IsRecordBelongsToCurrentTenantProducerParameters.Builder()
+                    .withSchema(schema)
+                    .withFunctionName(functionName)
+                    .withRecordTableName(recordTableName)
+                    .withRecordSchemaName(recordSchemaName)
+                    .withiGetCurrentTenantIdFunctionInvocationFactory(getCurrentTenantIdFunctionInvocationFactory)
+                    .withTenantColumnPair(tenantColumnPair)
+                    .withKeyColumnsPairsList(keyColumnsPairs).build()
+            Map<String, FunctionArgumentValue> map = new HashMap<>();
+            map.put(tenantColumnPair.getKey(), testCurrentTenantIdValue)
 
         when:
-        functionDefinition = tested.produce(parameters)
-        jdbcTemplate.execute(functionDefinition.getCreateScript())
+            functionDefinition = tested.produce(parameters)
+            jdbcTemplate.execute(functionDefinition.getCreateScript())
 
         then:
-        isFunctionExists(jdbcTemplate, functionName, schema)
+            getBooleanResultForSelectStatement(testCurrentTenantIdValue, returnTestedSelectStatement(functionDefinition.returnIsRecordBelongsToCurrentTenantFunctionInvocation(forNumeric(String.valueOf(testUsersId)), map))) == expectedBooleanValue
 
         where:
-        recordTableName     |   recordSchemaName    |   tenantColumnPair                                            |   keyColumnsPairs
-        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "users"             |   "public"            |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "users"             |   "public"            |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "users"             |   "non_public_schema" |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]
-        "comments"          |   "public"            |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
-        "comments"          |   "public"            |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
-        "comments"          |   "non_public_schema" |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
+        recordTableName     |   recordSchemaName    |   tenantColumnPair                                            |   keyColumnsPairs                         |   testCurrentTenantIdValue    |   testUsersId || expectedBooleanValue
+        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+        "users"             |   "public"            |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+        "users"             |   null                |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+        "users"             |   "public"            |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+        "users"             |   "non_public_schema" |   pairOfColumnWithType("tenant_id", "text")                   |   [pairOfColumnWithType("id", "bigint")]  |   "primary_tenant"            |   1           ||  true
+//        "comments"          |   "public"            |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
+//        "comments"          |   "public"            |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
+//        "comments"          |   "non_public_schema" |   pairOfColumnWithType("tenant", "character varying(255)")    |   [pairOfColumnWithType("id", "int"), pairOfColumnWithType("user_id", "bigint")]
     }
 
+    def getBooleanResultForSelectStatement(String propertyValue, String selectStatement)
+    {
+        return jdbcTemplate.execute(new StatementCallback<Boolean>() {
+            @Override
+            Boolean doInStatement(Statement statement) throws SQLException, DataAccessException {
+                statement.execute("SET " + VALID_CURRENT_TENANT_ID_PROPERTY_NAME + " = '" + propertyValue + "';")
+                ResultSet rs = statement.executeQuery(selectStatement)
+                rs.next()
+                return rs.getBoolean(1)
+            }
+        })
+    }
+
+    def returnTestedSelectStatement(String functionIvocation)
+    {
+        "SELECT " + functionIvocation
+    }
 
     def cleanup() {
         jdbcTemplate.execute(functionDefinition.getDropScript())
