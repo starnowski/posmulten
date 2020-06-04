b5e7730d9fb010cd886adf7bf4056412b664d06e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-04 15:08:16 +0200

    #58 - Inserting records for tables posts and comments in integration tests.

--- postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/clean-database.sql
+++ postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/clean-database.sql
@@ -1,4 +1,6 @@
 DELETE FROM public.comments CASCADE;
 DELETE FROM non_public_schema.comments CASCADE;
+DELETE FROM public.posts CASCADE;
+DELETE FROM non_public_schema.posts CASCADE;
 DELETE FROM public.users CASCADE;
 DELETE FROM non_public_schema.users CASCADE;
--- postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/rls/function/insert-unrelated-data.sql
+++ postgresql-core/src/test/resources/com/github/starnowski/posmulten/postgresql/core/rls/function/insert-unrelated-data.sql
@@ -5,10 +5,14 @@ INSERT INTO public.users (id, name, tenant_id) VALUES (1, 'Szymon Tarnowski', 'p
 INSERT INTO public.users (id, name, tenant_id) VALUES (2, 'John Doe', 'secondary_tenant');
 INSERT INTO public.users (id, name, tenant_id) VALUES (3, 'Jane Doe', 'primary_tenant');
 
+-- posts
+INSERT INTO posts (id, user_id, text, tenant_id) VALUES (8, 1, 'Some phrase', 'primary_tenant');
+INSERT INTO posts (id, user_id, text, tenant_id) VALUES (9, 2, 'Some phrase', 'secondary_tenant');
+
 -- comments
---INSERT INTO comments (id, user_id, text, tenant) VALUES (1, 2, 'Some phrase', 'secondary_tenant');
---INSERT INTO comments (id, user_id, text, tenant) VALUES (2, 2, 'Lorem Ipsum', 'secondary_tenant');
---INSERT INTO comments (id, user_id, text, tenant) VALUES (3, 1, 'Lorem Ipsum', 'primary_tenant');
+INSERT INTO comments (id, user_id, text, tenant, post_id) VALUES (1, 2, 'Some phrase', 'secondary_tenant', 9);
+INSERT INTO comments (id, user_id, text, tenant, post_id) VALUES (2, 2, 'Lorem Ipsum', 'secondary_tenant', 9);
+INSERT INTO comments (id, user_id, text, tenant, post_id) VALUES (3, 1, 'Lorem Ipsum', 'primary_tenant', 8);
 
 --- non_public_schema schema
 
@@ -18,8 +22,12 @@ INSERT INTO non_public_schema.users (id, name, tenant_id) VALUES (2, 'John Doe',
 INSERT INTO non_public_schema.users (id, name, tenant_id) VALUES (3, 'Bill Doe', 'primary_tenant');
 INSERT INTO non_public_schema.users (id, name, tenant_id) VALUES (4, 'Michael Doe', 'third_tenant');
 
+-- posts
+INSERT INTO non_public_schema.posts (id, user_id, text, tenant_id) VALUES (3, 1, 'Some phrase', 'third_tenant');
+INSERT INTO non_public_schema.posts (id, user_id, text, tenant_id) VALUES (4, 2, 'Some phrase', 'primary_tenant');
+
 -- comments
---INSERT INTO non_public_schema.comments (id, user_id, text, tenant) VALUES (1, 2, 'Some phrase', 'third_tenant');
---INSERT INTO non_public_schema.comments (id, user_id, text, tenant) VALUES (2, 2, 'Lorem Ipsum', 'third_tenant');
---INSERT INTO non_public_schema.comments (id, user_id, text, tenant) VALUES (3, 3, 'Lorem Ipsum', 'primary_tenant');
---INSERT INTO non_public_schema.comments (id, user_id, text, tenant) VALUES (4, 3, 'Lorem Ipsum', 'primary_tenant');
+INSERT INTO non_public_schema.comments (id, user_id, text, tenant, post_id) VALUES (1, 2, 'Some phrase', 'third_tenant', 3);
+INSERT INTO non_public_schema.comments (id, user_id, text, tenant, post_id) VALUES (2, 2, 'Lorem Ipsum', 'third_tenant', 3);
+INSERT INTO non_public_schema.comments (id, user_id, text, tenant, post_id) VALUES (3, 3, 'Lorem Ipsum', 'primary_tenant', 4);
+INSERT INTO non_public_schema.comments (id, user_id, text, tenant, post_id) VALUES (4, 3, 'Lorem Ipsum', 'primary_tenant', 4);
