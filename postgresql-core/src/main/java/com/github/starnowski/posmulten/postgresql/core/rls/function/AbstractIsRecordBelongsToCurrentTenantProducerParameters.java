226fb29dc46697027dce8b0902088d89fa449cb9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-05 08:21:22 +0200

    #58 - Removed TODO comment. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/AbstractIsRecordBelongsToCurrentTenantProducerParameters.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/AbstractIsRecordBelongsToCurrentTenantProducerParameters.java
@@ -15,7 +15,6 @@ public interface AbstractIsRecordBelongsToCurrentTenantProducerParameters extend
     //TODO types validation similar to TenantColumnPair
     List<Pair<String, IFunctionArgument>> getKeyColumnsPairsList();
 
-    //TODO cannot be null
     //TODO key cannot be empty
     String getTenantColumn();
 
