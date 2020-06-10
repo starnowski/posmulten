5fc861debbb903e814b5caad639ea39298f457c5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-10 23:27:15 +0200

    #58 - Added test case that checks if a correct exception is being thrown when the primary columns values map cannot be null. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantFunctionDefinitionTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantFunctionDefinitionTest.groovy
@@ -19,6 +19,21 @@ class IsRecordBelongsToCurrentTenantFunctionDefinitionTest extends Specification
             definition.returnIsRecordBelongsToCurrentTenantFunctionInvocation(preparePrimaryColumnsValuesMap()) == "schema222.record_exists_for_tenant(fk_col1, fk_id2)"
     }
 
+    def "should throw an exception of type 'IllegalArgumentException' when passed parameters list is null"()
+    {
+        given:
+            def definition = prepareCorrectDefinition()
+
+        when:
+            definition.returnIsRecordBelongsToCurrentTenantFunctionInvocation(null)
+
+        then:
+            def ex = thrown(IllegalArgumentException.class)
+
+        and: "exception should have correct message"
+            ex.message == "The primary columns values map cannot be null"
+    }
+
     IsRecordBelongsToCurrentTenantFunctionDefinition prepareCorrectDefinition() {
         IFunctionDefinition functionDefinitionMock = Mock(IFunctionDefinition)
         functionDefinitionMock.getFunctionReference() >> "schema222.record_exists_for_tenant"
