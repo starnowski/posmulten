788b6becdd06650ef86f0130e3f23dcd737135e6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-10 23:29:55 +0200

    #58 - Removed TODO comment. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantFunctionDefinition.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/IsRecordBelongsToCurrentTenantFunctionDefinition.java
@@ -20,7 +20,6 @@ public class IsRecordBelongsToCurrentTenantFunctionDefinition extends DefaultFun
     @Override
     public String returnIsRecordBelongsToCurrentTenantFunctionInvocation(Map<String, FunctionArgumentValue> primaryColumnsValuesMap) {
         //TODO Validate if
-        //TODO all entries in primaryColumnsValuesMap cannot be null
         //TODO all entries in primaryColumnsValuesMap cannot be blank
         //TODO all items from the keyColumnsPairsList should contains keys in primaryColumnsValuesMap
         validate(primaryColumnsValuesMap);
