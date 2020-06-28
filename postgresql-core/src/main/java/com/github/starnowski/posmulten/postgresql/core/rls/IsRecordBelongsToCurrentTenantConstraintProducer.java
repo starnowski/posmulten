7c6b7e52fbb5cfec7c28d7b5fbef8fbd1e3790a9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-28 18:53:12 +0200

    #58 - Added dummy implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/IsRecordBelongsToCurrentTenantConstraintProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/IsRecordBelongsToCurrentTenantConstraintProducer.java
@@ -1,5 +1,6 @@
 package com.github.starnowski.posmulten.postgresql.core.rls;
 
+import com.github.starnowski.posmulten.postgresql.core.common.DefaultSQLDefinition;
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
 
 public class IsRecordBelongsToCurrentTenantConstraintProducer {
@@ -7,6 +8,6 @@ public class IsRecordBelongsToCurrentTenantConstraintProducer {
     public SQLDefinition produce(IsRecordBelongsToCurrentTenantConstraintProducerParameters parameters)
     {
         //TODO
-        return null;
+        return new DefaultSQLDefinition("ALTER TABLE \"users\" ADD CONSTRAINT sss CHECK (cccsss);", "");
     }
 }
