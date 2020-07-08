c1d2ee56d9d647dec6aa4eb3bb1805203bda3025
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-07-08 23:35:06 +0200

    #70 - Added dummy tests method. [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/DummyTest.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/DummyTest.java
@@ -1,7 +1,15 @@
 package com.github.starnowski.posmulten.postgresql.core.functional.tests;
 
+import org.testng.annotations.Test;
+
 /**
  * Dummy test class, created only to test maven configuration.
  */
 public class DummyTest {
+
+    @Test
+    public void dummyTest()
+    {
+        System.out.println("Dummy test");
+    }
 }
