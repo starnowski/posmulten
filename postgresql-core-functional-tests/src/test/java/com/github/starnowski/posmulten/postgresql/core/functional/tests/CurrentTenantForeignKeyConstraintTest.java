5391291bd8ad7f12ba80cedcbc73fdf03c6f379b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-07-11 14:43:13 +0200

    #70 - Added basic test. [skip travis]

---
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/CurrentTenantForeignKeyConstraintTest.java
@@ -0,0 +1,10 @@
+package com.github.starnowski.posmulten.postgresql.core.functional.tests;
+
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+
+import java.util.List;
+
+public class CurrentTenantForeignKeyConstraintTest {
+
+    private List<SQLDefinition> sqlDefinitions;
+}
