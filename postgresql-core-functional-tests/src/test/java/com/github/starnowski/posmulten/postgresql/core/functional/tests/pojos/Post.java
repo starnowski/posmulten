feb07e80c07d7636baf8d9c78f450c6b17c889f0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-07-19 11:41:50 +0200

    #70 - Added the getters for pojo types. [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/pojos/Post.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/pojos/Post.java
@@ -1,6 +1,7 @@
 package com.github.starnowski.posmulten.postgresql.core.functional.tests.pojos;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
@@ -9,6 +10,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class Post {
 
     private Long id;
