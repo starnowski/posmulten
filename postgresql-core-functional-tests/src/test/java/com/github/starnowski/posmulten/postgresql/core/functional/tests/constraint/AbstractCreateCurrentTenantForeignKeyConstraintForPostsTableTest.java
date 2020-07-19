1b3d33266c4993ed67e13ea77092f384bba99e9e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-07-19 18:31:49 +0200

    #73 - Making the AbstractCreateCurrentTenantForeignKeyConstraintForPostsTableTest type more generic. [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/constraint/AbstractCreateCurrentTenantForeignKeyConstraintForPostsTableTest.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/constraint/AbstractCreateCurrentTenantForeignKeyConstraintForPostsTableTest.java
@@ -45,7 +45,7 @@ public abstract class AbstractCreateCurrentTenantForeignKeyConstraintForPostsTab
 
     abstract protected String getSchema();
 
-    protected String getUserTableReference()
+    protected String getUsersTableReference()
     {
         return (getSchema() == null ? getSchema() + "." : "" ) + "users";
     }
@@ -85,20 +85,20 @@ public abstract class AbstractCreateCurrentTenantForeignKeyConstraintForPostsTab
     {
         //Create function that returns current tenant function
         GetCurrentTenantIdFunctionProducer getCurrentTenantIdFunctionProducer = new GetCurrentTenantIdFunctionProducer();
-        GetCurrentTenantIdFunctionDefinition getCurrentTenantIdFunctionDefinition = getCurrentTenantIdFunctionProducer.produce(new GetCurrentTenantIdFunctionProducerParameters("rls_get_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, null, null));
+        GetCurrentTenantIdFunctionDefinition getCurrentTenantIdFunctionDefinition = getCurrentTenantIdFunctionProducer.produce(new GetCurrentTenantIdFunctionProducerParameters("rls_get_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, getSchema(), null));
         sqlDefinitions.add(getCurrentTenantIdFunctionDefinition);
 
         //Create function that sets current tenant function
         SetCurrentTenantIdFunctionProducer setCurrentTenantIdFunctionProducer = new SetCurrentTenantIdFunctionProducer();
-        setCurrentTenantIdFunctionDefinition = setCurrentTenantIdFunctionProducer.produce(new SetCurrentTenantIdFunctionProducerParameters("rls_set_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, null, null));
+        setCurrentTenantIdFunctionDefinition = setCurrentTenantIdFunctionProducer.produce(new SetCurrentTenantIdFunctionProducerParameters("rls_set_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, getSchema(), null));
         sqlDefinitions.add(setCurrentTenantIdFunctionDefinition);
 
         // Does record belongs to current tenant
         AbstractIsRecordBelongsToCurrentTenantProducerParameters isRecordBelongsToCurrentTenantProducerParameters = new IsRecordBelongsToCurrentTenantProducerParameters.Builder()
-                .withSchema(null)
+                .withSchema(getSchema())
                 .withFunctionName("is_user_belongs_to_current_tenant")
                 .withRecordTableName("users")
-                .withRecordSchemaName(null)
+                .withRecordSchemaName(getSchema())
                 .withiGetCurrentTenantIdFunctionInvocationFactory(getCurrentTenantIdFunctionDefinition)
                 .withTenantColumn("tenant_id")
                 .withKeyColumnsPairsList(ImmutableList.of(pairOfColumnWithType("id", "bigint"))).build();
@@ -114,7 +114,7 @@ public abstract class AbstractCreateCurrentTenantForeignKeyConstraintForPostsTab
         IsRecordBelongsToCurrentTenantConstraintProducerParameters isRecordBelongsToCurrentTenantConstraintProducerParameters = DefaultIsRecordBelongsToCurrentTenantConstraintProducerParameters.builder()
                 .withConstraintName(CONSTRAINT_NAME)
                 .withTableName("posts")
-                .withTableSchema(null)
+                .withTableSchema(getSchema())
                 .withIsRecordBelongsToCurrentTenantFunctionInvocationFactory(isRecordBelongsToCurrentTenantFunctionDefinition)
                 .withPrimaryColumnsValuesMap(primaryColumnsValuesMap).build();
         SQLDefinition recordBelongsToCurrentTenantConstrainSqlDefinition = isRecordBelongsToCurrentTenantConstraintProducer.produce(isRecordBelongsToCurrentTenantConstraintProducerParameters);
@@ -124,7 +124,7 @@ public abstract class AbstractCreateCurrentTenantForeignKeyConstraintForPostsTab
     @Test(dependsOnMethods = {"createSQLDefinitions"}, testName = "constraint should not exists before tests execution", description = "check if constraint does not exist before executing SQL definitions")
     public void constraintShouldNotExistsBeforeTests()
     {
-        assertFalse(isAnyRecordExists(jdbcTemplate, createSelectStatement("public", "posts", CONSTRAINT_NAME)), "Constraint should not exists");
+        assertFalse(isAnyRecordExists(jdbcTemplate, createSelectStatement(getSchema(), "posts", CONSTRAINT_NAME)), "Constraint should not exists");
     }
 
     @SqlGroup({
@@ -143,28 +143,28 @@ public abstract class AbstractCreateCurrentTenantForeignKeyConstraintForPostsTab
     @Test(dependsOnMethods = {"executeSQLDefinitions"}, testName = "constraint should exist after SQL definitions executed", description = "check if constraint exist after executing SQL definitions")
     public void constraintNameShouldExistAfterCreation()
     {
-        assertTrue(isAnyRecordExists(jdbcTemplate, createSelectStatement("public", "posts", CONSTRAINT_NAME)), "Constraint should exists");
+        assertTrue(isAnyRecordExists(jdbcTemplate, createSelectStatement(getSchema(), "posts", CONSTRAINT_NAME)), "Constraint should exists");
     }
 
     @Test(dataProvider = "userData", dependsOnMethods = {"constraintNameShouldExistAfterCreation"}, testName = "insert data into to user table")
     public void insertUserTestData(User user)
     {
-        assertThat(countRowsInTableWhere("users", "id = " + user.getId())).isEqualTo(0);
-        jdbcTemplate.execute(format("INSERT INTO public.users (id, name, tenant_id) VALUES (%1$d, '%2$s', '%3$s');", user.getId(), user.getName(), user.getTenantId()));
-        assertTrue(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM users WHERE id = %1$d AND name = '%2$s' AND tenant_id = '%3$s'", user.getId(), user.getName(), user.getTenantId())), "The tests user should exists");
+        assertThat(countRowsInTableWhere(getUsersTableReference(), "id = " + user.getId())).isEqualTo(0);
+        jdbcTemplate.execute(format("INSERT INTO %4$s (id, name, tenant_id) VALUES (%1$d, '%2$s', '%3$s');", user.getId(), user.getName(), user.getTenantId(), getUsersTableReference()));
+        assertTrue(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM %4$s WHERE id = %1$d AND name = '%2$s' AND tenant_id = '%3$s'", user.getId(), user.getName(), user.getTenantId(), getUsersTableReference())), "The tests user should exists");
     }
 
     @Test(dataProvider = "postData", dependsOnMethods = {"insertUserTestData"}, testName = "try to insert data into the post table related to primary tests tenant as different tenant", description = "test case assumes that constraint is not going to allow to insert data into the post table related to the primary tenant when a current tenant is different")
     public void tryToInsertPostForUserFromDifferentTenant(PostData postData)
     {
         Post post = postData.getPost();
-        assertTrue(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM users WHERE id = %1$d AND tenant_id = '%2$s'", postData.getUserIdThatBelongsToDifferentTenant(), postData.getDifferentTenantId())), "The tests user should exists");
-        assertThat(countRowsInTableWhere("posts", "id = " + post.getUserId())).isEqualTo(0);
+        assertTrue(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM %3$s WHERE id = %1$d AND tenant_id = '%2$s'", postData.getUserIdThatBelongsToDifferentTenant(), postData.getDifferentTenantId(), getUsersTableReference())), "The tests user should exists");
+        assertThat(countRowsInTableWhere(getPostsTableReference(), "id = " + post.getUserId())).isEqualTo(0);
         assertThatThrownBy(() ->
-            jdbcTemplate.execute(format("%1$s INSERT INTO posts (id, user_id, text, tenant_id) VALUES (%2$d, %3$d, '%4$s', '%5$s');", setCurrentTenantIdFunctionDefinition.generateStatementThatSetTenant(postData.getCorrectTenantId()), post.getId(), postData.getUserIdThatBelongsToDifferentTenant(), post.getText(), postData.getCorrectTenantId()))
+            jdbcTemplate.execute(format("%1$s INSERT INTO %6$s (id, user_id, text, tenant_id) VALUES (%2$d, %3$d, '%4$s', '%5$s');", setCurrentTenantIdFunctionDefinition.generateStatementThatSetTenant(postData.getCorrectTenantId()), post.getId(), postData.getUserIdThatBelongsToDifferentTenant(), post.getText(), postData.getCorrectTenantId(), getPostsTableReference()))
         )
         .isInstanceOf(DataIntegrityViolationException.class);
-        assertFalse(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM posts WHERE id = %1$d AND text = '%2$s' AND tenant_id = '%3$s'", post.getId(), post.getText(), postData.getCorrectTenantId())), "The tests post should not exists");
+        assertFalse(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM %4$s WHERE id = %1$d AND text = '%2$s' AND tenant_id = '%3$s'", post.getId(), post.getText(), postData.getCorrectTenantId(), getPostsTableReference())), "The tests post should not exists");
     }
 
     @Test(dataProvider = "postData", dependsOnMethods = {"insertUserTestData", "tryToInsertPostForUserFromDifferentTenant"}, testName = "insert data into the post table related to primary tests tenant as the primary tenant")
