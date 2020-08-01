419e58784ddf431d1082cdb5f64ff87e4509989c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-02 01:31:40 +0200

    #73 - Added pojo object for the comments table. [skip travis]

---
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/pojos/Comment.java
@@ -0,0 +1,23 @@
+package com.github.starnowski.posmulten.postgresql.core.functional.tests.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@ToString
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Comment {
+
+    private Integer id;
+    private Long user_id;
+    private String text;
+    private Long postId;
+    private String tenantId;
+    private Integer parentCommentId;
+    private Long parentCommentUserId;
+}
