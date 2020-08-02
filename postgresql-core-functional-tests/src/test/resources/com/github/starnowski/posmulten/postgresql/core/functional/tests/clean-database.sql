8218ee4c89baae9806df42129ded6db3cf70007e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-02 04:22:47 +0200

    #73 - Fixed deletion of the tests data.

--- postgresql-core-functional-tests/src/test/resources/com/github/starnowski/posmulten/postgresql/core/functional/tests/clean-database.sql
+++ postgresql-core-functional-tests/src/test/resources/com/github/starnowski/posmulten/postgresql/core/functional/tests/clean-database.sql
@@ -1,5 +1,3 @@
-UPDATE public.comments SET parent_comment_id = NULL, parent_comment_user_id = NULL;
-UPDATE non_public_schema.comments SET parent_comment_id = NULL, parent_comment_user_id = NULL;
 DELETE FROM public.comments CASCADE;
 DELETE FROM non_public_schema.comments CASCADE;
 DELETE FROM public.posts CASCADE;
