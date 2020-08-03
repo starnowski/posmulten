de6179a22cf4f2ee86c1a519b72f5b6c3ccc779a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-03 14:31:34 +0200

    #75 - Added the createSQLDefinitions method (not yet finished!). [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/rls/CreateRLSForSingleTableInPublicSchemaTest.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/rls/CreateRLSForSingleTableInPublicSchemaTest.java
@@ -2,8 +2,11 @@ package com.github.starnowski.posmulten.postgresql.core.functional.tests.rls;
 
 import com.github.starnowski.posmulten.postgresql.core.functional.tests.TestNGSpringContextWithoutGenericTransactionalSupportTests;
 import com.github.starnowski.posmulten.postgresql.core.functional.tests.pojos.User;
-import com.github.starnowski.posmulten.postgresql.core.rls.function.SetCurrentTenantIdFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.*;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.VALID_CURRENT_TENANT_ID_PROPERTY_NAME;
 
 public class CreateRLSForSingleTableInPublicSchemaTest extends TestNGSpringContextWithoutGenericTransactionalSupportTests {
 
@@ -29,5 +32,24 @@ public class CreateRLSForSingleTableInPublicSchemaTest extends TestNGSpringConte
                 {new User(1L, "Szymon Tarnowski", USER_TENANT), SECONDARY_USER_TENANT},
                 {new User(2L, "John Doe", SECONDARY_USER_TENANT), USER_TENANT}
         };
+    }
+
+    @Test(testName = "create SQL definitions", description = "Create SQL function that creates statements that set current tenant value, retrieve current tenant value and create the row level security policy for a table that is multi-tenant aware")
+    public void createSQLDefinitions()
+    {
+        //Create function that returns current tenant function
+        GetCurrentTenantIdFunctionProducer getCurrentTenantIdFunctionProducer = new GetCurrentTenantIdFunctionProducer();
+        GetCurrentTenantIdFunctionDefinition getCurrentTenantIdFunctionDefinition = getCurrentTenantIdFunctionProducer.produce(new GetCurrentTenantIdFunctionProducerParameters("rls_get_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, getSchema(), null));
+        sqlDefinitions.add(getCurrentTenantIdFunctionDefinition);
+
+        //Create function that sets current tenant function
+        SetCurrentTenantIdFunctionProducer setCurrentTenantIdFunctionProducer = new SetCurrentTenantIdFunctionProducer();
+        setCurrentTenantIdFunctionDefinition = setCurrentTenantIdFunctionProducer.produce(new SetCurrentTenantIdFunctionProducerParameters("rls_set_current_tenant", VALID_CURRENT_TENANT_ID_PROPERTY_NAME, getSchema(), null));
+        sqlDefinitions.add(setCurrentTenantIdFunctionDefinition);
+
+        //TODO EnableRowLevelSecurityProducer
+        //TODO ForceRowLevelSecurityProducer - table owner
+        //TODO RLSPolicyProducer
+        //TODO TenantHasAuthoritiesFunctionProducer
     }
 }
