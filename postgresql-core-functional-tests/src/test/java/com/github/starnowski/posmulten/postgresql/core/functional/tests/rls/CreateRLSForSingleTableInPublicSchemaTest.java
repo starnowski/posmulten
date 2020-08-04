e1b4cef7687790e2bfc8fd35d933482466fae2ce
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-04 17:23:20 +0200

    #75 - Added tests for the INSERT statement. [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/rls/CreateRLSForSingleTableInPublicSchemaTest.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/rls/CreateRLSForSingleTableInPublicSchemaTest.java
@@ -22,11 +22,13 @@ import static com.github.starnowski.posmulten.postgresql.core.functional.tests.T
 import static com.github.starnowski.posmulten.postgresql.core.rls.DefaultRLSPolicyProducerParameters.builder;
 import static com.github.starnowski.posmulten.postgresql.core.rls.PermissionCommandPolicyEnum.ALL;
 import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.VALID_CURRENT_TENANT_ID_PROPERTY_NAME;
+import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.isAnyRecordExists;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
+import static org.testng.Assert.assertTrue;
 
 public class CreateRLSForSingleTableInPublicSchemaTest extends TestNGSpringContextWithoutGenericTransactionalSupportTests {
 
@@ -124,8 +126,17 @@ public class CreateRLSForSingleTableInPublicSchemaTest extends TestNGSpringConte
         assertThat(countRowsInTableWhere(getUsersTableReference(), "id = " + user.getId())).isEqualTo(0);
     }
 
+    @Test(dataProvider = "userData", dependsOnMethods = {"tryToInsertPostForUserFromDifferentTenant"}, testName = "insert data into the users table assigned to the currently set", description = "test case assumes that row level security for users table is going to allow to insert data into the users table assigned to the current tenant")
+    public void insertUserTestData(Object[] parameters)
+    {
+        User user = (User) parameters[0];
+        assertThat(countRowsInTableWhere(getUsersTableReference(), "id = " + user.getId())).isEqualTo(0);
+        ownerJdbcTemplate.execute(format("%5$s INSERT INTO %4$s (id, name, tenant_id) VALUES (%1$d, '%2$s', '%3$s');", user.getId(), user.getName(), user.getTenantId(), getUsersTableReference(), setCurrentTenantIdFunctionDefinition.generateStatementThatSetTenant(user.getTenantId())));
+        assertTrue(isAnyRecordExists(jdbcTemplate, format("SELECT * FROM %4$s WHERE id = %1$d AND name = '%2$s' AND tenant_id = '%3$s'", user.getId(), user.getName(), user.getTenantId(), getUsersTableReference())), "The tests user should exists");
+    }
+
     @Override
-    @Test(dependsOnMethods = "tryToInsertPostForUserFromDifferentTenant", alwaysRun = true)
+    @Test(dependsOnMethods = { "tryToInsertPostForUserFromDifferentTenant", "insertUserTestData" }, alwaysRun = true)
     public void dropAllSQLDefinitions() {
         super.dropAllSQLDefinitions();
     }
