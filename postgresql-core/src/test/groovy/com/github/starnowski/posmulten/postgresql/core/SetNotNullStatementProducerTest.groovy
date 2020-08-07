9dab73acaca398f95024a60284bb65f58d5e7d90
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-07 12:33:28 +0200

    #76 - Changing the SetNotNullStatementProducer so that it would produce object of the SQLDefinition type. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducerTest.groovy
@@ -10,7 +10,7 @@ class SetNotNullStatementProducerTest extends Specification {
     @Unroll
     def "should return statement '#expectedStatement' for table '#table', schema '#schema' and column '#column'" () {
         expect:
-            tested.produce(new SetNotNullStatementProducerParameters(table, column, schema)) == expectedStatement
+            tested.produce(new SetNotNullStatementProducerParameters(table, column, schema)).getCreateScript() == expectedStatement
 
         where:
             table       |   column      | schema            ||  expectedStatement
@@ -26,6 +26,27 @@ class SetNotNullStatementProducerTest extends Specification {
             "groups"    |   "tenant_id" |   "secondary"     ||  "ALTER TABLE secondary.groups ALTER COLUMN tenant_id SET NOT NULL;"
             "users"     |   "col1"      |   "secondary"     ||  "ALTER TABLE secondary.users ALTER COLUMN col1 SET NOT NULL;"
             "groups"    |   "col1"      |   "secondary"     ||  "ALTER TABLE secondary.groups ALTER COLUMN col1 SET NOT NULL;"
+    }
+
+    @Unroll
+    def "should return statement '#expectedStatement' for table '#table', schema '#schema' and column '#column' that drops the NOT NULL constraint" () {
+        expect:
+            tested.produce(new SetNotNullStatementProducerParameters(table, column, schema)).getDropScript() == expectedStatement
+
+        where:
+            table       |   column      | schema            ||  expectedStatement
+            "users"     |   "tenant_id" |   null            ||  "ALTER TABLE users ALTER COLUMN tenant_id DROP NOT NULL;"
+            "groups"    |   "tenant_id" |   null            ||  "ALTER TABLE groups ALTER COLUMN tenant_id DROP NOT NULL;"
+            "users"     |   "col1"      |   null            ||  "ALTER TABLE users ALTER COLUMN col1 DROP NOT NULL;"
+            "groups"    |   "col1"      |   null            ||  "ALTER TABLE groups ALTER COLUMN col1 DROP NOT NULL;"
+            "users"     |   "tenant_id" |   "public"        ||  "ALTER TABLE public.users ALTER COLUMN tenant_id DROP NOT NULL;"
+            "groups"    |   "tenant_id" |   "public"        ||  "ALTER TABLE public.groups ALTER COLUMN tenant_id DROP NOT NULL;"
+            "users"     |   "col1"      |   "public"        ||  "ALTER TABLE public.users ALTER COLUMN col1 DROP NOT NULL;"
+            "groups"    |   "col1"      |   "public"        ||  "ALTER TABLE public.groups ALTER COLUMN col1 DROP NOT NULL;"
+            "users"     |   "tenant_id" |   "secondary"     ||  "ALTER TABLE secondary.users ALTER COLUMN tenant_id DROP NOT NULL;"
+            "groups"    |   "tenant_id" |   "secondary"     ||  "ALTER TABLE secondary.groups ALTER COLUMN tenant_id DROP NOT NULL;"
+            "users"     |   "col1"      |   "secondary"     ||  "ALTER TABLE secondary.users ALTER COLUMN col1 DROP NOT NULL;"
+            "groups"    |   "col1"      |   "secondary"     ||  "ALTER TABLE secondary.groups ALTER COLUMN col1 DROP NOT NULL;"
     }
 
     def "should throw exception of type 'IllegalArgumentException' when parameters object is null" ()
