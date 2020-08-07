a22184ec330e337b0986c030ec0d65e9b46c9b67
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-07 13:56:45 +0200

    #37 - Added the pojo type for the notifications table. [skip travis]

---
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/pojos/Notifications.java
@@ -0,0 +1,18 @@
+package com.github.starnowski.posmulten.postgresql.core.functional.tests.pojos;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@ToString
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Notifications {
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private String tenantId;
+}
