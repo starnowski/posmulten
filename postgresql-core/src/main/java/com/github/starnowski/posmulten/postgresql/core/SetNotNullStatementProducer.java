7f09a0f804494474525fb961ccfdd4a27f4dd801
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-07 12:36:01 +0200

    #76 - Changing the SetNotNullStatementProducer so that it would produce object of the SQLDefinition type. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/SetNotNullStatementProducer.java
@@ -1,7 +1,10 @@
 package com.github.starnowski.posmulten.postgresql.core;
 
+import com.github.starnowski.posmulten.postgresql.core.common.DefaultSQLDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+
 public class SetNotNullStatementProducer {
-    public String produce(ISetNotNullStatementProducerParameters parameters) {
+    public SQLDefinition produce(ISetNotNullStatementProducerParameters parameters) {
         if (parameters == null)
         {
             throw new IllegalArgumentException("The parameters object cannot be null");
@@ -20,6 +23,20 @@ public class SetNotNullStatementProducer {
         if (column.trim().isEmpty()) {
             throw new IllegalArgumentException("Column name cannot be blank");
         }
+        return new DefaultSQLDefinition(prepareCreateScript(parameters), prepareDropScript(parameters));
+    }
+
+    private String prepareDropScript(ISetNotNullStatementProducerParameters parameters) {
+        String table = parameters.getTable();
+        String column = parameters.getColumn();
+        String schema = parameters.getSchema();
+        String tableReference = schema == null ? table : schema + "." + table;
+        return "ALTER TABLE " + tableReference + " ALTER COLUMN " + column + " DROP NOT NULL;";
+    }
+
+    private String prepareCreateScript(ISetNotNullStatementProducerParameters parameters) {
+        String table = parameters.getTable();
+        String column = parameters.getColumn();
         String schema = parameters.getSchema();
         String tableReference = schema == null ? table : schema + "." + table;
         return "ALTER TABLE " + tableReference + " ALTER COLUMN " + column + " SET NOT NULL;";
