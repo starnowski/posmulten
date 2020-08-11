b1671de8b91bbc3a851a13f01b100523f1af0d3e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-11 22:00:51 +0200

    #77 - Added the builder type. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
@@ -0,0 +1,10 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+public class DefaultSharedSchemaContextBuilder {
+
+    public AbstractSharedSchemaContext build()
+    {
+        //TODO
+        return null;
+    }
+}
