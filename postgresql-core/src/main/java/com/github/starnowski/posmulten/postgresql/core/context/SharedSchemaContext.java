5f7520624d3d14c10be0337c3b5cf53de8f4b91e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-11 21:56:52 +0200

    #77 - Added basic methods. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SharedSchemaContext.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SharedSchemaContext.java
@@ -1,4 +1,35 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.EqualsCurrentTenantIdentifierFunctionInvocationFactory;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IGetCurrentTenantIdFunctionInvocationFactory;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.ISetCurrentTenantIdFunctionInvocationFactory;
+
+import java.util.List;
+
 public class SharedSchemaContext implements AbstractSharedSchemaContext {
+    @Override
+    public List<SQLDefinition> getSqlDefinitions() {
+        return null;
+    }
+
+    @Override
+    public void addSQLDefinition(SQLDefinition sqlDefinition) {
+
+    }
+
+    @Override
+    public EqualsCurrentTenantIdentifierFunctionInvocationFactory getEqualsCurrentTenantIdentifierFunctionInvocationFactory() {
+        return null;
+    }
+
+    @Override
+    public IGetCurrentTenantIdFunctionInvocationFactory getIGetCurrentTenantIdFunctionInvocationFactory() {
+        return null;
+    }
+
+    @Override
+    public ISetCurrentTenantIdFunctionInvocationFactory getISetCurrentTenantIdFunctionInvocationFactory() {
+        return null;
+    }
 }
