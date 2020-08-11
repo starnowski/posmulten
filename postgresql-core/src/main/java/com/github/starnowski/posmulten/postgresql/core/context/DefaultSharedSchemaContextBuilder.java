d6c6bd169c56c16cc9bd35a24375f0e2d6b28298
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-11 22:26:04 +0200

    #77 - Added basic methods. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
@@ -1,10 +1,25 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DefaultSharedSchemaContextBuilder {
+
+    private List<AbstractSharedSchemaContextEnricher> enrichers;
 
     public AbstractSharedSchemaContext build()
     {
         //TODO
         return null;
     }
+
+    public List<AbstractSharedSchemaContextEnricher> getEnrichers() {
+        return enrichers == null ? new ArrayList<>() : new ArrayList<>(enrichers);
+    }
+
+    public void setEnrichers(List<AbstractSharedSchemaContextEnricher> enrichers) {
+        this.enrichers = enrichers;
+    }
+
+
 }
