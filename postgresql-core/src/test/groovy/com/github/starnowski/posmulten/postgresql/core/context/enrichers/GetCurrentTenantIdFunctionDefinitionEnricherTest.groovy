620c7df15c899a330bfa480cda14230edb3d53f2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-12 22:03:31 +0200

    #77 - Added first test for the GetCurrentTenantIdFunctionDefinitionEnricher type. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/GetCurrentTenantIdFunctionDefinitionEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/GetCurrentTenantIdFunctionDefinitionEnricherTest.groovy
@@ -1,11 +1,33 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers
 
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContext
+import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionDefinition
 import spock.lang.Specification
 
 class GetCurrentTenantIdFunctionDefinitionEnricherTest extends Specification {
 
-    def "should enrich shared schema context with sql definition for function that "()
-    {
+    def tested = new GetCurrentTenantIdFunctionDefinitionEnricher()
 
+    def "should enrich shared schema context with sql definition for function that returns current tenant id based on default values for shares schema context builder"()
+    {
+        given:
+            def sharedSchemaContextRequest = new DefaultSharedSchemaContextBuilder().getSharedSchemaContextRequest()
+            def context = new SharedSchemaContext()
+            def capturedParameters = null
+            def mockedSQLDefinition = Mock(GetCurrentTenantIdFunctionDefinition)
+            def producer = {
+                parameters ->
+                    capturedParameters = parameters
+                    mockedSQLDefinition
+            }
+            tested.setGetCurrentTenantIdFunctionProducer(producer)
+
+        when:
+            def result = tested.enrich(context, sharedSchemaContextRequest)
+
+        then:
+            result.getSqlDefinitions().contains(mockedSQLDefinition)
+            result.getIGetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
     }
 }
