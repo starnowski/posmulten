7306d60fcff5401a58611c5bd541117ddca9fc26
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-12 21:22:55 +0200

    #77 - Added the currentTenantIdProperty property. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SharedSchemaContextRequest.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SharedSchemaContextRequest.java
@@ -3,6 +3,16 @@ package com.github.starnowski.posmulten.postgresql.core.context;
 public class SharedSchemaContextRequest {
 
     private String defaultSchema;
+    private String currentTenantIdProperty = "posmulten.tenant_Id";
+
+
+    public String getCurrentTenantIdProperty() {
+        return currentTenantIdProperty;
+    }
+
+    public void setCurrentTenantIdProperty(String currentTenantIdProperty) {
+        this.currentTenantIdProperty = currentTenantIdProperty;
+    }
 
     public String getDefaultSchema() {
         return defaultSchema;
