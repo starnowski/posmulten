ebdfd2ac0bf88c06c3870b6384351553bb73c63c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-12 21:33:22 +0200

    #77 - Added first test (not yet finished!). [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/GetCurrentTenantIdFunctionDefinitionEnricherTest.groovy
@@ -0,0 +1,11 @@
+package com.github.starnowski.posmulten.postgresql.core.context.enrichers
+
+import spock.lang.Specification
+
+class GetCurrentTenantIdFunctionDefinitionEnricherTest extends Specification {
+
+    def "should enrich shared schema context with sql definition for function that "()
+    {
+
+    }
+}
