44e9309d676bd47429b84319803615bcb5b29a02
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-12 22:35:45 +0200

    #77 - Added enricher implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/GetCurrentTenantIdFunctionDefinitionEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/GetCurrentTenantIdFunctionDefinitionEnricher.java
@@ -3,7 +3,9 @@ package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContextEnricher;
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionDefinition;
 import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionProducer;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionProducerParameters;
 
 public class GetCurrentTenantIdFunctionDefinitionEnricher implements AbstractSharedSchemaContextEnricher {
 
@@ -11,7 +13,10 @@ public class GetCurrentTenantIdFunctionDefinitionEnricher implements AbstractSha
 
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
-        return null;
+        GetCurrentTenantIdFunctionDefinition sqlDefinition = getCurrentTenantIdFunctionProducer.produce(new GetCurrentTenantIdFunctionProducerParameters("get_current_tenant_id", request.getCurrentTenantIdProperty(), request.getDefaultSchema(), request.getCurrentTenantIdPropertyType()));
+        context.addSQLDefinition(sqlDefinition);
+        context.setIGetCurrentTenantIdFunctionInvocationFactory(sqlDefinition);
+        return context;
     }
 
     void setGetCurrentTenantIdFunctionProducer(GetCurrentTenantIdFunctionProducer getCurrentTenantIdFunctionProducer) {
