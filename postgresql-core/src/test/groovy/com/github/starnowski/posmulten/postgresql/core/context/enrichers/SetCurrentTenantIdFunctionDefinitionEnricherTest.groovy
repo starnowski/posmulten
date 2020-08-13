8d5994fa097d9b80c648732bf3e8b3e9582cc9b1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-13 08:21:15 +0200

    #82 - Fixed tests assertions. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/SetCurrentTenantIdFunctionDefinitionEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/SetCurrentTenantIdFunctionDefinitionEnricherTest.groovy
@@ -2,8 +2,7 @@ package com.github.starnowski.posmulten.postgresql.core.context.enrichers
 
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContext
-import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionDefinition
-import com.github.starnowski.posmulten.postgresql.core.rls.function.GetCurrentTenantIdFunctionProducer
+import com.github.starnowski.posmulten.postgresql.core.rls.function.SetCurrentTenantIdFunctionDefinition
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -14,70 +13,70 @@ class SetCurrentTenantIdFunctionDefinitionEnricherTest extends Specification {
     def "should enrich shared schema context with sql definition for function that sets current tenant id based on default values for shares schema context builder"()
     {
         given:
-        def sharedSchemaContextRequest = new DefaultSharedSchemaContextBuilder().getSharedSchemaContextRequest()
-        def context = new SharedSchemaContext()
-        def capturedParameters = null
-        def mockedSQLDefinition = Mock(GetCurrentTenantIdFunctionDefinition)
-        def producer = Mock(GetCurrentTenantIdFunctionProducer)
-        tested.setGetCurrentTenantIdFunctionProducer(producer)
+            def sharedSchemaContextRequest = new DefaultSharedSchemaContextBuilder().getSharedSchemaContextRequest()
+            def context = new SharedSchemaContext()
+            def capturedParameters = null
+            def mockedSQLDefinition = Mock(SetCurrentTenantIdFunctionDefinition)
+            def producer = Mock(SetCurrentTenantIdFunctionProducer)
+            tested.setSetCurrentTenantIdFunctionProducer(producer)
 
         when:
-        def result = tested.enrich(context, sharedSchemaContextRequest)
+            def result = tested.enrich(context, sharedSchemaContextRequest)
 
         then:
-        1 * producer.produce(_) >>  {
-            parameters ->
-                capturedParameters = parameters[0]
-                mockedSQLDefinition
-        }
-        result.getSqlDefinitions().contains(mockedSQLDefinition)
-        result.getIGetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
+            1 * producer.produce(_) >>  {
+                parameters ->
+                    capturedParameters = parameters[0]
+                    mockedSQLDefinition
+            }
+            result.getSqlDefinitions().contains(mockedSQLDefinition)
+            result.getIGetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
 
         and: "passed parameters should match default values"
-        capturedParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
-        capturedParameters.getCurrentTenantIdProperty() == sharedSchemaContextRequest.getCurrentTenantIdProperty()
-        capturedParameters.getFunctionReturnType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
-        capturedParameters.getFunctionName() == "get_current_tenant_id"
+                capturedParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
+                capturedParameters.getCurrentTenantIdProperty() == sharedSchemaContextRequest.getCurrentTenantIdProperty()
+                capturedParameters.getFunctionReturnType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
+                capturedParameters.getFunctionName() == "set_current_tenant_id"
     }
 
     @Unroll
     def "should enrich shared schema context with sql definition for function that sets current tenant id based on defined values for builder, defaultSchema: #defaultSchema, currentTenantIdProperty #urrentTenantIdProperty, currentTenantIdPropertyType #currentTenantIdPropertyType, function name: #functionName"()
     {
         given:
-        def builder = new DefaultSharedSchemaContextBuilder()
-        builder.setDefaultSchema(defaultSchema)
-        builder.setCurrentTenantIdProperty(currentTenantIdProperty)
-        builder.setCurrentTenantIdPropertyType(currentTenantIdPropertyType)
-        builder.setGetCurrentTenantIdFunctionName(functionName)
-        def sharedSchemaContextRequest = builder.getSharedSchemaContextRequest()
-        def context = new SharedSchemaContext()
-        def capturedParameters = null
-        def mockedSQLDefinition = Mock(GetCurrentTenantIdFunctionDefinition)
-        def producer = Mock(GetCurrentTenantIdFunctionProducer)
-        tested.setGetCurrentTenantIdFunctionProducer(producer)
+            def builder = new DefaultSharedSchemaContextBuilder()
+            builder.setDefaultSchema(defaultSchema)
+            builder.setCurrentTenantIdProperty(currentTenantIdProperty)
+            builder.setCurrentTenantIdPropertyType(currentTenantIdPropertyType)
+            builder.setSetCurrentTenantIdFunctionName(functionName)
+            def sharedSchemaContextRequest = builder.getSharedSchemaContextRequest()
+            def context = new SharedSchemaContext()
+            def capturedParameters = null
+            def mockedSQLDefinition = Mock(SetCurrentTenantIdFunctionDefinition)
+            def producer = Mock(SetCurrentTenantIdFunctionProducer)
+            tested.setSetCurrentTenantIdFunctionProducer(producer)
 
         when:
-        def result = tested.enrich(context, sharedSchemaContextRequest)
+            def result = tested.enrich(context, sharedSchemaContextRequest)
 
         then:
-        1 * producer.produce(_) >>  {
-            parameters ->
-                capturedParameters = parameters[0]
-                mockedSQLDefinition
-        }
-        result.getSqlDefinitions().contains(mockedSQLDefinition)
-        result.getIGetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
+            1 * producer.produce(_) >>  {
+                parameters ->
+                    capturedParameters = parameters[0]
+                    mockedSQLDefinition
+            }
+            result.getSqlDefinitions().contains(mockedSQLDefinition)
+            result.getIGetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
 
         and: "passed parameters should match default values"
-        capturedParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
-        capturedParameters.getCurrentTenantIdProperty() == sharedSchemaContextRequest.getCurrentTenantIdProperty()
-        capturedParameters.getFunctionReturnType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
-        capturedParameters.getFunctionName() == functionName
+            capturedParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
+            capturedParameters.getCurrentTenantIdProperty() == sharedSchemaContextRequest.getCurrentTenantIdProperty()
+            capturedParameters.getFunctionReturnType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
+            capturedParameters.getFunctionName() == functionName
 
         where:
-        defaultSchema   |   currentTenantIdProperty |   currentTenantIdPropertyType |   functionName
-        null            |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
-        "public"        |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
-        "some_sche1"    |   "posmulte.prop.tenant"  |   "Some_SQL_TYPE"             |   "get_tenant_id"
+            defaultSchema   |   currentTenantIdProperty |   currentTenantIdPropertyType |   functionName
+            null            |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
+            "public"        |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
+            "some_sche1"    |   "posmulte.prop.tenant"  |   "Some_SQL_TYPE"             |   "get_tenant_id"
     }
 }
