9f476444d07d0bd374b029082c71864ec260b4c0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-13 21:20:06 +0200

    #84 - Added tests for passing parameters. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantHasAuthoritiesFunctionDefinitionEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantHasAuthoritiesFunctionDefinitionEnricherTest.groovy
@@ -8,6 +8,7 @@ import com.github.starnowski.posmulten.postgresql.core.rls.function.IGetCurrentT
 import com.github.starnowski.posmulten.postgresql.core.rls.function.TenantHasAuthoritiesFunctionDefinition
 import com.github.starnowski.posmulten.postgresql.core.rls.function.TenantHasAuthoritiesFunctionProducer
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class TenantHasAuthoritiesFunctionDefinitionEnricherTest extends Specification {
 
@@ -68,46 +69,66 @@ class TenantHasAuthoritiesFunctionDefinitionEnricherTest extends Specification {
             capturedTenantHasAuthoritiesFunctionProducerParameters.getSchemaArgumentType() == null
     }
 
-//    @Unroll
-//    def "should enrich shared schema context with sql definition for function that passed tenant id is equal to current tenant id based on defined values for builder, defaultSchema: #defaultSchema, currentTenantIdProperty #urrentTenantIdProperty, currentTenantIdPropertyType #currentTenantIdPropertyType, function name: #functionName"()
-//    {
-//        given:
-//        def builder = new DefaultSharedSchemaContextBuilder()
-//        builder.setDefaultSchema(defaultSchema)
-//        builder.setCurrentTenantIdProperty(currentTenantIdProperty)
-//        builder.setCurrentTenantIdPropertyType(currentTenantIdPropertyType)
-//        builder.setSetCurrentTenantIdFunctionName(functionName)
-//        def sharedSchemaContextRequest = builder.getSharedSchemaContextRequest()
-//        def context = new SharedSchemaContext()
-//        def capturedParameters = null
-//        def mockedSQLDefinition = Mock(SetCurrentTenantIdFunctionDefinition)
-//        def equalsCurrentTenantIdentifierFunctionProducer = Mock(EqualsCurrentTenantIdentifierFunctionProducer)
-//        def tenantHasAuthoritiesFunctionProducer = Mock(TenantHasAuthoritiesFunctionProducer)
-//        tested.setEqualsCurrentTenantIdentifierFunctionProducer(equalsCurrentTenantIdentifierFunctionProducer)
-//        tested.setTenantHasAuthoritiesFunctionProducer(tenantHasAuthoritiesFunctionProducer)
-//
-//        when:
-//        def result = tested.enrich(context, sharedSchemaContextRequest)
-//
-//        then:
-//        1 * producer.produce(_) >>  {
-//            parameters ->
-//                capturedParameters = parameters[0]
-//                mockedSQLDefinition
-//        }
-//        result.getSqlDefinitions().contains(mockedSQLDefinition)
-//        result.getISetCurrentTenantIdFunctionInvocationFactory().is(mockedSQLDefinition)
-//
-//        and: "passed parameters should match default values"
-//        capturedParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
-//        capturedParameters.getCurrentTenantIdProperty() == sharedSchemaContextRequest.getCurrentTenantIdProperty()
-//        capturedParameters.getArgumentType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
-//        capturedParameters.getFunctionName() == functionName
-//
-//        where:
-//        defaultSchema   |   currentTenantIdProperty |   currentTenantIdPropertyType |   functionName
-//        null            |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
-//        "public"        |   "c.tenant_id"           |   "text"                      |   "what_is_tenant_id"
-//        "some_sche1"    |   "posmulte.prop.tenant"  |   "Some_SQL_TYPE"             |   "get_tenant_id"
-//    }
+    @Unroll
+    def "should enrich shared schema context with sql definition for function that passed tenant id is equal to current tenant id based on defined values for builder, defaultSchema: #defaultSchema, currentTenantIdProperty #urrentTenantIdProperty, currentTenantIdPropertyType #currentTenantIdPropertyType, equalsCurrentTenantIdentifierFunctionName: #equalsCurrentTenantIdentifierFunctionName, tenantHasAuthoritiesFunctionName: #tenantHasAuthoritiesFunctionName"()
+    {
+        given:
+            def sharedSchemaContextRequest = new DefaultSharedSchemaContextBuilder().getSharedSchemaContextRequest()
+            def context = new SharedSchemaContext()
+            def capturedEqualsCurrentTenantIdentifierFunctionProducerParameters = null
+            def capturedTenantHasAuthoritiesFunctionProducerParameters = null
+            def getCurrentTenantIdFunctionInvocationFactory = Mock(IGetCurrentTenantIdFunctionInvocationFactory)
+            def mockedEqualsCurrentTenantIdentifierFunctionDefinition = Mock(EqualsCurrentTenantIdentifierFunctionDefinition)
+            def mockedTenantHasAuthoritiesFunctionDefinition = Mock(TenantHasAuthoritiesFunctionDefinition)
+            def equalsCurrentTenantIdentifierFunctionProducer = Mock(EqualsCurrentTenantIdentifierFunctionProducer)
+            def tenantHasAuthoritiesFunctionProducer = Mock(TenantHasAuthoritiesFunctionProducer)
+            tested.setEqualsCurrentTenantIdentifierFunctionProducer(equalsCurrentTenantIdentifierFunctionProducer)
+            tested.setTenantHasAuthoritiesFunctionProducer(tenantHasAuthoritiesFunctionProducer)
+            context.setIGetCurrentTenantIdFunctionInvocationFactory(getCurrentTenantIdFunctionInvocationFactory)
+
+        when:
+            def result = tested.enrich(context, sharedSchemaContextRequest)
+
+        then:
+            1 * equalsCurrentTenantIdentifierFunctionProducer.produce(_) >>  {
+                parameters ->
+                    capturedEqualsCurrentTenantIdentifierFunctionProducerParameters = parameters[0]
+                    mockedEqualsCurrentTenantIdentifierFunctionDefinition
+            }
+            result.getSqlDefinitions().contains(mockedEqualsCurrentTenantIdentifierFunctionDefinition)
+
+        then:
+            1 * tenantHasAuthoritiesFunctionProducer.produce(_) >>  {
+                parameters ->
+                    capturedTenantHasAuthoritiesFunctionProducerParameters = parameters[0]
+                    mockedTenantHasAuthoritiesFunctionDefinition
+            }
+            result.getSqlDefinitions().contains(mockedTenantHasAuthoritiesFunctionDefinition)
+            result.getTenantHasAuthoritiesFunctionInvocationFactory().is(mockedTenantHasAuthoritiesFunctionDefinition)
+
+        and: "generated sql definitions should be added in correct order"
+            result.getSqlDefinitions() == [mockedEqualsCurrentTenantIdentifierFunctionDefinition, mockedTenantHasAuthoritiesFunctionDefinition]
+
+        and: "passed parameters should match default values"
+            capturedEqualsCurrentTenantIdentifierFunctionProducerParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
+            capturedEqualsCurrentTenantIdentifierFunctionProducerParameters.getArgumentType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
+            capturedEqualsCurrentTenantIdentifierFunctionProducerParameters.getCurrentTenantIdFunctionInvocationFactory() == getCurrentTenantIdFunctionInvocationFactory
+            capturedEqualsCurrentTenantIdentifierFunctionProducerParameters.getFunctionName() == "is_id_equals_current_tenant_id"
+
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getSchema() == sharedSchemaContextRequest.getDefaultSchema()
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getFunctionName() == "tenant_has_authorities"
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getEqualsCurrentTenantIdentifierFunctionInvocationFactory() == mockedEqualsCurrentTenantIdentifierFunctionDefinition
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getTenantIdArgumentType() == null
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getPermissionCommandPolicyArgumentType() == null
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getRLSExpressionArgumentType() == null
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getTableArgumentType() == null
+            capturedTenantHasAuthoritiesFunctionProducerParameters.getSchemaArgumentType() == null
+
+        where:
+            defaultSchema   |   currentTenantIdPropertyType |   equalsCurrentTenantIdentifierFunctionName   |   tenantHasAuthoritiesFunctionName    ||  expectedEqualsCurrentTenantIdentifierFunctionName   |   expectedTenantHasAuthoritiesFunctionName
+            null            |   "text"                      |   "is_tenant_equal"                           |   "t_h_a"                             ||  "is_tenant_equal"                                   |   "t_h_a"
+            "public"        |   "text"                      |   null                                        |   "t_h_a"                             ||  "is_id_equals_current_tenant_id"                    |   "t_h_a"
+            "some_sche1"    |   "Some_SQL_TYPE"             |   "is_tenant_equal"                           |   null                                ||  "is_tenant_equal"                                   |   "tenant_has_authorities"
+    }
+
 }
