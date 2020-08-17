8c6b1d0fbbfaa0f9c01c82ccc61d105a566c1cfc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 22:59:38 +0200

    #90 - Added unit tests that check if the SQL definition which force RLS policy is also being added. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/TableRLSSettingsSQLDefinitionsProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/TableRLSSettingsSQLDefinitionsProducerTest.groovy
@@ -38,6 +38,34 @@ class TableRLSSettingsSQLDefinitionsProducerTest extends Specification {
             tk("some_tab", "other_schema")  |   _
     }
 
+    @Unroll
+    def "should create all required SQL definition that enables RLS policy and force policy for table owner in table #tableKey"()
+    {
+        given:
+            def enableRowLevelSecurityProducer = Mock(EnableRowLevelSecurityProducer)
+            def forceRowLevelSecurityProducer = Mock(ForceRowLevelSecurityProducer)
+            def enableRowLevelSecurityProducerSQLDefinition = Mock(SQLDefinition)
+            def forceRowLevelSecurityProducerSQLDefinition = Mock(SQLDefinition)
+            tested.setEnableRowLevelSecurityProducer(enableRowLevelSecurityProducer)
+            tested.setForceRowLevelSecurityProducer(forceRowLevelSecurityProducer)
+
+        when:
+            def results = tested.produce(tableKey, true)
+
+        then:
+            1 * enableRowLevelSecurityProducer.produce(tableKey.getTable(), tableKey.getSchema()) >> enableRowLevelSecurityProducerSQLDefinition
+            1 * forceRowLevelSecurityProducer.produce(tableKey.getTable(), tableKey.getSchema()) >> enableRowLevelSecurityProducerSQLDefinition
+            results == [enableRowLevelSecurityProducerSQLDefinition, forceRowLevelSecurityProducerSQLDefinition]
+
+        where:
+            tableKey                        |   _
+            tk("users", null)               |   _
+            tk("users", "public")           |   _
+            tk("users", "other_schema")     |   _
+            tk("some_tab", "other_schema")  |   _
+            tk("some_tab", "other_schema")  |   _
+    }
+
     TableKey tk(String table, String schema)
     {
         new TableKey(table, schema)
