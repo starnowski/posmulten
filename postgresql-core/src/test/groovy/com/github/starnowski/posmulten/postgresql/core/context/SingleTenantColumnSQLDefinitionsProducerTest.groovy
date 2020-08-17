e38f9404c3d5cda7e7da76c195032f6596ac8fb6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 19:11:29 +0200

    #88 - Added basic tests. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducerTest.groovy
@@ -4,18 +4,29 @@ import com.github.starnowski.posmulten.postgresql.core.CreateColumnStatementProd
 import com.github.starnowski.posmulten.postgresql.core.SetDefaultStatementProducer
 import com.github.starnowski.posmulten.postgresql.core.SetNotNullStatementProducer
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SingleTenantColumnSQLDefinitionsProducerTest extends Specification {
 
     def tested = new SingleTenantColumnSQLDefinitionsProducer()
 
-    def ""()
+    @Unroll
+    def "should create all required SQL definition for the tenant column"()
     {
-        def createColumnStatementProducer = Mock(CreateColumnStatementProducer)
-        def setDefaultStatementProducer = Mock(SetDefaultStatementProducer)
-        def setNotNullStatementProducer = Mock(SetNotNullStatementProducer)
-        tested.setCreateColumnStatementProducer(createColumnStatementProducer)
-        tested.setSetDefaultStatementProducer(setDefaultStatementProducer)
-        tested.setSetNotNullStatementProducer(setNotNullStatementProducer)
+        given:
+            def createColumnStatementProducer = Mock(CreateColumnStatementProducer)
+            def setDefaultStatementProducer = Mock(SetDefaultStatementProducer)
+            def setNotNullStatementProducer = Mock(SetNotNullStatementProducer)
+            tested.setCreateColumnStatementProducer(createColumnStatementProducer)
+            tested.setSetDefaultStatementProducer(setDefaultStatementProducer)
+            tested.setSetNotNullStatementProducer(setNotNullStatementProducer)
+
+        when:
+
+
+        then:
+            tenantTable |   tenantColumn    |   defaultTenantColumn |   defaultTenantColumnType ||  expectedTenantColumn    |   expectedTenantColumnType
+            "users"     |   "tenant_id"     |   "tenant"            |   "VARCHAR(255)"          ||  "tenant_id"             |   "VARCHAR(255)"
+
     }
 }
