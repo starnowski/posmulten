75b820e445b6455a4a2195df88aac72df930daac
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 18:54:21 +0200

    #88 - Added setters methods. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducer.java
@@ -1,14 +1,34 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+import com.github.starnowski.posmulten.postgresql.core.CreateColumnStatementProducer;
+import com.github.starnowski.posmulten.postgresql.core.SetDefaultStatementProducer;
+import com.github.starnowski.posmulten.postgresql.core.SetNotNullStatementProducer;
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
 
 import java.util.List;
 
 public class SingleTenantColumnSQLDefinitionsProducer {
 
+    private CreateColumnStatementProducer createColumnStatementProducer = new CreateColumnStatementProducer();
+    private SetDefaultStatementProducer setDefaultStatementProducer = new SetDefaultStatementProducer();
+    private SetNotNullStatementProducer setNotNullStatementProducer = new SetNotNullStatementProducer();
+
     public List<SQLDefinition> produce(TableKey tableKey, AbstractTableColumns tableColumns, String defaultTenantColumn, String defaultTenantColumnType)
     {
         //TODO
         return null;
     }
+
+    public void setCreateColumnStatementProducer(CreateColumnStatementProducer createColumnStatementProducer) {
+        this.createColumnStatementProducer = createColumnStatementProducer;
+    }
+
+    public void setSetDefaultStatementProducer(SetDefaultStatementProducer setDefaultStatementProducer) {
+        this.setDefaultStatementProducer = setDefaultStatementProducer;
+    }
+
+    public void setSetNotNullStatementProducer(SetNotNullStatementProducer setNotNullStatementProducer) {
+        this.setNotNullStatementProducer = setNotNullStatementProducer;
+    }
+
 }
