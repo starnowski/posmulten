ec7cc94030f6a5b67a6c7a40da978da288fc8b09
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 23:59:33 +0200

    #90 - Code styling. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TableRLSSettingsSQLDefinitionsEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TableRLSSettingsSQLDefinitionsEnricherTest.groovy
@@ -59,44 +59,44 @@ class TableRLSSettingsSQLDefinitionsEnricherTest extends Specification {
     def "should create all required SQL definition that enables RLS policy and force policy for schema #schema"()
     {
         given:
-        def builder = new DefaultSharedSchemaContextBuilder()
-        builder.createRLSPolicyForColumn("users", [:], "tenant", "user_policy")
-        builder.createRLSPolicyForColumn("comments", [:], "tenant_id", "comments_policy")
-        builder.createRLSPolicyForColumn("some_table", [:], "tenant_xxx_id", "some_table_policy")
-        builder.setForceRowLevelSecurityForTableOwner(true)
-        def sharedSchemaContextRequest = builder.getSharedSchemaContextRequest()
-        def context = new SharedSchemaContext()
-        def usersTableSQLDefinition1 = Mock(SQLDefinition)
-        def usersTableSQLDefinition2 = Mock(SQLDefinition)
-        def commentsTableSQLDefinition1 = Mock(SQLDefinition)
-        def someTableSQLDefinition1 = Mock(SQLDefinition)
-        def someTableSQLDefinition2 = Mock(SQLDefinition)
-        def tableRLSSettingsSQLDefinitionsProducer = Mock(TableRLSSettingsSQLDefinitionsProducer)
-        tested.setTableRLSSettingsSQLDefinitionsProducer(tableRLSSettingsSQLDefinitionsProducer)
-        def usersTableKey = tk("users", null)
-        def commentsTableKey = tk("comments", null)
-        def someTableKey = tk("some_table", null)
+            def builder = new DefaultSharedSchemaContextBuilder()
+            builder.createRLSPolicyForColumn("users", [:], "tenant", "user_policy")
+            builder.createRLSPolicyForColumn("comments", [:], "tenant_id", "comments_policy")
+            builder.createRLSPolicyForColumn("some_table", [:], "tenant_xxx_id", "some_table_policy")
+            builder.setForceRowLevelSecurityForTableOwner(true)
+            def sharedSchemaContextRequest = builder.getSharedSchemaContextRequest()
+            def context = new SharedSchemaContext()
+            def usersTableSQLDefinition1 = Mock(SQLDefinition)
+            def usersTableSQLDefinition2 = Mock(SQLDefinition)
+            def commentsTableSQLDefinition1 = Mock(SQLDefinition)
+            def someTableSQLDefinition1 = Mock(SQLDefinition)
+            def someTableSQLDefinition2 = Mock(SQLDefinition)
+            def tableRLSSettingsSQLDefinitionsProducer = Mock(TableRLSSettingsSQLDefinitionsProducer)
+            tested.setTableRLSSettingsSQLDefinitionsProducer(tableRLSSettingsSQLDefinitionsProducer)
+            def usersTableKey = tk("users", null)
+            def commentsTableKey = tk("comments", null)
+            def someTableKey = tk("some_table", null)
 
         when:
-        def result = tested.enrich(context, sharedSchemaContextRequest)
+            def result = tested.enrich(context, sharedSchemaContextRequest)
 
         then:
-        1 * tableRLSSettingsSQLDefinitionsProducer.produce(usersTableKey, true) >> [usersTableSQLDefinition1, usersTableSQLDefinition2]
-        1 * tableRLSSettingsSQLDefinitionsProducer.produce(commentsTableKey, true) >> [commentsTableSQLDefinition1]
-        1 * tableRLSSettingsSQLDefinitionsProducer.produce(someTableKey, true) >> [someTableSQLDefinition1, someTableSQLDefinition2]
+            1 * tableRLSSettingsSQLDefinitionsProducer.produce(usersTableKey, true) >> [usersTableSQLDefinition1, usersTableSQLDefinition2]
+            1 * tableRLSSettingsSQLDefinitionsProducer.produce(commentsTableKey, true) >> [commentsTableSQLDefinition1]
+            1 * tableRLSSettingsSQLDefinitionsProducer.produce(someTableKey, true) >> [someTableSQLDefinition1, someTableSQLDefinition2]
 
-        result.getSqlDefinitions().contains(usersTableSQLDefinition1)
-        result.getSqlDefinitions().contains(usersTableSQLDefinition2)
-        result.getSqlDefinitions().contains(commentsTableSQLDefinition1)
-        result.getSqlDefinitions().contains(someTableSQLDefinition1)
-        result.getSqlDefinitions().contains(someTableSQLDefinition2)
+            result.getSqlDefinitions().contains(usersTableSQLDefinition1)
+            result.getSqlDefinitions().contains(usersTableSQLDefinition2)
+            result.getSqlDefinitions().contains(commentsTableSQLDefinition1)
+            result.getSqlDefinitions().contains(someTableSQLDefinition1)
+            result.getSqlDefinitions().contains(someTableSQLDefinition2)
 
         and: "sql definitions are added in order returned by component of type SingleTenantColumnSQLDefinitionsProducer"
-        result.getSqlDefinitions().indexOf(usersTableSQLDefinition1) < result.getSqlDefinitions().indexOf(usersTableSQLDefinition2)
-        result.getSqlDefinitions().indexOf(someTableSQLDefinition1) < result.getSqlDefinitions().indexOf(someTableSQLDefinition2)
+            result.getSqlDefinitions().indexOf(usersTableSQLDefinition1) < result.getSqlDefinitions().indexOf(usersTableSQLDefinition2)
+            result.getSqlDefinitions().indexOf(someTableSQLDefinition1) < result.getSqlDefinitions().indexOf(someTableSQLDefinition2)
 
         where:
-        schema << [null, "public", "some_schema"]
+            schema << [null, "public", "some_schema"]
     }
 
     TableKey tk(String table, String schema)
