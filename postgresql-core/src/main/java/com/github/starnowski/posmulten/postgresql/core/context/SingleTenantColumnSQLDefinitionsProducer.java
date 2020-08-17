7f9a4892087fe4e2eaa6842aad52f87e408f00b6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 20:34:17 +0200

    #88 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducer.java
@@ -1,10 +1,9 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
-import com.github.starnowski.posmulten.postgresql.core.CreateColumnStatementProducer;
-import com.github.starnowski.posmulten.postgresql.core.SetDefaultStatementProducer;
-import com.github.starnowski.posmulten.postgresql.core.SetNotNullStatementProducer;
+import com.github.starnowski.posmulten.postgresql.core.*;
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SingleTenantColumnSQLDefinitionsProducer {
@@ -15,8 +14,12 @@ public class SingleTenantColumnSQLDefinitionsProducer {
 
     public List<SQLDefinition> produce(TableKey tableKey, AbstractTableColumns tableColumns, String defaultTenantColumnValue, String defaultTenantColumn, String defaultTenantColumnType)
     {
-        //TODO
-        return null;
+        List<SQLDefinition> results = new ArrayList<>();
+        String tenantColumn = tableColumns.getTenantColumnName() == null ? defaultTenantColumn : tableColumns.getTenantColumnName();
+        results.add(createColumnStatementProducer.produce(new CreateColumnStatementProducerParameters(tableKey.getTable(), tenantColumn, defaultTenantColumnType, tableKey.getSchema())));
+        results.add(setDefaultStatementProducer.produce(new SetDefaultStatementProducerParameters(tableKey.getTable(), tenantColumn, defaultTenantColumnValue, tableKey.getSchema())));
+        results.add(setNotNullStatementProducer.produce(new SetNotNullStatementProducerParameters(tableKey.getTable(), tenantColumn, tableKey.getSchema())));
+        return results;
     }
 
     public void setCreateColumnStatementProducer(CreateColumnStatementProducer createColumnStatementProducer) {
