24f2824d3084cc9b6651cb58530f6a6a7ebdd655
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 18:45:04 +0200

    #88 - Added component that adds SQL definitions for the single table. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/SingleTenantColumnSQLDefinitionsProducer.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+
+import java.util.List;
+
+public class SingleTenantColumnSQLDefinitionsProducer {
+
+    public List<SQLDefinition> produce(TableKey tableKey, AbstractTableColumns tableColumns, String defaultTenantColumn, String defaultTenantColumnType)
+    {
+        //TODO
+        return null;
+    }
+}
