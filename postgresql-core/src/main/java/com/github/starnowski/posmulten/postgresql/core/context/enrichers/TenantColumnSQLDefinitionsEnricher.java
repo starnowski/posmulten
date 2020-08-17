de6854dc3ae31ee627074e6a728a7541a5f444c3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-17 21:05:52 +0200

    #88 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantColumnSQLDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantColumnSQLDefinitionsEnricher.java
@@ -3,11 +3,19 @@ package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContextEnricher;
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.context.SingleTenantColumnSQLDefinitionsProducer;
 
 public class TenantColumnSQLDefinitionsEnricher implements AbstractSharedSchemaContextEnricher {
+
+    private SingleTenantColumnSQLDefinitionsProducer singleTenantColumnSQLDefinitionsProducer = new SingleTenantColumnSQLDefinitionsProducer();
+
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
         //TODO Add exception for non-existed table declaration for which rls policy should be created
         return null;
+    }
+
+    void setSingleTenantColumnSQLDefinitionsProducer(SingleTenantColumnSQLDefinitionsProducer singleTenantColumnSQLDefinitionsProducer) {
+        this.singleTenantColumnSQLDefinitionsProducer = singleTenantColumnSQLDefinitionsProducer;
     }
 }
