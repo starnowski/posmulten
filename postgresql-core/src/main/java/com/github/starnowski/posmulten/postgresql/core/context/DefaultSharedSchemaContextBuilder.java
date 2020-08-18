5ed1fc252b7a39967c1adc5d7ffd4b7afe8db181
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 22:19:19 +0200

    #92 - Setting the defaultTenantIdColumn property. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
@@ -95,4 +95,9 @@ public class DefaultSharedSchemaContextBuilder {
         return this;
     }
 
+    public DefaultSharedSchemaContextBuilder setDefaultTenantIdColumn(String defaultTenantIdColumn) {
+        sharedSchemaContextRequest.setDefaultTenantIdColumn(defaultTenantIdColumn);
+        return this;
+    }
+
 }
