4bde8dbfc2942a62ddfa544335c2c3e14848f374
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 20:49:46 +0200

    #92 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducer.java
@@ -6,6 +6,9 @@ import com.github.starnowski.posmulten.postgresql.core.rls.RLSPolicyProducer;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.starnowski.posmulten.postgresql.core.rls.DefaultRLSPolicyProducerParameters.builder;
+import static com.github.starnowski.posmulten.postgresql.core.rls.PermissionCommandPolicyEnum.ALL;
+
 public class TableRLSPolicySQLDefinitionsProducer {
 
     private RLSPolicyProducer rlsPolicyProducer = new RLSPolicyProducer();
@@ -13,6 +16,16 @@ public class TableRLSPolicySQLDefinitionsProducer {
     public List<SQLDefinition> produce(AbstractTableRLSPolicySQLDefinitionsProducerParameters parameters)
     {
         List<SQLDefinition> results = new ArrayList<>();
+        SQLDefinition sqlDefinition = rlsPolicyProducer.produce(builder().withPolicyName(parameters.getPolicyName())
+                .withPolicySchema(parameters.getTableKey().getSchema())
+                .withPolicyTable(parameters.getTableKey().getTable())
+                .withGrantee(parameters.getGrantee())
+                .withPermissionCommandPolicy(ALL)
+                .withUsingExpressionTenantHasAuthoritiesFunctionInvocationFactory(parameters.getTenantHasAuthoritiesFunctionInvocationFactory())
+                .withWithCheckExpressionTenantHasAuthoritiesFunctionInvocationFactory(parameters.getTenantHasAuthoritiesFunctionInvocationFactory())
+                .withTenantIdColumn(parameters.getTenantIdColumn())
+                .build());
+        results.add(sqlDefinition);
         return results;
     }
 
