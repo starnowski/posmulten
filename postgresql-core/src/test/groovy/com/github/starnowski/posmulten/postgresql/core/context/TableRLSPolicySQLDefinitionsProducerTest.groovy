9e2cf8cb713ef6728e0a1a762187d0d706fcad46
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 20:07:20 +0200

    #92 - Added basic unit tests. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducerTest.groovy
@@ -0,0 +1,46 @@
+package com.github.starnowski.posmulten.postgresql.core.context
+
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
+import com.github.starnowski.posmulten.postgresql.core.rls.RLSPolicyProducer
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class TableRLSPolicySQLDefinitionsProducerTest extends Specification {
+
+    def tested = new TableRLSPolicySQLDefinitionsProducer()
+
+    @Unroll
+    def "should create all required SQL definition that creates RLS policy for table #tableKey, grantee #grantee witn policy name #policyName"()
+    {
+        given:
+            def rlsPolicyProducer = Mock(RLSPolicyProducer)
+            def rlsPolicyProducerSQLDefinition = Mock(SQLDefinition)
+            def tenantHasAuthoritiesFunctionInvocationFactory = Mock(TenantHasAuthoritiesFunctionInvocationFactory)
+            tested.setRlsPolicyProducer(rlsPolicyProducer)
+            def parameters = new TableRLSPolicySQLDefinitionsProducerParameters.TableRLSPolicySQLDefinitionsProducerParametersBuilder()
+                    .withGrantee(grantee)
+                    .withTableKey(tableKey)
+                    .withPolicyName(policyName)
+                    .withTenantHasAuthoritiesFunctionInvocationFactory(tenantHasAuthoritiesFunctionInvocationFactory)
+                    .build()
+
+        when:
+            def results = tested.produce(tableKey, false)
+
+        then:
+            1 * rlsPolicyProducer.produce(tableKey.getTable(), tableKey.getSchema()) >> rlsPolicyProducerSQLDefinition
+            results == [rlsPolicyProducerSQLDefinition]
+
+        where:
+            tableKey                |   grantee     |   policyName
+            tk("users", null)       |   "core-user" |   "some_rls_policy"
+            tk("users", "public")   |   "owner"     |   "some_rls_policy"
+            tk("posts", "public")   |   "owner"     |   "posts_policy"
+            tk("posts", "some_sh")  |   "owner"     |   "posts_policy"
+    }
+
+    TableKey tk(String table, String schema)
+    {
+        new TableKey(table, schema)
+    }
+}
