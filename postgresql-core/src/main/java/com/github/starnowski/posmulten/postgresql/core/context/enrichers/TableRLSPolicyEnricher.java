1e88d924a5cce6c3778e11230aafc33774e4a561
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 23:00:27 +0200

    #92 - Added implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TableRLSPolicyEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TableRLSPolicyEnricher.java
@@ -1,9 +1,6 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContext;
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContextEnricher;
-import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
-import com.github.starnowski.posmulten.postgresql.core.context.TableRLSPolicySQLDefinitionsProducer;
+import com.github.starnowski.posmulten.postgresql.core.context.*;
 
 public class TableRLSPolicyEnricher implements AbstractSharedSchemaContextEnricher {
 
@@ -11,7 +8,20 @@ public class TableRLSPolicyEnricher implements AbstractSharedSchemaContextEnrich
 
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
-        return null;
+        request.getTableColumnsList().entrySet().forEach(entry ->
+        {
+            TableRLSPolicySQLDefinitionsProducerParameters.TableRLSPolicySQLDefinitionsProducerParametersBuilder builder = new TableRLSPolicySQLDefinitionsProducerParameters.TableRLSPolicySQLDefinitionsProducerParametersBuilder();
+            TableRLSPolicySQLDefinitionsProducerParameters parameters = builder
+                    .withDefaultTenantIdColumn(request.getDefaultTenantIdColumn())
+                    .withGrantee(request.getGrantee())
+                    .withPolicyName(request.getTableRLSPolicies().get(entry.getKey()).getPolicyName())
+                    .withTableKey(entry.getKey())
+                    .withTenantHasAuthoritiesFunctionInvocationFactory(context.getTenantHasAuthoritiesFunctionInvocationFactory())
+                    .withTenantIdColumn(entry.getValue().getTenantColumnName())
+                    .build();
+            tableRLSPolicySQLDefinitionsProducer.produce(parameters).forEach(context::addSQLDefinition);
+        });
+        return context;
     }
 
     void setTableRLSPolicySQLDefinitionsProducer(TableRLSPolicySQLDefinitionsProducer tableRLSPolicySQLDefinitionsProducer) {
