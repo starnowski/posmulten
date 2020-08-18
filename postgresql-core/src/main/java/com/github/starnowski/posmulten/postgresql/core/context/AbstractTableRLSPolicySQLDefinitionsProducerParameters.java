4fa4b9a312f9d06badee30991bb548b6ce53e6ec
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 19:12:06 +0200

    #92 - Added the parameters types. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/AbstractTableRLSPolicySQLDefinitionsProducerParameters.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.rls.TenantHasAuthoritiesFunctionInvocationFactory;
+
+public interface AbstractTableRLSPolicySQLDefinitionsProducerParameters {
+
+    String getGrantee();
+
+    TableKey getTableKey();
+
+    String getPolicyName();
+
+    TenantHasAuthoritiesFunctionInvocationFactory getTenantHasAuthoritiesFunctionInvocationFactory();
+}
---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducerParameters.java
@@ -0,0 +1,67 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.rls.TenantHasAuthoritiesFunctionInvocationFactory;
+
+public class TableRLSPolicySQLDefinitionsProducerParameters {
+
+    private final String grantee;
+    private final TableKey tableKey;
+    private final String policyName;
+    private final TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory;
+
+    public TableRLSPolicySQLDefinitionsProducerParameters(String grantee, TableKey tableKey, String policyName, TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory) {
+        this.grantee = grantee;
+        this.tableKey = tableKey;
+        this.policyName = policyName;
+        this.tenantHasAuthoritiesFunctionInvocationFactory = tenantHasAuthoritiesFunctionInvocationFactory;
+    }
+
+    public String getGrantee() {
+        return grantee;
+    }
+
+    public TableKey getTableKey() {
+        return tableKey;
+    }
+
+    public String getPolicyName() {
+        return policyName;
+    }
+
+    public TenantHasAuthoritiesFunctionInvocationFactory getTenantHasAuthoritiesFunctionInvocationFactory() {
+        return tenantHasAuthoritiesFunctionInvocationFactory;
+    }
+
+    public static class TableRLSPolicySQLDefinitionsProducerParametersBuilder
+    {
+        private String grantee;
+        private TableKey tableKey;
+        private String policyName;
+        private TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory;
+
+        public TableRLSPolicySQLDefinitionsProducerParametersBuilder withGrantee(String grantee) {
+            this.grantee = grantee;
+            return this;
+        }
+
+        public TableRLSPolicySQLDefinitionsProducerParametersBuilder withTableKey(TableKey tableKey) {
+            this.tableKey = tableKey;
+            return this;
+        }
+
+        public TableRLSPolicySQLDefinitionsProducerParametersBuilder withPolicyName(String policyName) {
+            this.policyName = policyName;
+            return this;
+        }
+
+        public TableRLSPolicySQLDefinitionsProducerParametersBuilder withTenantHasAuthoritiesFunctionInvocationFactory(TenantHasAuthoritiesFunctionInvocationFactory tenantHasAuthoritiesFunctionInvocationFactory) {
+            this.tenantHasAuthoritiesFunctionInvocationFactory = tenantHasAuthoritiesFunctionInvocationFactory;
+            return this;
+        }
+
+        public TableRLSPolicySQLDefinitionsProducerParameters build()
+        {
+            return new TableRLSPolicySQLDefinitionsProducerParameters(grantee, tableKey, policyName, tenantHasAuthoritiesFunctionInvocationFactory);
+        }
+    }
+}
