92eee2bbac0ef1db3f548f696eac81383c2a0261
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 19:54:28 +0200

    #92 - Added the producer component type. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducer.java
@@ -0,0 +1,15 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableRLSPolicySQLDefinitionsProducer {
+
+    public List<SQLDefinition> produce(AbstractTableRLSPolicySQLDefinitionsProducerParameters parameters)
+    {
+        List<SQLDefinition> results = new ArrayList<>();
+        return results;
+    }
+}
