937fb286d2f127539b892281ac51809b7ec59d3e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 20:07:45 +0200

    #92 - Added the rlsPolicyProducer property. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducer.java
@@ -1,15 +1,22 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+import com.github.starnowski.posmulten.postgresql.core.rls.RLSPolicyProducer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TableRLSPolicySQLDefinitionsProducer {
 
+    private RLSPolicyProducer rlsPolicyProducer = new RLSPolicyProducer();
+
     public List<SQLDefinition> produce(AbstractTableRLSPolicySQLDefinitionsProducerParameters parameters)
     {
         List<SQLDefinition> results = new ArrayList<>();
         return results;
+    }
+
+    void setRlsPolicyProducer(RLSPolicyProducer rlsPolicyProducer) {
+        this.rlsPolicyProducer = rlsPolicyProducer;
     }
 }
