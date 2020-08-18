287c98a21ddcd5ef853a38ade0d15f1b0c0cc50c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-18 20:24:10 +0200

    #92 - Tests code refactoring. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/TableRLSPolicySQLDefinitionsProducerTest.groovy
@@ -1,7 +1,9 @@
 package com.github.starnowski.posmulten.postgresql.core.context
 
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
+import com.github.starnowski.posmulten.postgresql.core.rls.PermissionCommandPolicyEnum
 import com.github.starnowski.posmulten.postgresql.core.rls.RLSPolicyProducer
+import com.github.starnowski.posmulten.postgresql.core.rls.RLSPolicyProducerParameters
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -10,9 +12,10 @@ class TableRLSPolicySQLDefinitionsProducerTest extends Specification {
     def tested = new TableRLSPolicySQLDefinitionsProducer()
 
     @Unroll
-    def "should create all required SQL definition that creates RLS policy for table #tableKey, grantee #grantee witn policy name #policyName"()
+    def "should create all required SQL definition that creates RLS policy for table #tableKey, grantee #grantee with policy name #policyName"()
     {
         given:
+            RLSPolicyProducerParameters capturedParameters = null
             def rlsPolicyProducer = Mock(RLSPolicyProducer)
             def rlsPolicyProducerSQLDefinition = Mock(SQLDefinition)
             def tenantHasAuthoritiesFunctionInvocationFactory = Mock(TenantHasAuthoritiesFunctionInvocationFactory)
@@ -28,8 +31,21 @@ class TableRLSPolicySQLDefinitionsProducerTest extends Specification {
             def results = tested.produce(parameters)
 
         then:
-            1 * rlsPolicyProducer.produce(tableKey.getTable(), tableKey.getSchema()) >> rlsPolicyProducerSQLDefinition
+            1 * rlsPolicyProducer.produce(_) >>  {
+                    passedParameters ->
+                    capturedParameters = parameters[0]
+                    rlsPolicyProducerSQLDefinition
+            }
             results == [rlsPolicyProducerSQLDefinition]
+
+        and: "pass correct parameters"
+            capturedParameters.getGrantee() == grantee
+            capturedParameters.getPolicyTable() == tableKey.getTable()
+            capturedParameters.getPolicySchema() == tableKey.getSchema()
+            capturedParameters.getPolicyName() == policyName
+            capturedParameters.getUsingExpressionTenantHasAuthoritiesFunctionInvocationFactory() == tenantHasAuthoritiesFunctionInvocationFactory
+            capturedParameters.getWithCheckExpressionTenantHasAuthoritiesFunctionInvocationFactory() == tenantHasAuthoritiesFunctionInvocationFactory
+            capturedParameters.getPermissionCommandPolicy() == PermissionCommandPolicyEnum.ALL
 
         where:
             tableKey                |   grantee     |   policyName
