c60028bcc9a31bcc131daf79b2c95f405ec7d915
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-19 23:06:53 +0200

    #96 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IsRecordBelongsToCurrentTenantFunctionDefinitionProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IsRecordBelongsToCurrentTenantFunctionDefinitionProducer.java
@@ -1,8 +1,9 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
-import com.github.starnowski.posmulten.postgresql.core.rls.function.IGetCurrentTenantIdFunctionInvocationFactory;
-import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionDefinition;
-import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantProducer;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.*;
+
+import static com.github.starnowski.posmulten.postgresql.core.rls.function.AbstractIsRecordBelongsToCurrentTenantProducerParameters.pairOfColumnWithType;
+import static java.util.stream.Collectors.toList;
 
 public class IsRecordBelongsToCurrentTenantFunctionDefinitionProducer {
 
@@ -10,8 +11,15 @@ public class IsRecordBelongsToCurrentTenantFunctionDefinitionProducer {
 
     public IsRecordBelongsToCurrentTenantFunctionDefinition produce(TableKey tableKey, AbstractTableColumns tableColumns, IGetCurrentTenantIdFunctionInvocationFactory iGetCurrentTenantIdFunctionInvocationFactory, String functionName, String schema)
     {
-        //TODO
-        return null;
+        AbstractIsRecordBelongsToCurrentTenantProducerParameters isRecordBelongsToCurrentTenantProducerParameters = new IsRecordBelongsToCurrentTenantProducerParameters.Builder()
+                .withSchema(schema)
+                .withFunctionName(functionName)
+                .withRecordTableName(tableKey.getTable())
+                .withRecordSchemaName(tableKey.getSchema())
+                .withiGetCurrentTenantIdFunctionInvocationFactory(iGetCurrentTenantIdFunctionInvocationFactory)
+                .withTenantColumn(tableColumns.getTenantColumnName())
+                .withKeyColumnsPairsList(tableColumns.getIdentityColumnNameAndTypeMap().entrySet().stream().map(entry -> pairOfColumnWithType(entry.getKey(), entry.getValue())).collect(toList())).build();
+        return isRecordBelongsToCurrentTenantProducer.produce(isRecordBelongsToCurrentTenantProducerParameters);
     }
 
     void setIsRecordBelongsToCurrentTenantProducer(IsRecordBelongsToCurrentTenantProducer isRecordBelongsToCurrentTenantProducer) {
