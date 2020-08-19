7bb13b4bf4b02bc2322cdf0353ef3dbbd7987f86
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-19 21:13:38 +0200

    #96 - Added the basic component. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IsRecordBelongsToCurrentTenantFunctionDefinitionProducer.java
@@ -0,0 +1,20 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IGetCurrentTenantIdFunctionInvocationFactory;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantProducer;
+
+public class IsRecordBelongsToCurrentTenantFunctionDefinitionProducer {
+
+    private IsRecordBelongsToCurrentTenantProducer isRecordBelongsToCurrentTenantProducer = new IsRecordBelongsToCurrentTenantProducer();
+
+    public IsRecordBelongsToCurrentTenantFunctionDefinition produce(TableKey tableKey, AbstractTableColumns tableColumns, IGetCurrentTenantIdFunctionInvocationFactory iGetCurrentTenantIdFunctionInvocationFactory, String funtionName)
+    {
+        //TODO
+        return null;
+    }
+
+    void setIsRecordBelongsToCurrentTenantProducer(IsRecordBelongsToCurrentTenantProducer isRecordBelongsToCurrentTenantProducer) {
+        this.isRecordBelongsToCurrentTenantProducer = isRecordBelongsToCurrentTenantProducer;
+    }
+}
