f0f153589489bb440775b04752e917d7834233ae
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-20 00:43:14 +0200

    #96 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilder.java
@@ -107,7 +107,7 @@ public class DefaultSharedSchemaContextBuilder {
     }
 
     public DefaultSharedSchemaContextBuilder createSameTenantConstraintForForeignKey(String mainTable, String foreignKeyTable, Map<String, String> foreignKeyPrimaryKeyColumnsMappings, String constraintName) {
-        sharedSchemaContextRequest.getSameTenantConstraintForForeignKeyProperties().put(new SameTenantConstraintForForeignKey(new TableKey(mainTable, defaultSchema), new TableKey(mainTable, foreignKeyTable), foreignKeyPrimaryKeyColumnsMappings.keySet()), null);
+        sharedSchemaContextRequest.getSameTenantConstraintForForeignKeyProperties().put(new SameTenantConstraintForForeignKey(new TableKey(mainTable, defaultSchema), new TableKey(foreignKeyTable, defaultSchema), foreignKeyPrimaryKeyColumnsMappings.keySet()), null);
         //TODO
         return this;
     }
