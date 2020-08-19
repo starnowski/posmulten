c00218f102877c5e15bfbddf51136748e6cc73c3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-20 00:37:04 +0200

    #96 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantFunctionDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantFunctionDefinitionsEnricher.java
@@ -1,9 +1,10 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContext;
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContextEnricher;
-import com.github.starnowski.posmulten.postgresql.core.context.IsRecordBelongsToCurrentTenantFunctionDefinitionProducer;
-import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.context.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class IsRecordBelongsToCurrentTenantFunctionDefinitionsEnricher implements AbstractSharedSchemaContextEnricher {
 
@@ -11,7 +12,14 @@ public class IsRecordBelongsToCurrentTenantFunctionDefinitionsEnricher implement
 
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
-        return null;
+        List<TableKey> tableRequiredFunction = request.getSameTenantConstraintForForeignKeyProperties().keySet().stream().map(constraintKey -> constraintKey.getForeignKeyTable()).collect(toList());
+        for (TableKey tableKey : tableRequiredFunction)
+        {
+            //TODO Throw exception when no name was defined
+            String functionName = request.getFunctionThatChecksIfRecordExistsInTableNames().get(tableKey);
+            context.addSQLDefinition(isRecordBelongsToCurrentTenantFunctionDefinitionProducer.produce(tableKey, request.getTableColumnsList().get(tableKey), context.getIGetCurrentTenantIdFunctionInvocationFactory(), functionName, request.getDefaultSchema() ));
+        }
+        return context;
     }
 
     void setIsRecordBelongsToCurrentTenantFunctionDefinitionProducer(IsRecordBelongsToCurrentTenantFunctionDefinitionProducer isRecordBelongsToCurrentTenantFunctionDefinitionProducer) {
