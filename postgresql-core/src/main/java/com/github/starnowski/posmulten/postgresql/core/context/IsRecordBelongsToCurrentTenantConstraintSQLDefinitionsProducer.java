f082ec4b7c71002e542c20a355deac630ba17379
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-20 22:47:20 +0200

    #98 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer.java
@@ -1,9 +1,17 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue;
+import com.github.starnowski.posmulten.postgresql.core.rls.DefaultIsRecordBelongsToCurrentTenantConstraintProducerParameters;
 import com.github.starnowski.posmulten.postgresql.core.rls.IsRecordBelongsToCurrentTenantConstraintProducer;
+import com.github.starnowski.posmulten.postgresql.core.rls.IsRecordBelongsToCurrentTenantConstraintProducerParameters;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forReference;
 
 public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer {
 
@@ -11,10 +19,22 @@ public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer {
 
     public List<SQLDefinition> produce(AbstractIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters parameters)
     {
-        return null;
+        IsRecordBelongsToCurrentTenantConstraintProducerParameters isRecordBelongsToCurrentTenantConstraintProducerParameters = DefaultIsRecordBelongsToCurrentTenantConstraintProducerParameters.builder()
+                .withConstraintName(parameters.getConstraintName())
+                .withTableName(parameters.getTableKey().getTable())
+                .withTableSchema(parameters.getTableKey().getSchema())
+                .withIsRecordBelongsToCurrentTenantFunctionInvocationFactory(parameters.getIsRecordBelongsToCurrentTenantFunctionInvocationFactory())
+                .withPrimaryColumnsValuesMap(convertForeignPrimaryKeyMapping(parameters.getForeignKeyPrimaryKeyMappings()))
+                .build();
+        return Collections.singletonList(isRecordBelongsToCurrentTenantConstraintProducer.produce(isRecordBelongsToCurrentTenantConstraintProducerParameters));
     }
 
     void setIsRecordBelongsToCurrentTenantConstraintProducer(IsRecordBelongsToCurrentTenantConstraintProducer isRecordBelongsToCurrentTenantConstraintProducer) {
         this.isRecordBelongsToCurrentTenantConstraintProducer = isRecordBelongsToCurrentTenantConstraintProducer;
+    }
+
+    private Map<String, FunctionArgumentValue> convertForeignPrimaryKeyMapping(Map<String, String> foreignKeyPrimaryKeyMappings)
+    {
+        return foreignKeyPrimaryKeyMappings.entrySet().stream().collect(Collectors.toMap(entry -> entry.getKey(), entry-> forReference(entry.getValue())));
     }
 }
