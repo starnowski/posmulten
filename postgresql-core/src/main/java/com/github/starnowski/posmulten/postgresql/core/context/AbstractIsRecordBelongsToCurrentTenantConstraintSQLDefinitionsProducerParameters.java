488885fd53d76693a2c55d77eab9a9579195c450
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-20 19:06:59 +0200

    #98 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/AbstractIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/AbstractIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters.java
@@ -1,4 +1,18 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionInvocationFactory;
+
+import java.util.Map;
+
 public interface AbstractIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters {
+
+    String getConstraintName();
+
+    String getTableName();
+
+    String  getSchema();
+
+    IsRecordBelongsToCurrentTenantFunctionInvocationFactory getIsRecordBelongsToCurrentTenantFunctionInvocationFactory();
+
+    Map<String, String> getForeignKeyPrimaryKeyMappings();
 }
