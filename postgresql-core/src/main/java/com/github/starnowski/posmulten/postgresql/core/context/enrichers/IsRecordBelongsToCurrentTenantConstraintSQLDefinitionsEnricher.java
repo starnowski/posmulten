103fb96a06d244b77768ac310da6acb800c85241
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-22 18:08:51 +0200

    #98 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher.java
@@ -1,9 +1,12 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContext;
-import com.github.starnowski.posmulten.postgresql.core.context.AbstractSharedSchemaContextEnricher;
-import com.github.starnowski.posmulten.postgresql.core.context.IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer;
-import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.context.*;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionDefinition;
+import javafx.util.Pair;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher implements AbstractSharedSchemaContextEnricher {
 
@@ -11,7 +14,16 @@ public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher impl
 
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
-        return null;
+        List<Pair<SameTenantConstraintForForeignKey, SameTenantConstraintForForeignKeyProperties>> constrainsRequests = request.getSameTenantConstraintForForeignKeyProperties().entrySet().stream().map(entry -> new Pair(entry.getKey(), entry.getValue())).collect(toList());
+        for (Pair<SameTenantConstraintForForeignKey, SameTenantConstraintForForeignKeyProperties> constraintRequest : constrainsRequests)
+        {
+            //TODO Throw exception when no name was defined
+            String functionName = request.getFunctionThatChecksIfRecordExistsInTableNames().get(tableKey);
+            IsRecordBelongsToCurrentTenantFunctionDefinition functionDefinition = isRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer.produce(tableKey, request.getTableColumnsList().get(tableKey), context.getIGetCurrentTenantIdFunctionInvocationFactory(), functionName, request.getDefaultSchema());
+            context.addSQLDefinition(functionDefinition);
+            context.getTableKeysIsRecordBelongsToCurrentTenantFunctionInvocationFactoryMap().put(tableKey, functionDefinition);
+        }
+        return context;
     }
 
     void setIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer(IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer isRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer) {
