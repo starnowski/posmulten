abfa44a1a7f2431b884e3aa4348334d80c4fe039
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-23 18:16:02 +0200

    #102 - Added implementation for constructors methods. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/SharedSchemaContextBuilderException.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/SharedSchemaContextBuilderException.java
@@ -1,4 +1,23 @@
 package com.github.starnowski.posmulten.postgresql.core.context.exceptions;
 
 public abstract class SharedSchemaContextBuilderException extends Exception{
+
+    public SharedSchemaContextBuilderException() {
+    }
+
+    public SharedSchemaContextBuilderException(String message) {
+        super(message);
+    }
+
+    public SharedSchemaContextBuilderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SharedSchemaContextBuilderException(Throwable cause) {
+        super(cause);
+    }
+
+    public SharedSchemaContextBuilderException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
