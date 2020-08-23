e733fe9f3860d11a8b623c999bda6a3aca6e3949
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-23 17:14:48 +0200

    #102 - Code fix. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantColumnSQLDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/TenantColumnSQLDefinitionsEnricher.java
@@ -2,15 +2,22 @@ package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
 import com.github.starnowski.posmulten.postgresql.core.context.*;
 
+import java.util.Set;
+
 public class TenantColumnSQLDefinitionsEnricher implements AbstractSharedSchemaContextEnricher {
 
     private SingleTenantColumnSQLDefinitionsProducer singleTenantColumnSQLDefinitionsProducer = new SingleTenantColumnSQLDefinitionsProducer();
 
     @Override
     public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
+        Set<TableKey> tableThatRequireCreationOfTheTenantColumn = request.getCreateTenantColumnTableLists();
+        if (tableThatRequireCreationOfTheTenantColumn.isEmpty())
+        {
+            return context;
+        }
         //TODO Add exception for non-existed table declaration for which rls policy should be created
         String getCurrentTenantIdFunctionInvocation = context.getIGetCurrentTenantIdFunctionInvocationFactory().returnGetCurrentTenantIdFunctionInvocation();
-        for (TableKey tableKey: request.getCreateTenantColumnTableLists())
+        for (TableKey tableKey: tableThatRequireCreationOfTheTenantColumn)
         {
             AbstractTableColumns tableColumns = request.getTableColumnsList().get(tableKey);
             singleTenantColumnSQLDefinitionsProducer.produce(tableKey, tableColumns, getCurrentTenantIdFunctionInvocation, request.getCurrentTenantIdProperty(), request.getCurrentTenantIdPropertyType())
