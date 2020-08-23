31043b9ce285dc35b70a6fce3014484ca16ec9c9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-23 20:17:49 +0200

    #102 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/MissingConstraintNameDeclarationForTableException.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/MissingConstraintNameDeclarationForTableException.java
@@ -2,13 +2,21 @@ package com.github.starnowski.posmulten.postgresql.core.context.exceptions;
 
 import com.github.starnowski.posmulten.postgresql.core.context.TableKey;
 
+import java.util.Set;
+
 public class MissingConstraintNameDeclarationForTableException extends SharedSchemaContextBuilderException{
 
     private final TableKey tableKey;
+    private final Set<String> foreignKeysColumns;
 
-    public MissingConstraintNameDeclarationForTableException(TableKey tableKey, String message) {
+    public MissingConstraintNameDeclarationForTableException(TableKey tableKey, String message, Set<String> foreignKeysColumns) {
         super(message);
         this.tableKey = tableKey;
+        this.foreignKeysColumns = foreignKeysColumns;
+    }
+
+    public Set<String> getForeignKeysColumns() {
+        return foreignKeysColumns;
     }
 
     public TableKey getTableKey() {
