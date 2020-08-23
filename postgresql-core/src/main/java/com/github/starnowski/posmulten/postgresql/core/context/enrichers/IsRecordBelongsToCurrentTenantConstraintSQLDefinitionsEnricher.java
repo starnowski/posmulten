564050b6b9717e69f8ac49a6c6c37ee80731a2a0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-08-23 20:27:11 +0200

    #102 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher.java
@@ -1,11 +1,14 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
 import com.github.starnowski.posmulten.postgresql.core.context.*;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.MissingConstraintNameDeclarationForTableException;
 import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionInvocationFactory;
 import javafx.util.Pair;
 
 import java.util.List;
 
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher implements AbstractSharedSchemaContextEnricher {
@@ -13,13 +16,21 @@ public class IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsEnricher impl
     private IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer isRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer = new IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducer();
 
     @Override
-    public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) {
+    public AbstractSharedSchemaContext enrich(AbstractSharedSchemaContext context, SharedSchemaContextRequest request) throws MissingConstraintNameDeclarationForTableException {
         List<Pair<SameTenantConstraintForForeignKey, AbstractSameTenantConstraintForForeignKeyProperties>> constrainsRequests = request.getSameTenantConstraintForForeignKeyProperties().entrySet().stream().map(entry -> new Pair<SameTenantConstraintForForeignKey, AbstractSameTenantConstraintForForeignKeyProperties>(entry.getKey(), entry.getValue())).collect(toList());
         for (Pair<SameTenantConstraintForForeignKey, AbstractSameTenantConstraintForForeignKeyProperties> constraintRequest : constrainsRequests)
         {
             SameTenantConstraintForForeignKey key = constraintRequest.getKey();
             AbstractSameTenantConstraintForForeignKeyProperties requestProperties = constraintRequest.getValue();
-            //TODO Throw exception when no name was defined, for constraint and there is no sql definition 'IsRecordBelongsToCurrentTenantFunctionInvocationFactory'
+            //TODO Throw exception when there is no sql definition 'IsRecordBelongsToCurrentTenantFunctionInvocationFactory'
+            if (requestProperties.getConstraintName() == null)
+            {
+                throw new MissingConstraintNameDeclarationForTableException(key.getMainTable(), key.getForeignKeyColumns(),
+                        format("Missing constraint name that in table %1$s and schema %2$s checks  if the foreign key columns (%3$s) refers to records that belong to the same tenant",
+                                key.getMainTable().getTable(),
+                                key.getMainTable().getSchema(),
+                                key.getForeignKeyColumns().stream().sorted().collect(joining(", "))));
+            }
             IsRecordBelongsToCurrentTenantFunctionInvocationFactory isRecordBelongsToCurrentTenantFunctionInvocationFactory = context.getTableKeysIsRecordBelongsToCurrentTenantFunctionInvocationFactoryMap().get(key.getForeignKeyTable());
             AbstractIsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters parameters = IsRecordBelongsToCurrentTenantConstraintSQLDefinitionsProducerParameters.builder()
                     .withConstraintName(requestProperties.getConstraintName())
