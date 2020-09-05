a152035ddf1b1d6485792a50121732dd639d9600
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-06 01:56:42 +0200

    #113 - Tests code refactoring. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/ForeignKeysMappingSharedSchemaContextRequestValidatorTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/ForeignKeysMappingSharedSchemaContextRequestValidatorTest.groovy
@@ -2,6 +2,7 @@ package com.github.starnowski.posmulten.postgresql.core.context.validators
 
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.IncorrectForeignKeysMappingException
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -30,5 +31,31 @@ class ForeignKeysMappingSharedSchemaContextRequestValidatorTest extends Specific
             "posts"             |   "users"             |   [user_id: "uuid"]                                           |   [uuid: null]
             "posts"             |   "comments"          |   [comment_id: "comment_id"]                                  |   [comment_id: null]
             "posts"             |   "comments"          |   [comment_id: "comment_id", comment_user: "user"]            |   [comment_id: null, user: null]
+    }
+
+    @Unroll
+    def "should throw exception when foreign keys mapping does not match to defined primary keys for tests parameters: #foreignKeysTable, #primaryKeysTable, #foreignKeysMapping, #primayKeyTypeDefinition"()
+    {
+        given:
+            DefaultSharedSchemaContextBuilder builder = new DefaultSharedSchemaContextBuilder()
+            builder.createSameTenantConstraintForForeignKey(foreignKeysTable, primaryKeysTable, foreignKeysMapping, null)
+            builder.createRLSPolicyForTable(primaryKeysTable, primayKeyTypeDefinition, null, null)
+            SharedSchemaContextRequest request = builder.getSharedSchemaContextRequestCopy()
+
+        when:
+            tested.validate(request)
+
+        then:
+            def ex = thrown(IncorrectForeignKeysMappingException)
+
+        and: "exception should have correct message"
+            ex.message == expectedMessage
+
+        where:
+            foreignKeysTable    |   primaryKeysTable    |   foreignKeysMapping                                          |   primayKeyTypeDefinition             ||  expectedMessage
+            "comments"          |   "users"             |   [user_id: "id"]                                             |   [uuid: null]                        ||  "There is mismatch between foreign keys column mapping (id) in comments table and primary keys column declaration (uuid) for users table"
+            "posts"             |   "users"             |   [user_id: "uuid"]                                           |   [id: null]                          ||  ""
+            "posts"             |   "comments"          |   [comment_id: "comment_id", comment_user: "user_id"]         |   [comment_id: null, user: null]      ||  ""
+            "posts"             |   "comments"          |   [comment_id: "id", comment_user: "user_id"]                 |   [comment_id: null, user: null]      ||  ""
     }
 }
