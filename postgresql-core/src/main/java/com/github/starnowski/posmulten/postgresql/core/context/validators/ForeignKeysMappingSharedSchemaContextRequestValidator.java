ddb4597b93ba45bd8a6c9a6fec6c3a24075f3ef5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-06 12:48:46 +0200

    #113 - Added implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/ForeignKeysMappingSharedSchemaContextRequestValidator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/ForeignKeysMappingSharedSchemaContextRequestValidator.java
@@ -1,11 +1,48 @@
 package com.github.starnowski.posmulten.postgresql.core.context.validators;
 
-import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
-import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import com.github.starnowski.posmulten.postgresql.core.context.*;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.IncorrectForeignKeysMappingException;
+import javafx.util.Pair;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class ForeignKeysMappingSharedSchemaContextRequestValidator implements ISharedSchemaContextRequestValidator{
     @Override
-    public void validate(SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
+    public void validate(SharedSchemaContextRequest request) throws IncorrectForeignKeysMappingException {
+        List<Pair<SameTenantConstraintForForeignKey, ISameTenantConstraintForForeignKeyProperties>> constraintsRequests = request.getSameTenantConstraintForForeignKeyProperties().entrySet().stream().map(entry -> new Pair<SameTenantConstraintForForeignKey, ISameTenantConstraintForForeignKeyProperties>(entry.getKey(), entry.getValue())).collect(toList());
+        Map<TableKey, ITableColumns> primaryKeyDefinitions = request.getTableColumnsList();
+        for (Pair<SameTenantConstraintForForeignKey, ISameTenantConstraintForForeignKeyProperties> constrainRequest : constraintsRequests)
+        {
+            Set<String> primaryKeysInForeignKeysMapping = constrainRequest.getValue().getForeignKeyPrimaryKeyColumnsMappings().values().stream().collect(Collectors.toSet());
+            Set<String> primaryKeysForRLSPolicy = primaryKeyDefinitions.get(constrainRequest.getKey().getForeignKeyTable()).getIdentityColumnNameAndTypeMap().keySet();
+            if (!primaryKeysInForeignKeysMapping.equals(primaryKeysForRLSPolicy))
+            {
+                throw new IncorrectForeignKeysMappingException(prepareExceptionMessage(constrainRequest.getKey().getMainTable(), constrainRequest.getKey().getForeignKeyTable(), primaryKeysInForeignKeysMapping, primaryKeysForRLSPolicy),
+                        constrainRequest.getKey().getMainTable(),
+                        constrainRequest.getKey().getForeignKeyTable(),
+                        primaryKeysInForeignKeysMapping,
+                        primaryKeysForRLSPolicy);
+            }
+        }
+    }
 
+    private String prepareExceptionMessage(TableKey foreignTableKey, TableKey primaryTableKey, Set<String> foreignKeys, Set<String> primaryKeys)
+    {
+        return String.format("There is mismatch between foreign keys column mapping (id) in comments table and primary keys column declaration (uuid) for users table",
+                foreignKeys.stream().sorted().collect(joining(", ")),
+                returnTableName(foreignTableKey),
+                primaryKeys.stream().sorted().collect(joining(", ")),
+                returnTableName(primaryTableKey));
+    }
+
+    private String returnTableName(TableKey tableKey)
+    {
+        return (tableKey.getSchema() == null ? "" : tableKey.getSchema() + ".") + tableKey.getTable();
     }
 }
