a365bbf6b981afd1cb8a1c79d3de6134798752a9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-06 23:07:55 +0200

    #106 - Added generic tests. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsTenantValidBasedOnConstantValuesFunctionProducerTest.groovy
@@ -0,0 +1,17 @@
+package com.github.starnowski.posmulten.postgresql.core.rls.function
+
+import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactoryTest
+
+class IsTenantValidBasedOnConstantValuesFunctionProducerTest extends AbstractFunctionFactoryTest {
+    @Override
+    protected returnTestedObject() {
+        new IsTenantValidBasedOnConstantValuesFunctionProducer()
+    }
+
+    @Override
+    protected returnCorrectParametersSpyObject() {
+        Spy(IsTenantValidBasedOnConstantValuesFunctionProducerParameters, constructorArgs: ["is_tenant_valid",
+                                                                                "public",
+                                                                                new HashSet<>(Arrays.asList("bad_tenant", "tenant_1"))])
+    }
+}
