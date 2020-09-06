a93e47b975bcd188705f11c9725cfc49fea458d5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-06 11:54:04 +0200

    #113 - Added properties that describe the invalid mapping of the foreign keys in the IncorrectForeignKeysMappingException exception. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/IncorrectForeignKeysMappingException.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/exceptions/IncorrectForeignKeysMappingException.java
@@ -1,4 +1,39 @@
 package com.github.starnowski.posmulten.postgresql.core.context.exceptions;
 
+import com.github.starnowski.posmulten.postgresql.core.context.TableKey;
+
+import java.util.Set;
+
 public class IncorrectForeignKeysMappingException extends SharedSchemaContextBuilderException{
+
+    private TableKey foreignTableKey;
+    private TableKey primaryTableKey;
+    private Set<String> foreignKeys;
+    private Set<String> primaryKeys;
+
+    public IncorrectForeignKeysMappingException(String message, TableKey foreignTableKey, TableKey primaryTableKey, Set<String> foreignKeys, Set<String> primaryKeys) {
+        super(message);
+        this.foreignTableKey = foreignTableKey;
+        this.primaryTableKey = primaryTableKey;
+        this.foreignKeys = foreignKeys;
+        this.primaryKeys = primaryKeys;
+    }
+
+    public TableKey getForeignTableKey() {
+        return foreignTableKey;
+    }
+
+    public TableKey getPrimaryTableKey() {
+        return primaryTableKey;
+    }
+
+    public Set<String> getForeignKeys() {
+        return foreignKeys;
+    }
+
+    public Set<String> getPrimaryKeys() {
+        return primaryKeys;
+    }
+
+
 }
