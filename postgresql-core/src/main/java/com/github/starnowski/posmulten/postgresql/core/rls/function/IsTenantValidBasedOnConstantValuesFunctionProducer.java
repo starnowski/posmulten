58642c0cf0d381c633ecd665a6eb7f6026ebf4b3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-06 22:55:43 +0200

    #106 - Added producer component. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/function/IsTenantValidBasedOnConstantValuesFunctionProducer.java
@@ -0,0 +1,27 @@
+package com.github.starnowski.posmulten.postgresql.core.rls.function;
+
+import com.github.starnowski.posmulten.postgresql.core.common.function.ExtendedAbstractFunctionFactory;
+import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.function.metadata.MetadataPhraseBuilder;
+
+public class IsTenantValidBasedOnConstantValuesFunctionProducer extends ExtendedAbstractFunctionFactory<IIsTenantValidBasedOnConstantValuesFunctionProducerParameters, IsTenantValidBasedOnConstantValuesFunctionDefinition> {
+    @Override
+    protected String prepareReturnType(IIsTenantValidBasedOnConstantValuesFunctionProducerParameters parameters) {
+        return null;
+    }
+
+    @Override
+    protected void enrichMetadataPhraseBuilder(IIsTenantValidBasedOnConstantValuesFunctionProducerParameters parameters, MetadataPhraseBuilder metadataPhraseBuilder) {
+
+    }
+
+    @Override
+    protected String buildBody(IIsTenantValidBasedOnConstantValuesFunctionProducerParameters parameters) {
+        return null;
+    }
+
+    @Override
+    protected IsTenantValidBasedOnConstantValuesFunctionDefinition returnFunctionDefinition(IIsTenantValidBasedOnConstantValuesFunctionProducerParameters parameters, IFunctionDefinition functionDefinition) {
+        return new IsTenantValidBasedOnConstantValuesFunctionDefinition(functionDefinition);
+    }
+}
