65db4c40bd57feb76c63d9e2714b9b64b7dc01bb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-07 22:14:39 +0200

    #106 - Added unit tests. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsTenantValidBasedOnConstantValuesFunctionProducerTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/function/IsTenantValidBasedOnConstantValuesFunctionProducerTest.groovy
@@ -1,8 +1,24 @@
 package com.github.starnowski.posmulten.postgresql.core.rls.function
 
 import com.github.starnowski.posmulten.postgresql.core.common.function.AbstractFunctionFactoryTest
+import spock.lang.Unroll
 
 class IsTenantValidBasedOnConstantValuesFunctionProducerTest extends AbstractFunctionFactoryTest {
+
+    def tested = new IsTenantValidBasedOnConstantValuesFunctionProducer()
+
+    @Unroll
+    def "should generate statement that creates function '#testFunctionName' for schema '#testSchema' with argument type '#argumentType' (null means 'text') which invalid tenant values'#invalidTenantValues'" () {
+        expect:
+            tested.produce(new IsTenantValidBasedOnConstantValuesFunctionProducerParameters(testFunctionName, testSchema, new HashSet<String>(invalidTenantValues), argumentType)).getCreateScript() == expectedStatement
+
+        where:
+            testSchema              |   testFunctionName            |   invalidTenantValues     |   argumentType        ||  expectedStatement
+            null                    |   "is_tenant_valid"           |   ["XX-dadf-dsa"]         |   null                ||  "CREATE OR REPLACE FUNCTION is_tenant_valid(text) RETURNS BOOLEAN AS \$\$\nSELECT \\\$1 <> CAST ('XX-dadf-dsa' AS text)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
+            "public"                |   "is_tenant_valid"           |   ["XX-dadf-dsa"]         |   null                ||  "CREATE OR REPLACE FUNCTION public.is_tenant_valid(text) RETURNS BOOLEAN AS \$\$\nSELECT \\\$1 <> CAST ('XX-dadf-dsa' AS text)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
+            "non_public_schema"     |   "is_tenant_valid"           |   ["XX-dadf-dsa"]         |   null                ||  "CREATE OR REPLACE FUNCTION non_public_schema.is_tenant_valid(text) RETURNS BOOLEAN AS \$\$\nSELECT \\\$1 <> CAST ('XX-dadf-dsa' AS text)\n\$\$ LANGUAGE sql\nSTABLE\nPARALLEL SAFE;"
+    }
+
     @Override
     protected returnTestedObject() {
         new IsTenantValidBasedOnConstantValuesFunctionProducer()
