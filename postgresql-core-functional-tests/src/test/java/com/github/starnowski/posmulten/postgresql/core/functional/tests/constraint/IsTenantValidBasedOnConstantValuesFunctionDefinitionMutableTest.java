04a8e85a6abe0af1aea3bd6758406c2f5a072cb3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-09 23:42:20 +0200

    #106 - Tests code refactoring. [skip travis]

--- postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/constraint/IsTenantValidBasedOnConstantValuesFunctionDefinitionMutableTest.java
+++ postgresql-core-functional-tests/src/test/java/com/github/starnowski/posmulten/postgresql/core/functional/tests/constraint/IsTenantValidBasedOnConstantValuesFunctionDefinitionMutableTest.java
@@ -1,5 +1,6 @@
 package com.github.starnowski.posmulten.postgresql.core.functional.tests.constraint;
 
+import com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue;
 import com.github.starnowski.posmulten.postgresql.core.functional.tests.TestNGSpringContextWithoutGenericTransactionalSupportTests;
 import com.github.starnowski.posmulten.postgresql.core.rls.function.IsTenantValidBasedOnConstantValuesFunctionDefinition;
 import com.github.starnowski.posmulten.postgresql.core.rls.function.IsTenantValidBasedOnConstantValuesFunctionProducer;
@@ -10,7 +11,10 @@ import org.testng.annotations.Test;
 
 import java.util.HashSet;
 
+import static com.github.starnowski.posmulten.postgresql.core.common.function.FunctionArgumentValue.forString;
 import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.isFunctionExists;
+import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.selectAndReturnFirstRecordAsBoolean;
+import static java.lang.String.format;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
@@ -50,13 +54,22 @@ public class IsTenantValidBasedOnConstantValuesFunctionDefinitionMutableTest ext
         assertEquals(true, isFunctionExists(jdbcTemplate, testFunctionName, getSchema()));
     }
 
-    //TODO Test function execution - invalid value (first)
-    //TODO Test function execution - valid value (second)
+    @Test(dependsOnMethods = "functionShouldExist")
+    public void firstFunctionVersionShouldReturnFalseForFirstInvalidValue()
+    {
+        assertEquals(false, returnFunctionResultForValue(firstInvalidValue));
+    }
+
+    @Test(dependsOnMethods = "firstFunctionVersionShouldReturnFalseForFirstInvalidValue")
+    public void firstFunctionVersionShouldReturnTrueForSecondInvalidValue()
+    {
+        assertEquals(true, returnFunctionResultForValue(secondInvalidValue));
+    }
     //TODO Update function body with new invalid value (second)
     //TODO Test function execution - invalid value (second)
     //TODO Test function execution - valid value (first)
 
-    @Test(dependsOnMethods = "functionShouldExist", alwaysRun = true)
+    @Test(dependsOnMethods = "firstFunctionVersionShouldReturnTrueForSecondInvalidValue", alwaysRun = true)
     public void dropFunction()
     {
         jdbcTemplate.execute(functionDefinition.getDropScript());
@@ -64,5 +77,13 @@ public class IsTenantValidBasedOnConstantValuesFunctionDefinitionMutableTest ext
 
     private String getSchema() {
         return null;
+    }
+
+    private boolean returnFunctionResultForValue(String value)
+    {
+        FunctionArgumentValue argumentValue = forString(value);
+        String functionInvocation = functionDefinition.returnIsTenantValidFunctionInvocation(argumentValue);
+        String selectStatement = format("SELECT %1$s;", functionInvocation);
+        return selectAndReturnFirstRecordAsBoolean(jdbcTemplate, selectStatement);
     }
 }
