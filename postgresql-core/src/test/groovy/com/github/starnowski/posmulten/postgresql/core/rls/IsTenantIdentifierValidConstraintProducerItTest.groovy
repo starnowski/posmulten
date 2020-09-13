099ca897bd18df5857c9087701bf223b55d26067
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-13 18:00:58 +0200

    #106 - Tests code refactoring. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/IsTenantIdentifierValidConstraintProducerItTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/rls/IsTenantIdentifierValidConstraintProducerItTest.groovy
@@ -1,13 +1,14 @@
 package com.github.starnowski.posmulten.postgresql.core.rls
 
 import com.github.starnowski.posmulten.postgresql.core.TestApplication
-import com.github.starnowski.posmulten.postgresql.core.rls.function.IsRecordBelongsToCurrentTenantFunctionInvocationFactory
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IIsTenantValidFunctionInvocationFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static com.github.starnowski.posmulten.postgresql.core.rls.DefaultIsTenantIdentifierValidConstraintProducerParameters.builder
 import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.isConstraintExists
 import static org.junit.Assert.assertEquals
 
@@ -25,40 +26,40 @@ class IsTenantIdentifierValidConstraintProducerItTest extends Specification {
     def constraintName
 
     @Unroll
-    def "should return statement that adds '#testConstraintName' constraint to table (#testTable) and schema (#testSchema) with condition '#conditionStatement'"()
+    def "should return statement that adds '#testConstraintName' constraint to table (#testTable) and schema (#testSchema) with condition '#conditionStatement' and tenant column #tenantColumn"()
     {
         given:
-        schema = testSchema
-        table = testTable
-        constraintName = testConstraintName
-        assertEquals(false, isConstraintExists(jdbcTemplate, schema, table, constraintName))
-        IsRecordBelongsToCurrentTenantFunctionInvocationFactory isRecordBelongsToCurrentTenantFunctionInvocationFactory =
-                {
-                    conditionStatement
-                }
-        def parameters = DefaultIsRecordBelongsToCurrentTenantConstraintProducerParameters.builder()
-                .withConstraintName(constraintName)
-                .withTableName(table)
-                .withTableSchema(schema)
-                .withIsRecordBelongsToCurrentTenantFunctionInvocationFactory(isRecordBelongsToCurrentTenantFunctionInvocationFactory)
-                .withPrimaryColumnsValuesMap(primaryColumnsValuesMap).build()
+            schema = testSchema
+            table = testTable
+            constraintName = testConstraintName
+            assertEquals(false, isConstraintExists(jdbcTemplate, schema, table, constraintName))
+            IIsTenantValidFunctionInvocationFactory factory =
+                    {
+                        conditionStatement
+                    }
+            def parameters = builder()
+                    .withConstraintName(constraintName)
+                    .withTableName(table)
+                    .withTableSchema(schema)
+                    .withIIsTenantValidFunctionInvocationFactory(factory)
+                    .withTenantColumnName(tenantColumn).build()
         when:
-        definition = tested.produce(parameters)
-        jdbcTemplate.execute(definition.getCreateScript())
+            definition = tested.produce(parameters)
+            jdbcTemplate.execute(definition.getCreateScript())
 
         then:
-        isConstraintExists(jdbcTemplate, schema, table, constraintName)
+            isConstraintExists(jdbcTemplate, schema, table, constraintName)
 
         where:
             testConstraintName      |   testSchema              | testTable     |   tenantColumn    |   conditionStatement
             "sss"                   |   null                    | "users"       |   "tenant_id"     |   "tenant_id = 'dsasdf'"
             "sss"                   |   "public"                | "users"       |   "tenant_id"     |   "tenant_id <> 'asdfjlzvz[HALA'"
             "sss"                   |   "non_public_schema"     | "users"       |   "tenant_id"     |   "tenant_id = 'dsasdf'"
-            "user_belongs_tt"       |   "non_public_schema"     | "users"       |   "tenant_id"     |   "id <> 'SSSAA'"
+            "user_belongs_tt"       |   "non_public_schema"     | "users"       |   "tenant_id"     |   "id <> 3344"
             "user_belongs_tt"       |   "non_public_schema"     | "users"       |   "tenant_id"     |   "tenant_id = 'dsasdf'"
-            "user_belongs_tt"       |   "non_public_schema"     | "posts"       |   "tenant_id"     |   "id <> 'SSSAA'"
-            "user_belongs_tt"       |   "non_public_schema"     | "posts"       |   "tenant_id"     |   "jhhh <> 'SSSAA'"
-            "does_comment_from_t"   |   "non_public_schema"     | "comments"    |   "tenant"        |   "uuid = 'dsasdf'"
+            "user_belongs_tt"       |   "non_public_schema"     | "posts"       |   "tenant_id"     |   "id = 5544"
+            "user_belongs_tt"       |   "non_public_schema"     | "posts"       |   "tenant_id"     |   "tenant_id <> 'SSSAA'"
+            "does_comment_from_t"   |   "non_public_schema"     | "comments"    |   "tenant"        |   "tenant = 'gggfff'"
     }
 
     def cleanup() {
