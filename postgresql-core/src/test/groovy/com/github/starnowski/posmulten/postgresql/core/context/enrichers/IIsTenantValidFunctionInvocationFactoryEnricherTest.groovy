3ef8e0fd60e1103fd5700dd873134c4c0bb21120
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-19 17:41:43 +0200

    #106 - Added basic type like main class and tests class. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IIsTenantValidFunctionInvocationFactoryEnricherTest.groovy
@@ -0,0 +1,6 @@
+package com.github.starnowski.posmulten.postgresql.core.context.enrichers
+
+import spock.lang.Specification
+
+class IIsTenantValidFunctionInvocationFactoryEnricherTest extends Specification {
+}
