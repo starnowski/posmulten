02283baf04fe8d3ddf59671579069696deaa5bdb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-20 21:17:21 +0200

    #106 - Added tests case for setting custom function name. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IIsTenantValidFunctionInvocationFactoryEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IIsTenantValidFunctionInvocationFactoryEnricherTest.groovy
@@ -51,5 +51,46 @@ class IIsTenantValidFunctionInvocationFactoryEnricherTest extends Specification 
             "some_schema"   |   ["10.22", "9990"]
     }
 
+    @Unroll
+    def "should enrich shared schema context with SQL definition for the function that checks if tenant value is correct based on custom values for shares schema context builder for schema #schema and black list values (#blacklist) and function name #isTenantValidFunctionName and constraint name #isTenantValidConstraintName"()
+    {
+        given:
+            def builder = new DefaultSharedSchemaContextBuilder(schema)
+            builder.createValidTenantValueConstraint(blacklist, isTenantValidFunctionName, isTenantValidConstraintName)
+            def sharedSchemaContextRequest = builder.getSharedSchemaContextRequestCopy()
+            def context = new SharedSchemaContext()
+            IIsTenantValidBasedOnConstantValuesFunctionProducerParameters capturedParameters = null
+            def mockedSQLDefinition = Mock(IsTenantValidBasedOnConstantValuesFunctionDefinition)
+            def producer = Mock(IsTenantValidBasedOnConstantValuesFunctionProducer)
+            IIsTenantValidFunctionInvocationFactoryEnricher tested = new IIsTenantValidFunctionInvocationFactoryEnricher(producer)
+
+        when:
+            def result = tested.enrich(context, sharedSchemaContextRequest)
+
+        then:
+            1 * producer.produce(_) >>  {
+                parameters ->
+                    capturedParameters = parameters[0]
+                    mockedSQLDefinition
+            }
+            result.getSqlDefinitions().contains(mockedSQLDefinition)
+            result.getIIsTenantValidFunctionInvocationFactory().is(mockedSQLDefinition)
+
+        and: "passed parameters should match default values"
+            capturedParameters.getSchema() == schema
+            capturedParameters.getBlacklistTenantIds() == new HashSet<String>(blacklist)
+            capturedParameters.getArgumentType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
+            capturedParameters.getFunctionName() == isTenantValidFunctionName
+
+        where:
+            schema          |   blacklist               |   isTenantValidFunctionName   |   isTenantValidConstraintName
+            null            |   ["ADFZ", "DFZCXVZ"]     |   "is_t_val"                  |   null
+            null            |   ["10.22", "9990"]       |   "is_t_val"                  |   "ten_const_va"
+            "public"        |   ["ADFZ", "DFZCXVZ"]     |   "tenant_valid"              |   "ten_const_va"
+            "public"        |   ["10.22", "9990"]       |   "tenant_valid"              |   null
+            "some_schema"   |   ["BADSF", "DSFZCV"]     |   "tenant_valid"              |   null
+            "some_schema"   |   ["10.22", "9990"]       |   "is_t_valid"                |   "ten_const_va"
+    }
+
     //TODO Setting function name
 }
