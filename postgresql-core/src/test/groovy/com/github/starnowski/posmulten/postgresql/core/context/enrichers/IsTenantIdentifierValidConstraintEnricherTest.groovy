d96d1a834062fd3ee40dbb9283efc121549f6471
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-09-27 19:31:20 +0200

    #106 - Tests code refactoring. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsTenantIdentifierValidConstraintEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/IsTenantIdentifierValidConstraintEnricherTest.groovy
@@ -1,8 +1,59 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers
 
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContext
+import com.github.starnowski.posmulten.postgresql.core.rls.IIsTenantIdentifierValidConstraintProducerParameters
+import com.github.starnowski.posmulten.postgresql.core.rls.IsTenantIdentifierValidConstraintProducer
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IIsTenantValidBasedOnConstantValuesFunctionProducerParameters
+import com.github.starnowski.posmulten.postgresql.core.rls.function.IsTenantValidBasedOnConstantValuesFunctionDefinition
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class IsTenantIdentifierValidConstraintEnricherTest extends Specification {
+
+    /*
+     * TODO
+     */
+    @Unroll
+    def "should enrich shared schema context with SQL definition for the function that checks if tenant value is correct based on default values for shares schema context builder for schema #schema and black list values (#blacklist)"()
+    {
+        given:
+        def builder = new DefaultSharedSchemaContextBuilder(schema)
+        builder.createValidTenantValueConstraint(blacklist, null, null)
+        def sharedSchemaContextRequest = builder.getSharedSchemaContextRequestCopy()
+        def context = new SharedSchemaContext()
+        IIsTenantValidBasedOnConstantValuesFunctionProducerParameters capturedParameters = null
+        def mockedSQLDefinition = Mock(IsTenantValidBasedOnConstantValuesFunctionDefinition)
+        def producer = Mock(IsTenantIdentifierValidConstraintProducer)
+        IsTenantIdentifierValidConstraintEnricher tested = new IsTenantIdentifierValidConstraintEnricher(producer)
+
+        when:
+        def result = tested.enrich(context, sharedSchemaContextRequest)
+
+        then:
+        1 * producer.produce(_) >>  {
+            parameters ->
+                capturedParameters = parameters[0]
+                mockedSQLDefinition
+        }
+        result.getSqlDefinitions().contains(mockedSQLDefinition)
+        result.getIIsTenantValidFunctionInvocationFactory().is(mockedSQLDefinition)
+
+        and: "passed parameters should match default values"
+        capturedParameters.getSchema() == schema
+        capturedParameters.getBlacklistTenantIds() == new HashSet<String>(blacklist)
+        capturedParameters.getArgumentType() == sharedSchemaContextRequest.getCurrentTenantIdPropertyType()
+        capturedParameters.getFunctionName() == "is_tenant_identifier_valid"
+
+        where:
+        schema          |   blacklist
+        null            |   ["ADFZ", "DFZCXVZ"]
+        null            |   ["10.22", "9990"]
+        "public"        |   ["ADFZ", "DFZCXVZ"]
+        "public"        |   ["10.22", "9990"]
+        "some_schema"   |   ["BADSF", "DSFZCV"]
+        "some_schema"   |   ["10.22", "9990"]
+    }
 
     static class IsTenantIdentifierValidConstraintProducerKey
     {
@@ -31,6 +82,11 @@ class IsTenantIdentifierValidConstraintEnricherTest extends Specification {
             this.tableName = tableName
             this.tableSchema = tableSchema
             this.tenantColumnName = tenantColumnName
+        }
+
+        static IsTenantIdentifierValidConstraintProducerKey map(IIsTenantIdentifierValidConstraintProducerParameters parameters)
+        {
+            new IsTenantIdentifierValidConstraintProducerKey(parameters.getConstraintName(), parameters.getTableName(), parameters.getTableSchema(), parameters.getTenantColumnName())
         }
 
         boolean equals(o) {
