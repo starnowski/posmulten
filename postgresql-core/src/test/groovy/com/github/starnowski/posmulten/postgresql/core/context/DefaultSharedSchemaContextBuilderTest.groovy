58dc8a96fd5c0798be3910010ac96ec78c996402
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-10-03 02:11:56 +0200

    #106 - Added more tests coverage. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderTest.groovy
@@ -1,5 +1,6 @@
 package com.github.starnowski.posmulten.postgresql.core.context
 
+import com.github.starnowski.posmulten.postgresql.core.context.enrichers.ISharedSchemaContextEnricher
 import spock.lang.Specification
 
 class DefaultSharedSchemaContextBuilderTest extends Specification {
@@ -16,5 +17,47 @@ class DefaultSharedSchemaContextBuilderTest extends Specification {
 
         then:
             result == null
+    }
+
+    def "should return empty list of enrichers when list has value null"()
+    {
+        given:
+            def tested = new DefaultSharedSchemaContextBuilder()
+                .setEnrichers(null)
+
+        when:
+            def result = tested.getEnrichersCopy()
+
+        then:
+            result != null
+            result.isEmpty()
+    }
+
+    def "should return always new copy of enrichers list"()
+    {
+        given:
+        def enricher = Mock(ISharedSchemaContextEnricher)
+        def tested = new DefaultSharedSchemaContextBuilder()
+                .setEnrichers([enricher])
+
+        when:
+        def result1 = tested.getEnrichersCopy()
+        def result2 = tested.getEnrichersCopy()
+
+        then:
+            result1 != null
+            result2 != null
+            !result1.is(result2)
+
+        and: "list has correct size"
+            result1.size() == 1
+            result2.size() == 1
+
+        and: "list should contains reference to passed enricher"
+            result1.contains(enricher)
+            result2.contains(enricher)
+
+        and: "returned lists should be equal"
+            result1 == result2
     }
 }
