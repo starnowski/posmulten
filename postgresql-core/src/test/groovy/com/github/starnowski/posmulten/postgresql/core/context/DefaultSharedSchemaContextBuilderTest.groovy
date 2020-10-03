1589b648ec718cf213d47dbf4808baae7f82123b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-10-03 02:17:23 +0200

    #106 - Added more tests coverage. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderTest.groovy
@@ -1,6 +1,7 @@
 package com.github.starnowski.posmulten.postgresql.core.context
 
 import com.github.starnowski.posmulten.postgresql.core.context.enrichers.ISharedSchemaContextEnricher
+import com.github.starnowski.posmulten.postgresql.core.context.validators.ISharedSchemaContextRequestValidator
 import spock.lang.Specification
 
 class DefaultSharedSchemaContextBuilderTest extends Specification {
@@ -56,6 +57,48 @@ class DefaultSharedSchemaContextBuilderTest extends Specification {
         and: "list should contains reference to passed enricher"
             result1.contains(enricher)
             result2.contains(enricher)
+
+        and: "returned lists should be equal"
+            result1 == result2
+    }
+
+    def "should return empty list of validators when list has value null"()
+    {
+        given:
+            def tested = new DefaultSharedSchemaContextBuilder()
+                    .setValidators(null)
+
+        when:
+            def result = tested.getValidatorsCopy()
+
+        then:
+            result != null
+            result.isEmpty()
+    }
+
+    def "should return always new copy of validators list"()
+    {
+        given:
+            def validator = Mock(ISharedSchemaContextRequestValidator)
+            def tested = new DefaultSharedSchemaContextBuilder()
+                    .setValidators([validator])
+
+        when:
+            def result1 = tested.getValidatorsCopy()
+            def result2 = tested.getValidatorsCopy()
+
+        then:
+            result1 != null
+            result2 != null
+            !result1.is(result2)
+
+        and: "list has correct size"
+            result1.size() == 1
+            result2.size() == 1
+
+        and: "list should contains reference to passed validator"
+            result1.contains(validator)
+            result2.contains(validator)
 
         and: "returned lists should be equal"
             result1 == result2
