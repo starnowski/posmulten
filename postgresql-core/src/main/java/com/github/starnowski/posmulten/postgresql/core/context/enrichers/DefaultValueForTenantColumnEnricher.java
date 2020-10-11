92daac8643f633c7f304134f3da9d81b2b46a19a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-10-11 14:56:24 +0200

    #106 - Added basic types. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/DefaultValueForTenantColumnEnricher.java
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
+
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+
+public class DefaultValueForTenantColumnEnricher implements ISharedSchemaContextEnricher {
+    @Override
+    public ISharedSchemaContext enrich(ISharedSchemaContext context, SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
+        return null;
+    }
+}
