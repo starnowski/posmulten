ff23386bf5009053da8557d9a0ab289837012369
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-10-19 22:04:57 +0200

    #106 - Code refactoring. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/DefaultValueForTenantColumnEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/DefaultValueForTenantColumnEnricher.java
@@ -1,10 +1,22 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
+import com.github.starnowski.posmulten.postgresql.core.SetDefaultStatementProducer;
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
 import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
 
 public class DefaultValueForTenantColumnEnricher implements ISharedSchemaContextEnricher {
+
+    private final SetDefaultStatementProducer producer;
+
+    public DefaultValueForTenantColumnEnricher() {
+        this(new SetDefaultStatementProducer());
+    }
+
+    public DefaultValueForTenantColumnEnricher(SetDefaultStatementProducer producer) {
+        this.producer = producer;
+    }
+
     @Override
     public ISharedSchemaContext enrich(ISharedSchemaContext context, SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
         return null;
