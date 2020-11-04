ea5ebfb8ede8a92cd02b227eae8e2daeae440357
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-04 01:49:27 +0100

    #106 - Added test. [skip travis]

---
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest.groovy
@@ -0,0 +1,8 @@
+package com.github.starnowski.posmulten.postgresql.core.context.validators
+
+import spock.lang.Specification
+
+class CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest extends Specification {
+
+
+}
