76043ec50a92cf1105c9cb2a6795d0f0a6b7c210
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-04 01:08:16 +0100

    #106 - Added validator. [skip travis]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidator.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posmulten.postgresql.core.context.validators;
+
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+
+public class CreateTenantColumnTableMappingSharedSchemaContextRequestValidator implements ISharedSchemaContextRequestValidator{
+    @Override
+    public void validate(SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
+        //TODO
+    }
+}
