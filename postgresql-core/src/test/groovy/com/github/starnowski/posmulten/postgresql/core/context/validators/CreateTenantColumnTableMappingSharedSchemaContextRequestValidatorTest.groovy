c29e56cc70643646dfc26e85c857b7380aedf322
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-08 23:03:19 +0100

    #106 - Added tests cases. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest.groovy
@@ -1,8 +1,35 @@
 package com.github.starnowski.posmulten.postgresql.core.context.validators
 
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest extends Specification {
 
+    def tested = new CreateTenantColumnTableMappingSharedSchemaContextRequestValidator()
+
+    @Unroll
+    def "should not throw any exception when all tables that required tenant column creation has reference in map for tables that required rls policy creation for tests parameters: #foreignKeysTable, #primaryKeysTable, #foreignKeysMapping, #primayKeyTypeDefinition"()
+    {
+        given:
+            DefaultSharedSchemaContextBuilder builder = new DefaultSharedSchemaContextBuilder()
+            builder.createRLSPolicyForTable(primaryKeysTable, primayKeyTypeDefinition, null, null)
+            builder.createTenantColumnForTable(primaryKeysTable)
+            SharedSchemaContextRequest request = builder.getSharedSchemaContextRequestCopy()
+
+        when:
+            tested.validate(request)
+
+        then:
+            noExceptionThrown()
+
+        where:
+            foreignKeysTable    |   primaryKeysTable    |   primayKeyTypeDefinition
+            "comments"          |   "users"             |   [id: null]
+            "posts"             |   "users"             |   [uuid: null]
+            "posts"             |   "comments"          |   [comment_id: null]
+            "posts"             |   "comments"          |   [comment_id: null, user: null]
+    }
 
 }
