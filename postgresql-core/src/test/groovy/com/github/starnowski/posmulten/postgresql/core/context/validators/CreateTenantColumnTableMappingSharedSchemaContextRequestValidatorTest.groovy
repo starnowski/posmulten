9705603119c0b52c76111f52538d0cc549ccab47
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-11 20:01:12 +0100

    #106 - Added tests cases. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest.groovy
@@ -2,6 +2,9 @@ package com.github.starnowski.posmulten.postgresql.core.context.validators
 
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest
+import com.github.starnowski.posmulten.postgresql.core.context.TableKey
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.IncorrectForeignKeysMappingException
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.MissingRLSPolicyDeclarationForTableException
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -30,6 +33,38 @@ class CreateTenantColumnTableMappingSharedSchemaContextRequestValidatorTest exte
             "posts"             |   "users"             |   [uuid: null]
             "posts"             |   "comments"          |   [comment_id: null]
             "posts"             |   "comments"          |   [comment_id: null, user: null]
+    }
+
+    @Unroll
+    def "should throw exception when there is missing rls policy declaration for table #tableRequiredTenantColumnCreation in schema #schema for which creation of tenant column was requested when there are only rls policy declaration for tables #rlsTables"()
+    {
+        given:
+            DefaultSharedSchemaContextBuilder builder = new DefaultSharedSchemaContextBuilder(schema)
+            for (String table : rlsTables)
+            {
+                builder.createRLSPolicyForTable(rlsTables, [id: null], null, null)
+            }
+            builder.createTenantColumnForTable(tableRequiredTenantColumnCreation)
+            SharedSchemaContextRequest request = builder.getSharedSchemaContextRequestCopy()
+
+        when:
+            tested.validate(request)
+
+        then:
+            def ex = thrown(MissingRLSPolicyDeclarationForTableException)
+
+        and: "exception should have correct message"
+            ex.message == expectedMessage
+
+        and: "exception should have correctly set table key"
+            ex.tableKey == new TableKey(tableRequiredTenantColumnCreation, schema)
+
+        where:
+            schema          |   tableRequiredTenantColumnCreation       |   rlsTables                   ||   expectedMessage
+            null            |   "comments"                              |   ["users", "company"]        ||  "Missing RLS policy declaration for table comments in schema null for which creation of tenant column was requested"
+            null            |   "posts"                                 |   ["users", "company"]        ||  "Missing RLS policy declaration for table user_notification in schema null for which creation of tenant column was requested"
+            "some_schema"   |   "users"                                 |   ["posts", "company"]        ||  "Missing RLS policy declaration for table users posts schema some_schema for which creation of tenant column was requested"
+            "some_schema"   |   "comments"                              |   ["users", "company"]        ||  "Missing RLS policy declaration for table comments in schema some_schema for which creation of tenant column was requested"
     }
 
 }
