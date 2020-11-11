8270685ac789a9e8a1dadfbd42237c27729e8035
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-11 20:24:43 +0100

    #106 - Added implementation. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/CreateTenantColumnTableMappingSharedSchemaContextRequestValidator.java
@@ -1,11 +1,26 @@
 package com.github.starnowski.posmulten.postgresql.core.context.validators;
 
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
-import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import com.github.starnowski.posmulten.postgresql.core.context.TableKey;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.MissingRLSPolicyDeclarationForTableThatRequiredTenatColumnCreationException;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static java.lang.String.format;
 
 public class CreateTenantColumnTableMappingSharedSchemaContextRequestValidator implements ISharedSchemaContextRequestValidator{
     @Override
-    public void validate(SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
-        //TODO
+    public void validate(SharedSchemaContextRequest request) throws MissingRLSPolicyDeclarationForTableThatRequiredTenatColumnCreationException {
+        if (!request.getCreateTenantColumnTableLists().isEmpty())
+        {
+            Set<TableKey> rlsTables = request.getTableColumnsList().keySet();
+            Optional<TableKey> tableWithRLSPolicyDeclaration = request.getCreateTenantColumnTableLists().stream().filter(tableKey -> !rlsTables.contains(tableKey)).findFirst();
+            if (tableWithRLSPolicyDeclaration.isPresent())
+            {
+                TableKey table = tableWithRLSPolicyDeclaration.get();
+                throw new MissingRLSPolicyDeclarationForTableThatRequiredTenatColumnCreationException(table, format("Missing RLS policy declaration for table %1$s in schema %2$s for which creation of tenant column was requested", table.getTable(), table.getSchema()));
+            }
+        }
     }
 }
