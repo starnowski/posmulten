22fd2cf4d0fc2f0e337abb3b65f353595d06e983
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-11 21:25:19 +0100

    #106 - Added validator. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/TablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedSharedSchemaContextRequestValidator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/TablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedSharedSchemaContextRequestValidator.java
@@ -1,12 +1,26 @@
 package com.github.starnowski.posmulten.postgresql.core.context.validators;
 
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
-import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import com.github.starnowski.posmulten.postgresql.core.context.TableKey;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.MissingRLSPolicyDeclarationForTablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedException;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static java.lang.String.format;
 
 public class TablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedSharedSchemaContextRequestValidator implements ISharedSchemaContextRequestValidator{
     @Override
-    public void validate(SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
-        //TODO
+    public void validate(SharedSchemaContextRequest request) throws MissingRLSPolicyDeclarationForTablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedException {
+        if (!request.getTablesThatAddingOfTenantColumnDefaultValueShouldBeSkipped().isEmpty())
+        {
+            Set<TableKey> rlsTables = request.getTableColumnsList().keySet();
+            Optional<TableKey> tableWithRLSPolicyDeclaration = request.getTablesThatAddingOfTenantColumnDefaultValueShouldBeSkipped().stream().filter(tableKey -> !rlsTables.contains(tableKey)).findFirst();
+            if (tableWithRLSPolicyDeclaration.isPresent())
+            {
+                TableKey table = tableWithRLSPolicyDeclaration.get();
+                throw new MissingRLSPolicyDeclarationForTablesThatAddingOfTenantColumnDefaultValueShouldBeSkippedException(table, format("Missing RLS policy declaration for table %1$s in schema %2$s for which creation of tenant column should be skipped", table.getTable(), table.getSchema()));
+            }
+        }
     }
-
 }
