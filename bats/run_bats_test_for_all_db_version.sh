2a97173adcf711462e05b22d87d2ea65ac118211
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-11-13 18:37:54 +0100

    #125 - Displaying message before tests. [skip travis]

--- bats/run_bats_test_for_all_db_version.sh
+++ bats/run_bats_test_for_all_db_version.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 DIRNAME="$(dirname $0)"
+echo "Running tests for postgres with version 9.6"
 "$DIRNAME"/run_bats_test.sh
+echo "Running tests for postgres with version 10"
 "$DIRNAME"/run_bats_test.sh --postgres_docker_version 10.14
+echo "Running tests for postgres with version 11"
 "$DIRNAME"/run_bats_test.sh --postgres_docker_version 11.9
+echo "Running tests for postgres with version 12"
 "$DIRNAME"/run_bats_test.sh --postgres_docker_version 12.4
+echo "Running tests for postgres with version 13"
 "$DIRNAME"/run_bats_test.sh --postgres_docker_version 13.0
