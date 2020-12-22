854f6be0d607398151b5ac1fc371aae48edeb06f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-22 10:35:29 +0100

    #138 - Added tests case that checks if constructor creates new map object based on passed map parameter. [skip travis]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultTableColumnsTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultTableColumnsTest.groovy
@@ -1,6 +1,7 @@
 package com.github.starnowski.posmulten.postgresql.core.context
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class DefaultTableColumnsTest extends Specification {
 
@@ -8,5 +9,22 @@ class DefaultTableColumnsTest extends Specification {
     {
         expect:
             new DefaultTableColumns("table", null).getIdentityColumnNameAndTypeMap().isEmpty()
+    }
+
+    @Unroll
+    def "constructor should create new map object based on passed object (#map)"()
+    {
+        given:
+            def tested = new DefaultTableColumns("table", map)
+
+        when:
+            def result = tested.getIdentityColumnNameAndTypeMap()
+
+        then:
+            result == map
+            !result.is(map)
+
+        where:
+            map << [[:], [id:"bigint"], [user_uid: "UUID", id:"bigint"]]
     }
 }
