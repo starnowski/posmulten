36edded8cd3d0a2b3a3382f9906160e0b4932116
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-25 23:36:53 +0100

    #155 - Added dao object [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDao.java
@@ -0,0 +1,17 @@
+package com.github.starnowski.posmulten.configuration.yaml.dao;
+
+import com.github.starnowski.posmulten.configuration.yaml.model.SharedSchemaContextConfiguration;
+
+public class SharedSchemaContextConfigurationYamlDao {
+
+    public SharedSchemaContextConfiguration read(String filePath)
+    {
+        //TODO
+        return null;
+    }
+
+    public void save(SharedSchemaContextConfiguration configuration, String filePath)
+    {
+        //TODO
+    }
+}
