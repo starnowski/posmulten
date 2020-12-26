7e75ce5c3896ffff9b5ee9eb65331c1cd0d73f6e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-26 01:43:53 +0100

    #155 - Added method that reads yaml file [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDao.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDao.java
@@ -1,13 +1,18 @@
 package com.github.starnowski.posmulten.configuration.yaml.dao;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.starnowski.posmulten.configuration.yaml.model.SharedSchemaContextConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SharedSchemaContextConfigurationYamlDao {
 
-    public SharedSchemaContextConfiguration read(String filePath)
-    {
-        //TODO
-        return null;
+    private final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+    public SharedSchemaContextConfiguration read(String filePath) throws IOException {
+        return mapper.readValue(new File(filePath), SharedSchemaContextConfiguration.class);
     }
 
     public void save(SharedSchemaContextConfiguration configuration, String filePath)
