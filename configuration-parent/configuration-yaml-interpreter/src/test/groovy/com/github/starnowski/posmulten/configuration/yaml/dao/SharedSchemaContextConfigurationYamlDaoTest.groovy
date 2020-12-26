0c9865cc53a2c9884c424efa3d8fefb2c77c9ced
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-26 01:00:24 +0100

    #155 - Added tests cases [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoTest.groovy
@@ -1,6 +1,23 @@
 package com.github.starnowski.posmulten.configuration.yaml.dao
 
+import spock.lang.Unroll
+
 class SharedSchemaContextConfigurationYamlDaoTest extends spock.lang.Specification {
 
+    public static final ALL_FIELDS_FILE_PATH = "all-fields.yaml"
 
+    def tested = new SharedSchemaContextConfigurationYamlDao()
+
+    @Unroll
+    def "should return non null object based on file #filePath"()
+    {
+        when:
+            def result = tested.read(filePath)
+
+        then:
+            result
+
+        where:
+            filePath << [ALL_FIELDS_FILE_PATH]
+    }
 }
