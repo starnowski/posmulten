8344396218ae7c3698286b88a187cc2fa83b953f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-26 01:53:15 +0100

    #155 - Added definition for property "default_schema" [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/model/SharedSchemaContextConfiguration.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/model/SharedSchemaContextConfiguration.java
@@ -1,6 +1,20 @@
 package com.github.starnowski.posmulten.configuration.yaml.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class SharedSchemaContextConfiguration {
 
+    @JsonProperty(value = "default_schema", required = true)
     private String defaultSchema;
+    private String currentTenantIdPropertyType;//current_tenant_id_property_type
 }
