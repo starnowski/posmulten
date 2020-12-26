755ac97699c829a1ec891cee5764b3962a812195
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-26 01:16:20 +0100

    #155 - Tests code refactoring [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoTest.groovy
@@ -2,17 +2,22 @@ package com.github.starnowski.posmulten.configuration.yaml.dao
 
 import spock.lang.Unroll
 
+import java.nio.file.Paths
+
 class SharedSchemaContextConfigurationYamlDaoTest extends spock.lang.Specification {
 
-    public static final ALL_FIELDS_FILE_PATH = "all-fields.yaml"
+    public static final ALL_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/all-fields.yaml"
 
     def tested = new SharedSchemaContextConfigurationYamlDao()
 
     @Unroll
     def "should return non null object based on file #filePath"()
     {
+        given:
+            def resolvedPath = Paths.get(this.class.getResource(ALL_FIELDS_FILE_PATH).toURI()).toFile().getPath()
+
         when:
-            def result = tested.read(filePath)
+            def result = tested.read(resolvedPath)
 
         then:
             result
