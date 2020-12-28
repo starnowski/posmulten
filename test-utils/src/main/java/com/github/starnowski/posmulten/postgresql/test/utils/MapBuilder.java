9cd25d0b9d04dde1ad10b39fe988fe7796800a06
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-28 20:04:31 +0100

    #155 - Added MapBuilder type. [skip travis]

---
+++ test-utils/src/main/java/com/github/starnowski/posmulten/postgresql/test/utils/MapBuilder.java
@@ -0,0 +1,25 @@
+package com.github.starnowski.posmulten.postgresql.test.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapBuilder <K, V>
+{
+    private Map<K, V> map = new HashMap<>();
+
+    public MapBuilder put(K key, V value)
+    {
+        map.put(key, value);
+        return this;
+    }
+
+    public Map<K, V> build()
+    {
+        return map;
+    }
+
+    public static <K, V> MapBuilder<K, V> mapBuilder()
+    {
+        return new MapBuilder<>();
+    }
+}
