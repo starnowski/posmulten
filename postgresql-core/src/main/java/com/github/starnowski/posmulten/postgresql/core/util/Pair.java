f5e363ac158b0e6b2eaf02040092c3c707efc27d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-30 17:32:53 +0100

    #165 - Added the toString() method. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/util/Pair.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/util/Pair.java
@@ -43,4 +43,13 @@ public final class Pair<L, R> {
     public int hashCode() {
         return Objects.hash(left, right);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(left);
+        sb.append("=");
+        sb.append(right);
+        return sb.toString();
+    }
 }
