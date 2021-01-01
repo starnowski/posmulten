de8ad868c0f11cb9b68411d565179bd52c6006fd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-02 00:00:47 +0100

    #156 - Added DefaultSharedSchemaContextBuilderConfigurationEnricher type. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/model/DefaultSharedSchemaContextBuilderConfigurationEnricher.java
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.configuration.core.model;
+
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+
+public class DefaultSharedSchemaContextBuilderConfigurationEnricher {
+
+    public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, SharedSchemaContextConfiguration contextConfiguration)
+    {
+        //TODO
+        return null;
+    }
+}
