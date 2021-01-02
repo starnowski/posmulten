3eff6a30c953d9d2a53ebc7da5a0b797e9468d18
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 00:13:10 +0100

    #156 - Added enricher types. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/ValidTenantValueConstraintConfigurationEnricher.java
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.configuration.core;
+
+import com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration;
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+
+public class ValidTenantValueConstraintConfigurationEnricher {
+
+    public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, ValidTenantValueConstraintConfiguration configuration) {
+        //TODO
+        return null;
+    }
+}
