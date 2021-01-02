03e48656126f12b6d3748a29a7fa9b04415973a3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-02 15:43:10 +0100

    #156 - Moved the DefaultSharedSchemaContextBuilderConfigurationEnricher type to correct package. [skip travis]

---
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/DefaultSharedSchemaContextBuilderConfigurationEnricherTest.groovy
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.configuration.core
+
+import spock.lang.Specification
+
+class DefaultSharedSchemaContextBuilderConfigurationEnricherTest extends Specification {
+
+
+    def "should "()
+    {
+
+    }
+}
