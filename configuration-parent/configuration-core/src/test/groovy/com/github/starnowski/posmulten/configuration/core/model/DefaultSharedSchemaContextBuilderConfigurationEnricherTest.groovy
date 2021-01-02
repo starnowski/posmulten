629d3845baa9b3ed1f2e1118c8d03b4676492f24
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-02 19:57:51 +0100

    #156 - Added tests assertions. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/model/DefaultSharedSchemaContextBuilderConfigurationEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/model/DefaultSharedSchemaContextBuilderConfigurationEnricherTest.groovy
@@ -13,7 +13,7 @@ class DefaultSharedSchemaContextBuilderConfigurationEnricherTest extends Specifi
     def "should set builder component with specific properties currentTenantIdPropertyType (#currentTenantIdPropertyType), currentTenantIdProperty (#currentTenantIdProperty), getCurrentTenantIdFunctionName (#getCurrentTenantIdFunctionName), setCurrentTenantIdFunctionName (#setCurrentTenantIdFunctionName), equalsCurrentTenantIdentifierFunctionName (#equalsCurrentTenantIdentifierFunctionName)"()
     {
         given:
-            def builder = Mock(DefaultSharedSchemaContextBuilder)
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
             def configuration = new SharedSchemaContextConfiguration()
             .setCurrentTenantIdPropertyType(currentTenantIdPropertyType)
             .setCurrentTenantIdProperty(currentTenantIdProperty)
@@ -37,5 +37,16 @@ class DefaultSharedSchemaContextBuilderConfigurationEnricherTest extends Specifi
             currentTenantIdPropertyType |   currentTenantIdProperty |   getCurrentTenantIdFunctionName  |   setCurrentTenantIdFunctionName  |   equalsCurrentTenantIdentifierFunctionName
             "VARCHAR(37)"               |   "customer_d"            |   "what_is_tenant_id"             |   "tenant_is_now"                 |   "is_it_this_tenant_id"
             "UUID"                      |   "tenantId"              |   "get_current_t"                 |   "set_t"                         |   "equals_tenant"
+    }
+
+    private DefaultSharedSchemaContextBuilder prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+    {
+        def builder = Mock(DefaultSharedSchemaContextBuilder)
+        0 * builder.setCurrentTenantIdProperty(_)
+        0 * builder.setCurrentTenantIdPropertyType(_)
+        0 * builder.setGetCurrentTenantIdFunctionName(_)
+        0 * builder.setSetCurrentTenantIdFunctionName(_)
+        0 * builder.setEqualsCurrentTenantIdentifierFunctionName(_)
+        builder
     }
 }
