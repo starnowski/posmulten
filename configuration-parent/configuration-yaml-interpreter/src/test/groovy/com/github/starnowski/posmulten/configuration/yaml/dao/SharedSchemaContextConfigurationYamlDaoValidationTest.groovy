285033470e6b8f58044072c88d18d32e175613ec
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 16:59:58 +0100

    #168 - Removed tests cases. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoValidationTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoValidationTest.groovy
@@ -10,7 +10,6 @@ class SharedSchemaContextConfigurationYamlDaoValidationTest extends spock.lang.S
     public static final INVALID_ROOT_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-root-node-blank-fields.yaml"
     public static final INVALID_NESTED_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-blank-fields.yaml"
     public static final INVALID_NESTED_NODE_EMPTY_LIST_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-empty-list.yaml"
-    public static final INVALID_NESTED_NODE_NULL_LIST_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-null-list.yaml"
 
     def tested = new SharedSchemaContextConfigurationYamlDao()
 
@@ -47,7 +46,7 @@ class SharedSchemaContextConfigurationYamlDaoValidationTest extends spock.lang.S
             ex.getErrorMessages().contains(errorMessage)
 
         where:
-            errorMessage << ["valid_tenant_value_constraint.is_tenant_valid_function_name must not be blank", "valid_tenant_value_constraint.is_tenant_valid_constraint_name must not be blank"]
+            errorMessage << ["valid_tenant_value_constraint.is_tenant_valid_function_name must not be blank", "valid_tenant_value_constraint.is_tenant_valid_constraint_name must not be blank", "valid_tenant_value_constraint.tenant_identifiers_blacklist must not be null"]
     }
 
     @Unroll
@@ -66,24 +65,6 @@ class SharedSchemaContextConfigurationYamlDaoValidationTest extends spock.lang.S
 
         where:
             errorMessage << ["valid_tenant_value_constraint.tenant_identifiers_blacklist must have at least one element"]
-    }
-
-    @Unroll
-    def "should throw exception that contains error message (#errorMessage) for file invalid-nested-node-null-list.yaml"()
-    {
-        given:
-            def resolvedPath = resolveFilePath(INVALID_NESTED_NODE_NULL_LIST_FILE_PATH)
-
-        when:
-            tested.read(resolvedPath)
-
-        then:
-            def ex = thrown(YamlInvalidSchema)
-            ex
-            ex.getErrorMessages().contains(errorMessage)
-
-        where:
-            errorMessage << ["valid_tenant_value_constraint.tenant_identifiers_blacklist must not be null"]
     }
 
     private String resolveFilePath(String filePath) {
