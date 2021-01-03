1c3f6e35c936e8f3dd177e1765ced670806699f2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 16:17:25 +0100

    #168 - Added tests data [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoValidationTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDaoValidationTest.groovy
@@ -8,6 +8,7 @@ import java.nio.file.Paths
 class SharedSchemaContextConfigurationYamlDaoValidationTest extends spock.lang.Specification {
 
     public static final INVALID_ROOT_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-root-node-blank-fields.yaml"
+    public static final INVALID_NESTED_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-blank-fields.yaml"
 
     def tested = new SharedSchemaContextConfigurationYamlDao()
 
@@ -27,6 +28,24 @@ class SharedSchemaContextConfigurationYamlDaoValidationTest extends spock.lang.S
 
         where:
             errorMessage << ["grantee must not be blank", "default_schema must not be blank", "get_current_tenant_id_function_name must not be blank", "set_current_tenant_id_function_name must not be blank", "equals_current_tenant_identifier_function_name must not be blank"]
+    }
+
+    @Unroll
+    def "should throw exception that contains error message (#errorMessage) for file invalid-nested-node-blank-fields.yaml"()
+    {
+        given:
+            def resolvedPath = resolveFilePath(INVALID_NESTED_NODE_BLANK_FIELDS_FILE_PATH)
+
+        when:
+            tested.read(resolvedPath)
+
+        then:
+            def ex = thrown(YamlInvalidSchema)
+            ex
+            ex.getErrorMessages().contains(errorMessage)
+
+        where:
+            errorMessage << ["valid_tenant_value_constraint.is_tenant_valid_function_name must not be blank", "valid_tenant_value_constraint.is_tenant_valid_constraint_name must not be blank", "valid_tenant_value_constraint.tenant_identifiers_blacklist must not be blank"]
     }
 
     private String resolveFilePath(String filePath) {
