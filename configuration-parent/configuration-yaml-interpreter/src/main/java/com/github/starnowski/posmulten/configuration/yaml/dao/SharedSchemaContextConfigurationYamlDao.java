51a516f06eb455e202f7fab6c8cfafc48c72475d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 15:02:06 +0100

    #168 - Code refactoring. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDao.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/dao/SharedSchemaContextConfigurationYamlDao.java
@@ -2,6 +2,7 @@ package com.github.starnowski.posmulten.configuration.yaml.dao;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.github.starnowski.posmulten.configuration.yaml.exceptions.YamlInvalidSchema;
 import com.github.starnowski.posmulten.configuration.yaml.model.SharedSchemaContextConfiguration;
 
 import javax.validation.ConstraintViolation;
@@ -10,7 +11,12 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class SharedSchemaContextConfigurationYamlDao {
 
@@ -27,25 +33,24 @@ public class SharedSchemaContextConfigurationYamlDao {
         mapper.writeValue(new File(filePath), configuration);
     }
 
-    private void validateConfigurationObject(SharedSchemaContextConfiguration configuration)
-    {
+    private void validateConfigurationObject(SharedSchemaContextConfiguration configuration) {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
         Set<ConstraintViolation<SharedSchemaContextConfiguration>> errors = validator.validate(configuration);
-        if (!errors.isEmpty())
-        {
-            throw new RuntimeException(prepareErrorMessage(errors));
+        if (!errors.isEmpty()) {
+            throw new YamlInvalidSchema(prepareErrorsMessages(errors));
         }
     }
 
-    private String prepareErrorMessage(Set<ConstraintViolation<SharedSchemaContextConfiguration>> errors)
-    {
+    private List<String> prepareErrorsMessages(Set<ConstraintViolation<SharedSchemaContextConfiguration>> errors) {
+        return errors.stream().map(error -> prepareErrorMessage(error)).collect(toList());
+    }
+
+    private String prepareErrorMessage(ConstraintViolation<SharedSchemaContextConfiguration> error) {
         StringBuilder sb = new StringBuilder();
-        for (ConstraintViolation<SharedSchemaContextConfiguration> error : errors) {
-            sb.append(error.getPropertyPath());
-            sb.append(" ");
-            sb.append(error.getMessage());
-        }
+        sb.append(error.getPropertyPath());
+        sb.append(" ");
+        sb.append(error.getMessage());
         return sb.toString();
     }
 }
