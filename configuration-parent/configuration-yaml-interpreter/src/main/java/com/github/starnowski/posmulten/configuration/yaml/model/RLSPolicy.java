2e278c15753b572f43e302f8339bab06b1cbd6ee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 13:29:47 +0100

    #168 - Code refactoring. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/model/RLSPolicy.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/model/RLSPolicy.java
@@ -21,9 +21,8 @@ public class RLSPolicy {
     @NotBlank
     @JsonProperty(value = "name", required = true)
     private String name;
-    @NotBlank
     @JsonProperty(value = "tenant_column")
-    private String tenantColumn;
+    private StringWrapperWithNotBlankValue tenantColumn;
     @JsonProperty(value = "pk_columns_name_to_type")
     private Map<String, String> primaryKeyColumnsNameToTypeMap;
     @JsonProperty(value = "create_tenant_column_for_table")
@@ -31,9 +30,18 @@ public class RLSPolicy {
     @NotBlank
     @JsonProperty(value = "name_for_function_that_checks_if_record_exists_in_table", required = true)
     private String nameForFunctionThatChecksIfRecordExistsInTable;
-    @NotBlank
     @JsonProperty(value = "valid_tenant_value_constraint_name")
-    private String validTenantValueConstraintName;
+    private StringWrapperWithNotBlankValue validTenantValueConstraintName;
     @JsonProperty(value = "skip_adding_of_tenant_column_default_value")
     private Boolean skipAddingOfTenantColumnDefaultValue;
+
+    public RLSPolicy setTenantColumn(String tenantColumn) {
+        this.tenantColumn = new StringWrapperWithNotBlankValue(tenantColumn);
+        return this;
+    }
+
+    public RLSPolicy setValidTenantValueConstraintName(String validTenantValueConstraintName) {
+        this.validTenantValueConstraintName = new StringWrapperWithNotBlankValue(validTenantValueConstraintName);
+        return this;
+    }
 }
