af60d8d0c969d4abbcba9d84946b6fdb4bcc3d1a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 14:46:51 +0100

    #168 - Code refactoring. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/exceptions/YamlInvalidSchema.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/exceptions/YamlInvalidSchema.java
@@ -2,19 +2,23 @@ package com.github.starnowski.posmulten.configuration.yaml.exceptions;
 
 import java.util.List;
 
+import static java.util.Collections.unmodifiableList;
 import static java.util.stream.Collectors.joining;
 
-public class YamlInvalidSchema extends RuntimeException{
-
-    public YamlInvalidSchema(List<String> errorMessages) {
-        super(prepareExceptionMessage(errorMessages));
-        this.errorMessages = errorMessages;
-    }
+public class YamlInvalidSchema extends RuntimeException {
 
     private final List<String> errorMessages;
 
-    private static String prepareExceptionMessage(List<String> errorMessages)
-    {
+    public YamlInvalidSchema(List<String> errorMessages) {
+        super(prepareExceptionMessage(errorMessages));
+        this.errorMessages = unmodifiableList(errorMessages);
+    }
+
+    public List<String> getErrorMessages() {
+        return errorMessages;
+    }
+
+    private static String prepareExceptionMessage(List<String> errorMessages) {
         return errorMessages.stream().collect(joining(", "));
     }
 
