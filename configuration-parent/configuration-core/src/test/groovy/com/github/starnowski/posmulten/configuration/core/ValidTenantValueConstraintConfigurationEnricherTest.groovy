b1d4ad1938cb01c90c53e01acfb8f16fd3d79e4f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-03 01:50:56 +0100

    #156 - Added tests cases. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/ValidTenantValueConstraintConfigurationEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/ValidTenantValueConstraintConfigurationEnricherTest.groovy
@@ -1,6 +1,35 @@
 package com.github.starnowski.posmulten.configuration.core
 
+
+import com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration
+import spock.lang.Unroll
+
 class ValidTenantValueConstraintConfigurationEnricherTest extends AbstractBaseTest {
 
+    def tested = new ValidTenantValueConstraintConfigurationEnricher()
 
+    @Unroll
+    def "should set builder component with specific properties tenantValuesBlacklist (#tenantValuesBlacklist), isTenantValidFunctionName (#isTenantValidFunctionName), isTenantValidConstraintName (#isTenantValidConstraintName)"()
+    {
+        given:
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+            def configuration = new ValidTenantValueConstraintConfiguration()
+                    .setTenantIdentifiersBlacklist(tenantValuesBlacklist)
+                    .setIsTenantValidFunctionName(isTenantValidFunctionName)
+                    .setIsTenantValidConstraintName(isTenantValidConstraintName)
+
+        when:
+            def result = tested.enrich(builder, configuration)
+
+        then:
+            result == builder
+            1 * builder.createValidTenantValueConstraint(tenantValuesBlacklist, isTenantValidFunctionName, isTenantValidConstraintName)
+
+
+        where:
+            tenantValuesBlacklist   |   isTenantValidFunctionName   |   isTenantValidConstraintName
+            ["invalid", "value1"]   |   "is_t_f"                    |   "is_t_c"
+            []                      |   null                        |   null
+            null                    |   null                        |   null
+    }
 }
