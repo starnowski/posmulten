ec77c64addc466b1979c49f862ef33fc22681dfb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-04 19:02:59 +0100

    #156 - Added constructor. [skip travis]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/TablesEntriesEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/TablesEntriesEnricher.java
@@ -3,14 +3,22 @@ package com.github.starnowski.posmulten.configuration.core;
 import com.github.starnowski.posmulten.configuration.core.model.TableEntry;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
 
 public class TablesEntriesEnricher {
 
     private final List<ITableEntryEnricher> enrichers;
 
+    public TablesEntriesEnricher()
+    {
+        this(Arrays.asList(new RLSPolicyConfigurationEnricher(), new ForeignKeyConfigurationsEnricher()));
+    }
+
     public TablesEntriesEnricher(List<ITableEntryEnricher> enrichers) {
-        this.enrichers = enrichers;
+        this.enrichers = unmodifiableList(enrichers);
     }
 
     public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, List<TableEntry> tableEntries) {
