df53da35228ebe3b26e63007664cd3d3984ffcb8
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-05 19:36:17 +0100

    #156 - Added tests cases. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationsEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationsEnricherTest.groovy
@@ -1,6 +1,36 @@
 package com.github.starnowski.posmulten.configuration.core
 
-import spock.lang.Specification
+import com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration
+import com.github.starnowski.posmulten.configuration.core.model.TableEntry
+import spock.lang.Unroll
 
-class ForeignKeyConfigurationsEnricherTest extends Specification {
+import static java.util.Arrays.asList
+
+class ForeignKeyConfigurationsEnricherTest extends AbstractBaseTest {
+
+    @Unroll
+    def "should invoke component that handles foreign key configuration for each foreign key entry in table #table"()
+    {
+        given:
+            def component = Mock(ForeignKeyConfigurationEnricher)
+            def tested = new ForeignKeyConfigurationsEnricher(component)
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+            def fk1 = new ForeignKeyConfiguration().setConstraintName("fk1")
+            def fk2 = new ForeignKeyConfiguration().setConstraintName("fk2")
+            def tableEntry = new TableEntry().setName(table).setForeignKeys(asList(fk1, fk2))
+
+        when:
+            def result = tested.enrich(builder, tableEntry)
+
+        then:
+            result == builder
+            1 * component.enrich(builder, table, fk1)
+            1 * component.enrich(builder, table, fk2)
+
+        and: "do not invoke builder"
+            0 * builder._
+
+        where:
+            table << ["tab1", "users"]
+    }
 }
