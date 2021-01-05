6d540f922e0b2588045c36e62592f45284d77a55
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-05 02:06:27 +0100

    #156 - Added implementation. [skip travis]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
@@ -10,6 +10,16 @@ public class RLSPolicyConfigurationEnricher implements ITableEntryEnricher {
         if (tableEntry != null && tableEntry.getRlsPolicy() != null) {
             RLSPolicy rlsPolicy = tableEntry.getRlsPolicy();
             builder.createRLSPolicyForTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyColumnsNameToTypeMap(), rlsPolicy.getTenantColumn(), rlsPolicy.getName());
+
+            if (Boolean.TRUE.equals(rlsPolicy.getCreateTenantColumnForTable())) {
+                builder.createTenantColumnForTable(tableEntry.getName());
+            }
+            if (rlsPolicy.getNameForFunctionThatChecksIfRecordExistsInTable() != null) {
+                builder.setNameForFunctionThatChecksIfRecordExistsInTable(tableEntry.getName(), rlsPolicy.getNameForFunctionThatChecksIfRecordExistsInTable());
+            }
+            if (rlsPolicy.getValidTenantValueConstraintName() != null) {
+                builder.registerCustomValidTenantValueConstraintNameForTable(tableEntry.getName(), rlsPolicy.getValidTenantValueConstraintName());
+            }
         }
         return builder;
     }
