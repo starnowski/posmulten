30ed85bdf709aa0405d3bf9fea2408aa4556d383
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-05 01:48:24 +0100

    #156 - Code refactoring. [skip travis]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
@@ -1,11 +1,16 @@
 package com.github.starnowski.posmulten.configuration.core;
 
+import com.github.starnowski.posmulten.configuration.core.model.RLSPolicy;
 import com.github.starnowski.posmulten.configuration.core.model.TableEntry;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 
 public class RLSPolicyConfigurationEnricher implements ITableEntryEnricher {
     @Override
     public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, TableEntry tableEntry) {
-        return null;
+        if (tableEntry != null && tableEntry.getRlsPolicy() != null) {
+            RLSPolicy rlsPolicy = tableEntry.getRlsPolicy();
+            builder.createRLSPolicyForTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyColumnsNameToTypeMap(), rlsPolicy.getTenantColumn(), rlsPolicy.getName());
+        }
+        return builder;
     }
 }
