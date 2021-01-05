c33fbd9e195b5f274f9a7f08239314d7cd2a9d41
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-05 18:45:53 +0100

    #156 - Code refactoring. [skip travis]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationsEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationsEnricher.java
@@ -4,6 +4,17 @@ import com.github.starnowski.posmulten.configuration.core.model.TableEntry;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 
 public class ForeignKeyConfigurationsEnricher implements ITableEntryEnricher {
+
+    private final ForeignKeyConfigurationEnricher foreignKeyConfigurationEnricher;
+
+    public ForeignKeyConfigurationsEnricher() {
+        this(new ForeignKeyConfigurationEnricher());
+    }
+
+    public ForeignKeyConfigurationsEnricher(ForeignKeyConfigurationEnricher foreignKeyConfigurationEnricher) {
+        this.foreignKeyConfigurationEnricher = foreignKeyConfigurationEnricher;
+    }
+
     @Override
     public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, TableEntry tableEntry) {
         return null;
