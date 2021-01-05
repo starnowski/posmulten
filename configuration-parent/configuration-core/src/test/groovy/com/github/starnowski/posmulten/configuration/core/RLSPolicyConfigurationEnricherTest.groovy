9ad92bc56ec3b7b1a6e51ac41d6abcab7bbd9543
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-05 01:41:38 +0100

    #156 - Tests code refactoring. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricherTest.groovy
@@ -1,7 +1,8 @@
 package com.github.starnowski.posmulten.configuration.core
 
-import com.github.starnowski.posmulten.configuration.core.model.SharedSchemaContextConfiguration
+import com.github.starnowski.posmulten.configuration.core.model.RLSPolicy
 import com.github.starnowski.posmulten.configuration.core.model.TableEntry
+import com.github.starnowski.posmulten.postgresql.test.utils.MapBuilder
 import spock.lang.Unroll
 
 class RLSPolicyConfigurationEnricherTest extends AbstractBaseTest {
@@ -9,28 +10,31 @@ class RLSPolicyConfigurationEnricherTest extends AbstractBaseTest {
     def tested = new RLSPolicyConfigurationEnricher()
 
     @Unroll
-    def "should set builder component with specific properties currentTenantIdPropertyType (#currentTenantIdPropertyType), currentTenantIdProperty (#currentTenantIdProperty), getCurrentTenantIdFunctionName (#getCurrentTenantIdFunctionName), setCurrentTenantIdFunctionName (#setCurrentTenantIdFunctionName), equalsCurrentTenantIdentifierFunctionName (#equalsCurrentTenantIdentifierFunctionName)"()
+    def "should set builder component with rls policy for table name '#tableName', rls policy name '#rlsPolicyName', tenant column '#tenantColumn', primaryKeyColumnsNameToTypeMap '#primaryKeyColumnsNameToTypeMap', createTenantColumnForTable '#createTenantColumnForTable', nameForFunctionThatChecksIfRecordExistsInTable '#nameForFunctionThatChecksIfRecordExistsInTable', validTenantValueConstraintName '#validTenantValueConstraintName', skipAddingOfTenantColumnDefaultValue '#skipAddingOfTenantColumnDefaultValue'"()
     {
         given:
-        def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
-        def entry = new TableEntry()
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+            def entry = new TableEntry().setName(tableName)
+                    .setRlsPolicy(new RLSPolicy()
+                                    .setName(rlsPolicyName)
+                                    .setTenantColumn(tenantColumn)
+                                    .setPrimaryKeyColumnsNameToTypeMap(primaryKeyColumnsNameToTypeMap)
+                                    .setCreateTenantColumnForTable(createTenantColumnForTable)
+                                    .setNameForFunctionThatChecksIfRecordExistsInTable(nameForFunctionThatChecksIfRecordExistsInTable)
+                                    .setValidTenantValueConstraintName(validTenantValueConstraintName)
+                                    .setSkipAddingOfTenantColumnDefaultValue(skipAddingOfTenantColumnDefaultValue))
 
         when:
-        def result = tested.enrich(builder, entry)
+            def result = tested.enrich(builder, entry)
 
         then:
-        result == builder
-        1 * builder.setCurrentTenantIdProperty(currentTenantIdProperty)
-        1 * builder.setCurrentTenantIdPropertyType(currentTenantIdPropertyType)
-        1 * builder.setGetCurrentTenantIdFunctionName(getCurrentTenantIdFunctionName)
-        1 * builder.setSetCurrentTenantIdFunctionName(setCurrentTenantIdFunctionName)
-        1 * builder.setEqualsCurrentTenantIdentifierFunctionName(equalsCurrentTenantIdentifierFunctionName)
+            result == builder
+            1 * builder.createRLSPolicyForTable(tableName, primaryKeyColumnsNameToTypeMap, tenantColumn, rlsPolicyName)
 
 
         where:
-        currentTenantIdPropertyType |   currentTenantIdProperty |   getCurrentTenantIdFunctionName  |   setCurrentTenantIdFunctionName  |   equalsCurrentTenantIdentifierFunctionName
-        "VARCHAR(37)"               |   "customer_d"            |   "what_is_tenant_id"             |   "tenant_is_now"                 |   "is_it_this_tenant_id"
-        "UUID"                      |   "tenantId"              |   "get_current_t"                 |   "set_t"                         |   "equals_tenant"
+            tableName   |   rlsPolicyName   |   tenantColumn    |   primaryKeyColumnsNameToTypeMap                                  |   createTenantColumnForTable  |   nameForFunctionThatChecksIfRecordExistsInTable  |   validTenantValueConstraintName  |   skipAddingOfTenantColumnDefaultValue
+            "t1"        |   "rls_pol"       |   "tenant_co"     |   MapBuilder.mapBuilder().put("id", "bigint").build()             |   false                       |   "is_record_exists"                              |   "table_ten_is_valid_con"        |   true
     }
 
 }
