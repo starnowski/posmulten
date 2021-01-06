cc3df0ccfd8b47b046b82d83dbcfa5ce0ac54e7e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-06 21:34:45 +0100

    #156 - Added mapper. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ValidTenantValueConstraintConfigurationMapper.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.ValidTenantValueConstraintConfiguration;
+
+public class ValidTenantValueConstraintConfigurationMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration, ValidTenantValueConstraintConfiguration> {
+    @Override
+    public ValidTenantValueConstraintConfiguration map(com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration input) {
+        return null;
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration unmap(ValidTenantValueConstraintConfiguration output) {
+        return null;
+    }
+}
