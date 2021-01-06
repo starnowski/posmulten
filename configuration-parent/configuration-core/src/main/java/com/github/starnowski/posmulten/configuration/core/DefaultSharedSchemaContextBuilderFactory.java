e15d93a923b60f75128a08ae09c314a317527100
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-06 02:10:00 +0100

    #156 - Added implementation. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/DefaultSharedSchemaContextBuilderFactory.java
@@ -0,0 +1,13 @@
+package com.github.starnowski.posmulten.configuration.core;
+
+import com.github.starnowski.posmulten.configuration.core.model.SharedSchemaContextConfiguration;
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+
+public class DefaultSharedSchemaContextBuilderFactory {
+
+    public DefaultSharedSchemaContextBuilder build(SharedSchemaContextConfiguration contextConfiguration)
+    {
+        //TODO
+        return null;
+    }
+}
