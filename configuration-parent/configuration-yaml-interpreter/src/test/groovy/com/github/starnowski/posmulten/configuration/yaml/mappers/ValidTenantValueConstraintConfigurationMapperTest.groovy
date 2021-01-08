cf10433a50c5a777abf309ef88a9025779469b20
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-08 19:44:55 +0100

    #156 - Added tests cases. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/mappers/ValidTenantValueConstraintConfigurationMapperTest.groovy
@@ -0,0 +1,48 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers
+
+import com.github.starnowski.posmulten.configuration.yaml.model.ValidTenantValueConstraintConfiguration
+import spock.lang.Specification
+
+import static java.util.Arrays.asList
+import static java.util.stream.Collectors.toList
+
+class ValidTenantValueConstraintConfigurationMapperTest extends Specification {
+
+    def "should unmap yaml objects to expected configuration objects"()
+    {
+        given:
+            def tested = new ValidTenantValueConstraintConfigurationMapper()
+            List<ValidTenantValueConstraintConfiguration> yamlObjects = prepareExpectedMappedObjectsList()
+            List<com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration> expectedbjects = prepareExpectedUmnappeddObjectsList()
+
+        when:
+            def actualObjects = yamlObjects.stream().map({yamlObject -> tested.unmap(yamlObject)}).collect(toList())
+
+        then:
+            actualObjects == expectedbjects
+    }
+
+    protected List<ValidTenantValueConstraintConfiguration> prepareExpectedMappedObjectsList()
+    {
+        [
+                new ValidTenantValueConstraintConfiguration(),
+                new ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("tenant_x_id"),
+                new ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("i_t_v_constraint"),
+                new ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("i_t_v_constraint")
+                                                            .setIsTenantValidFunctionName("function_name_is_tenant_valid")
+                                                            .setTenantIdentifiersBlacklist(asList("XXX", "invalid_tenant"))
+        ]
+    }
+
+    protected List<com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration> prepareExpectedUmnappeddObjectsList()
+    {
+        [
+                new com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration(),
+                new com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("tenant_x_id"),
+                new com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("i_t_v_constraint"),
+                new com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName("i_t_v_constraint")
+                        .setIsTenantValidFunctionName("function_name_is_tenant_valid")
+                        .setTenantIdentifiersBlacklist(asList("XXX", "invalid_tenant"))
+        ]
+    }
+}
