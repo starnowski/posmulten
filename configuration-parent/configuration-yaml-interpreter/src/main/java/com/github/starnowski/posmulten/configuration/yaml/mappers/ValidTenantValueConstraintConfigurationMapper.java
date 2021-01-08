2c25476f503529430ef5b866b942836fec7e8835
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-08 20:33:56 +0100

    #156 - Added implementation. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ValidTenantValueConstraintConfigurationMapper.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ValidTenantValueConstraintConfigurationMapper.java
@@ -1,12 +1,15 @@
 package com.github.starnowski.posmulten.configuration.yaml.mappers;
 
 import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.StringWrapperWithNotBlankValue;
 import com.github.starnowski.posmulten.configuration.yaml.model.ValidTenantValueConstraintConfiguration;
 
 public class ValidTenantValueConstraintConfigurationMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration, ValidTenantValueConstraintConfiguration> {
     @Override
     public ValidTenantValueConstraintConfiguration map(com.github.starnowski.posmulten.configuration.core.model.ValidTenantValueConstraintConfiguration input) {
-        return null;
+        return input == null ? null : new ValidTenantValueConstraintConfiguration().setIsTenantValidConstraintName(input.getIsTenantValidConstraintName() == null ? null : new StringWrapperWithNotBlankValue(input.getIsTenantValidConstraintName()))
+                .setIsTenantValidFunctionName(input.getIsTenantValidFunctionName() == null ? null : new StringWrapperWithNotBlankValue(input.getIsTenantValidFunctionName()))
+                .setTenantIdentifiersBlacklist(input.getTenantIdentifiersBlacklist());
     }
 
     @Override
