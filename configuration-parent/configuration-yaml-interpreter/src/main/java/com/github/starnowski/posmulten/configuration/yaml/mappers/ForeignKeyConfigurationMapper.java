a52291b56341bfd4a48ee899f49f914df7d1a965
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-08 23:16:04 +0100

    #156 - Added the ForeignKeyConfigurationMapper type. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ForeignKeyConfigurationMapper.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.ForeignKeyConfiguration;
+
+public class ForeignKeyConfigurationMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration, ForeignKeyConfiguration> {
+    @Override
+    public ForeignKeyConfiguration map(com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration input) {
+        return null;
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration unmap(ForeignKeyConfiguration output) {
+        return null;
+    }
+}
