d07219e2a21213fd1c6feeacb12d56bc19f6966a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 17:09:40 +0100

    #156 - Fixed tests data. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapperTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapperTest.groovy
@@ -28,8 +28,8 @@ class RLSPolicyMapperTest extends AbstractConfigurationMapperTest<com.github.sta
                 new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setName("_some_policy"),
                 new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setName("rls_1").setSkipAddingOfTenantColumnDefaultValue(true),
                 new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setName("policy_2").setSkipAddingOfTenantColumnDefaultValue(false),
-                new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setValidTenantValueConstraintName("constr_1").setValidTenantValueConstraintName(false),
-                new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setValidTenantValueConstraintName(true).setNameForFunctionThatChecksIfRecordExistsInTable("record_is_valid"),
+                new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setValidTenantValueConstraintName("constr_1").setCreateTenantColumnForTable(true),
+                new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setValidTenantValueConstraintName("wrong_tenant_const").setNameForFunctionThatChecksIfRecordExistsInTable("record_is_valid"),
                 new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setTenantColumn("ten_col").setName("posts").setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "int").build()),
                 new com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy().setName("posts").setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "int").put("some_uuid", "UUID").build())
 
@@ -43,8 +43,8 @@ class RLSPolicyMapperTest extends AbstractConfigurationMapperTest<com.github.sta
                 new RLSPolicy().setName("_some_policy"),
                 new RLSPolicy().setName("rls_1").setSkipAddingOfTenantColumnDefaultValue(true),
                 new RLSPolicy().setName("policy_2").setSkipAddingOfTenantColumnDefaultValue(false),
-                new RLSPolicy().setValidTenantValueConstraintName("constr_1").setValidTenantValueConstraintName(false),
-                new RLSPolicy().setValidTenantValueConstraintName(true).setNameForFunctionThatChecksIfRecordExistsInTable("record_is_valid"),
+                new RLSPolicy().setValidTenantValueConstraintName("constr_1").setCreateTenantColumnForTable(true),
+                new RLSPolicy().setValidTenantValueConstraintName("wrong_tenant_const").setNameForFunctionThatChecksIfRecordExistsInTable("record_is_valid"),
                 new RLSPolicy().setTenantColumn("ten_col").setName("posts").setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "int").build()),
                 new RLSPolicy().setName("posts").setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "int").put("some_uuid", "UUID").build())
 
