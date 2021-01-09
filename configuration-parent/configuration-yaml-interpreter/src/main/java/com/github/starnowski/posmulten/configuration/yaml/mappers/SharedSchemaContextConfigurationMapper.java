b315e1ecbec5423527be96f9d3893e2bb11362cb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 18:18:51 +0100

    #156 - Added the SharedSchemaContextConfigurationMapper type. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/SharedSchemaContextConfigurationMapper.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.SharedSchemaContextConfiguration;
+
+public class SharedSchemaContextConfigurationMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.SharedSchemaContextConfiguration, SharedSchemaContextConfiguration> {
+    @Override
+    public SharedSchemaContextConfiguration map(com.github.starnowski.posmulten.configuration.core.model.SharedSchemaContextConfiguration input) {
+        return null;
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.SharedSchemaContextConfiguration unmap(SharedSchemaContextConfiguration output) {
+        return null;
+    }
+}
