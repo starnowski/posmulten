baae303a594187fc55fd9fe96b2863490906a6cc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 18:12:09 +0100

    #156 - Added implementation for the TableEntryMapper type. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/TableEntryMapper.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/TableEntryMapper.java
@@ -3,14 +3,22 @@ package com.github.starnowski.posmulten.configuration.yaml.mappers;
 import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
 import com.github.starnowski.posmulten.configuration.yaml.model.TableEntry;
 
+import static java.util.stream.Collectors.toList;
+
 public class TableEntryMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.TableEntry, TableEntry> {
+
+    private final RLSPolicyMapper rlsPolicyMapper = new RLSPolicyMapper();
+    private final ForeignKeyConfigurationMapper foreignKeyConfigurationMapper = new ForeignKeyConfigurationMapper();
+
     @Override
     public TableEntry map(com.github.starnowski.posmulten.configuration.core.model.TableEntry input) {
-        return null;
+        return input == null ? null : new TableEntry().setName(input.getName()).setRlsPolicy(rlsPolicyMapper.map(input.getRlsPolicy()))
+                .setForeignKeys(input.getForeignKeys() == null ? null : input.getForeignKeys().stream().map(key -> foreignKeyConfigurationMapper.map(key)).collect(toList()));
     }
 
     @Override
     public com.github.starnowski.posmulten.configuration.core.model.TableEntry unmap(TableEntry output) {
-        return null;
+        return output == null ? null : new com.github.starnowski.posmulten.configuration.core.model.TableEntry().setName(output.getName()).setRlsPolicy(rlsPolicyMapper.unmap(output.getRlsPolicy()))
+                .setForeignKeys(output.getForeignKeys() == null ? null : output.getForeignKeys().stream().map(key -> foreignKeyConfigurationMapper.unmap(key)).collect(toList()));
     }
 }
