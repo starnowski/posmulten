f960c3148f2b7584b38cd88a9ef9c027aabf492f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 16:18:38 +0100

    #156 - Added implementation. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ForeignKeyConfigurationMapper.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/ForeignKeyConfigurationMapper.java
@@ -6,11 +6,17 @@ import com.github.starnowski.posmulten.configuration.yaml.model.ForeignKeyConfig
 public class ForeignKeyConfigurationMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration, ForeignKeyConfiguration> {
     @Override
     public ForeignKeyConfiguration map(com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration input) {
-        return input == null ? null : new ForeignKeyConfiguration().setConstraintName(input.getConstraintName());
+        return input == null ? null : new ForeignKeyConfiguration()
+                .setConstraintName(input.getConstraintName())
+                .setTableName(input.getTableName())
+                .setForeignKeyPrimaryKeyColumnsMappings(input.getForeignKeyPrimaryKeyColumnsMappings());
     }
 
     @Override
     public com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration unmap(ForeignKeyConfiguration output) {
-        return null;
+        return output == null ? null : new com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration()
+                .setConstraintName(output.getConstraintName())
+                .setTableName(output.getTableName())
+                .setForeignKeyPrimaryKeyColumnsMappings(output.getForeignKeyPrimaryKeyColumnsMappings());
     }
 }
