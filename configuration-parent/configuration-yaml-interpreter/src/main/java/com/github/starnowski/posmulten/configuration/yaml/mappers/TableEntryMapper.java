d781f12b86eb756cbcef29cac98f4ab58a1d3b8f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 17:43:20 +0100

    #156 - Added the TableEntryMapper type. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/TableEntryMapper.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.TableEntry;
+
+public class TableEntryMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.TableEntry, TableEntry> {
+    @Override
+    public TableEntry map(com.github.starnowski.posmulten.configuration.core.model.TableEntry input) {
+        return null;
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.TableEntry unmap(TableEntry output) {
+        return null;
+    }
+}
