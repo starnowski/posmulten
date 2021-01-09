83b8c03649911d5975512ef9ba6510692d5856d7
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 16:46:09 +0100

    #156 - Added the RLSPolicyMapper type. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapper.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.yaml.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy;
+
+public class RLSPolicyMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.RLSPolicy, RLSPolicy> {
+    @Override
+    public RLSPolicy map(com.github.starnowski.posmulten.configuration.core.model.RLSPolicy input) {
+        return null;
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.RLSPolicy unmap(RLSPolicy output) {
+        return null;
+    }
+}
