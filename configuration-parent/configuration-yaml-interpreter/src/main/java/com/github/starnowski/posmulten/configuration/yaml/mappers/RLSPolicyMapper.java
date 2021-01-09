6b29986e19e5bb60cbcfcf7fefa24d391f8ecac0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-09 17:25:30 +0100

    #156 - Added implementation. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapper.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapper.java
@@ -2,11 +2,19 @@ package com.github.starnowski.posmulten.configuration.yaml.mappers;
 
 import com.github.starnowski.posmulten.configuration.yaml.IConfigurationMapper;
 import com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy;
+import com.github.starnowski.posmulten.configuration.yaml.model.StringWrapperWithNotBlankValue;
 
 public class RLSPolicyMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.RLSPolicy, RLSPolicy> {
     @Override
     public RLSPolicy map(com.github.starnowski.posmulten.configuration.core.model.RLSPolicy input) {
-        return null;
+        return input == null ? null : new RLSPolicy()
+                .setName(input.getName())
+                .setCreateTenantColumnForTable(input.getCreateTenantColumnForTable())
+                .setNameForFunctionThatChecksIfRecordExistsInTable(input.getNameForFunctionThatChecksIfRecordExistsInTable())
+                .setPrimaryKeyColumnsNameToTypeMap(input.getPrimaryKeyColumnsNameToTypeMap())
+                .setSkipAddingOfTenantColumnDefaultValue(input.getSkipAddingOfTenantColumnDefaultValue())
+                .setTenantColumn(input.getTenantColumn() == null ? null : new StringWrapperWithNotBlankValue(input.getTenantColumn()))
+                .setValidTenantValueConstraintName(input.getValidTenantValueConstraintName() == null ? null : new StringWrapperWithNotBlankValue(input.getValidTenantValueConstraintName()));
     }
 
     @Override
