453d03b25df5255bc0a9046e27a71ab926be1448
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 19:38:17 +0100

    #158 - Added test properties class. [skip travis]

---
+++ configuration-parent/configuration-yaml-interpreter/src/test/java/com/github/starnowski/posmulten/configuration/yaml/TestProperties.java
@@ -0,0 +1,7 @@
+package com.github.starnowski.posmulten.configuration.yaml;
+
+public class TestProperties {
+
+    public static final String ALL_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/all-fields.yaml";
+    public static final String ONLY_MANDATORY_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/only-mandatory-fields.yaml";
+}
