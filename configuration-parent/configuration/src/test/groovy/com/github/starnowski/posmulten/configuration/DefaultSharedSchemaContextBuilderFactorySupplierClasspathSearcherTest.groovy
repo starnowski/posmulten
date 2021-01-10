3a9b2d20053018e40274dac664cc93097bafa20e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 22:17:10 +0100

    #158 - Fixed tests cases for the DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher type. [skip travis]

--- configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest.groovy
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest.groovy
@@ -3,6 +3,8 @@ package com.github.starnowski.posmulten.configuration
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static java.util.stream.Collectors.toSet
+
 class DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest extends Specification {
 
     @Unroll
@@ -15,7 +17,7 @@ class DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest exte
             def results = tested.findDefaultSharedSchemaContextBuilderFactorySuppliers()
 
         then:
-            results == expectedTypes
+            results.stream().map({it -> it.getClass()}).collect(toSet()) == expectedTypes
 
         where:
             expectedTypes << [new HashSet<>(Arrays.asList(Test1DefaultSharedSchemaContextBuilderFactorySupplier.class, Test2DefaultSharedSchemaContextBuilderFactorySupplier.class))]
