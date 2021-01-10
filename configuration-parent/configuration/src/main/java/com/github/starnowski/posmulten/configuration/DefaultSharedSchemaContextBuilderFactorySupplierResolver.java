2a69838a653658febe32aed6473ae5353f39e67b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-11 00:02:35 +0100

    #158 - Added basic implementation. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolver.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolver.java
@@ -2,6 +2,7 @@ package com.github.starnowski.posmulten.configuration;
 
 import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactorySupplier;
 
+import java.util.Comparator;
 import java.util.Set;
 
 public class DefaultSharedSchemaContextBuilderFactorySupplierResolver {
@@ -9,6 +10,7 @@ public class DefaultSharedSchemaContextBuilderFactorySupplierResolver {
     public IDefaultSharedSchemaContextBuilderFactorySupplier resolveSupplierBasedOnPriorityForFile(String file, Set<IDefaultSharedSchemaContextBuilderFactorySupplier> suppliers)
     {
         //TODO
-        return null;
+        Comparator<IDefaultSharedSchemaContextBuilderFactorySupplier> priorityComparator = Comparator.comparingInt(supplier -> supplier.getPriority());
+        return suppliers.stream().sorted(priorityComparator.reversed()).findFirst().orElse(null);
     }
 }
