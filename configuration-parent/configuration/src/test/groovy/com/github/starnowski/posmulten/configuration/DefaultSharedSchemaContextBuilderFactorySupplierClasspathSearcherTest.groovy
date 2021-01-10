110c1731b31360e956255dab8921476d2f36f02a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 21:36:00 +0100

    #158 - Added tests for the DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher type. [skip travis]

---
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest.groovy
@@ -0,0 +1,6 @@
+package com.github.starnowski.posmulten.configuration
+
+import spock.lang.Specification
+
+class DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcherTest extends Specification {
+}
