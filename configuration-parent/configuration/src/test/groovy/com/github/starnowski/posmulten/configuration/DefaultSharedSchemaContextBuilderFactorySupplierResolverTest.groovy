34ae173063f667264b91f63675e69e60628ad55d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 23:01:50 +0100

    #158 - Added tests for the DefaultSharedSchemaContextBuilderFactorySupplierResolver type. [skip travis]

---
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolverTest.groovy
@@ -0,0 +1,6 @@
+package com.github.starnowski.posmulten.configuration
+
+import spock.lang.Specification
+
+class DefaultSharedSchemaContextBuilderFactorySupplierResolverTest extends Specification {
+}
