d3033cac66df68954ea28103c0458a080828df48
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 22:59:51 +0100

    #158 - Added the DefaultSharedSchemaContextBuilderFactorySupplierResolver type. [skip travis]

---
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolver.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.posmulten.configuration;
+
+import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactorySupplier;
+
+import java.util.Set;
+
+public class DefaultSharedSchemaContextBuilderFactorySupplierResolver {
+
+    public IDefaultSharedSchemaContextBuilderFactorySupplier resolveSupplierBasedOnPriorityForFile(String file, Set<IDefaultSharedSchemaContextBuilderFactorySupplier> suppliers)
+    {
+        //TODO
+        return null;
+    }
+}
