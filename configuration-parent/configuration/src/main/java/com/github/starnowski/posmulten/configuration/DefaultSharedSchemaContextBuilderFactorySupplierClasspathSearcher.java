c8012f0108ec8179c694a48ade3e14162ffa7ecc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 21:35:51 +0100

    #158 - Added the DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher type. [skip travis]

---
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.posmulten.configuration;
+
+import com.github.starnowski.posmulten.configuration.core.context.AbstractDefaultSharedSchemaContextBuilderFactorySupplier;
+
+import java.util.Set;
+
+public class DefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher {
+
+    public Set<AbstractDefaultSharedSchemaContextBuilderFactorySupplier> findDefaultSharedSchemaContextBuilderFactorySuppliers()
+    {
+        //TODO
+        return null;
+    }
+}
