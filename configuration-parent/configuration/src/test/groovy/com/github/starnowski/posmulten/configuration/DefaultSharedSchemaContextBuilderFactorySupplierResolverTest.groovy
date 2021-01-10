d33182f036ca8f235e6c4bed7df4c9f72c39b854
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 23:10:21 +0100

    #158 - Added TODO comment. [skip travis]

--- configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolverTest.groovy
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolverTest.groovy
@@ -3,4 +3,6 @@ package com.github.starnowski.posmulten.configuration
 import spock.lang.Specification
 
 class DefaultSharedSchemaContextBuilderFactorySupplierResolverTest extends Specification {
+
+    //TODO
 }
