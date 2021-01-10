c5260eebbf17e11c36d2d9adcd906febca70990b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 19:54:51 +0100

    #158 - Added tests cases. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest.groovy
@@ -1,18 +1,29 @@
 package com.github.starnowski.posmulten.configuration.yaml.context
 
-import spock.lang.Specification
+import com.github.starnowski.posmulten.configuration.yaml.AbstractSpecification
 import spock.lang.Unroll
 
 import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.ALL_FIELDS_FILE_PATH
 import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.ONLY_MANDATORY_FIELDS_FILE_PATH
 
-class YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest extends Specification {
+class YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest extends AbstractSpecification {
 
     def tested = new YamlConfigurationDefaultSharedSchemaContextBuilderFactory()
 
     @Unroll
     def "should create builder component based on file #filePath"()
     {
+        given:
+            def resolvedPath = resolveFilePath(filePath)
+
+        when:
+            def builder = tested.build(resolvedPath)
+
+        then:
+            builder
+
+        and: "builder should return a non-empty list of DDL statements"
+            !builder.build().getSqlDefinitions().isEmpty()
 
         where:
             filePath << [ALL_FIELDS_FILE_PATH, ONLY_MANDATORY_FIELDS_FILE_PATH]
