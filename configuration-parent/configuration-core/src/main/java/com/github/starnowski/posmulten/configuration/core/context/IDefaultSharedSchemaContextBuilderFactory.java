b401b198595769d50abeb6974b5e2f60138c3f5b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 17:03:42 +0100

    #158 - Added basic interface. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/context/IDefaultSharedSchemaContextBuilderFactory.java
@@ -0,0 +1,8 @@
+package com.github.starnowski.posmulten.configuration.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+
+public interface IDefaultSharedSchemaContextBuilderFactory {
+
+    DefaultSharedSchemaContextBuilder build(String filePath);
+}
