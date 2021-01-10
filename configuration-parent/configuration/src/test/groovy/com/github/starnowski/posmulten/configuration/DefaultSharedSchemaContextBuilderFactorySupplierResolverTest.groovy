968017ca3a218f3b219f3367a46b0c96412ff5fe
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 23:50:18 +0100

    #158 - Added tests cases for the DefaultSharedSchemaContextBuilderFactorySupplierResolver type. [skip travis]

--- configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolverTest.groovy
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolverTest.groovy
@@ -1,8 +1,40 @@
 package com.github.starnowski.posmulten.configuration
 
+import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactorySupplier
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static java.util.Arrays.asList
 
 class DefaultSharedSchemaContextBuilderFactorySupplierResolverTest extends Specification {
 
-    //TODO
+    def tested = new DefaultSharedSchemaContextBuilderFactorySupplierResolver()
+
+    @Unroll
+    def "should return expected supplier (#expectedSupplier) for file path #filePath and other suppliers #expectedSupplier"()
+    {
+        given:
+            Set<IDefaultSharedSchemaContextBuilderFactorySupplier> suppliersSet = new HashSet<>()
+            suppliersSet.addAll(suppliers)
+            suppliersSet.add(expectedSupplier)
+
+        when:
+            def result = tested.resolveSupplierBasedOnPriorityForFile(filePath, suppliersSet)
+
+        then:
+            result.is(expectedSupplier)
+
+        where:
+            filePath                |   suppliers                                   ||  expectedSupplier
+            "somePath/file.yml"     |   [supplier(1, "yml")]    ||  supplier(49, "yml")
+    }
+
+    private IDefaultSharedSchemaContextBuilderFactorySupplier supplier(int priority, String... extensions)
+    {
+        def result = Mock(IDefaultSharedSchemaContextBuilderFactorySupplier)
+        result.getPriority() >> priority
+        result.getSupportedFileExtensions() >> asList(extensions)
+        result.toString() >> String.format("priority: %d extensions: %s", priority, asList(extensions).toString())
+        result
+    }
 }
