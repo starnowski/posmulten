f8aed243c4ad34e30c99514d6bf3407299f83662
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 20:32:33 +0100

    #158 - Added tests for the YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier type. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest.groovy
@@ -3,4 +3,12 @@ package com.github.starnowski.posmulten.configuration.yaml.context
 import spock.lang.Specification
 
 class YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest extends Specification {
+
+    def tested = new YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier()
+
+    def "should return priority with value zero"()
+    {
+        expect:
+            tested.getPriority() == 0
+    }
 }
