6981c3e58b38da2532f05e0c5692a6dcfaa85ac8
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-10 11:01:49 +0100

    #172 - Added tests data. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/DefaultSharedSchemaContextBuilderFactorySmokeTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/DefaultSharedSchemaContextBuilderFactorySmokeTest.groovy
@@ -15,8 +15,7 @@ class DefaultSharedSchemaContextBuilderFactorySmokeTest extends Specification {
     def tested = new DefaultSharedSchemaContextBuilderFactory()
 
     @Unroll
-    def "should able to create builder components based on correct configuration object (#configuration)"()
-    {
+    def "should able to create builder components based on correct configuration object (#configuration)"() {
         when:
             def builder = tested.build(configuration)
 
@@ -29,14 +28,32 @@ class DefaultSharedSchemaContextBuilderFactorySmokeTest extends Specification {
         where:
             configuration << [
                     new SharedSchemaContextConfiguration().setDefaultSchema("public")
-                        .setGrantee("db-user")
-                        .setValidTenantValueConstraint(new ValidTenantValueConstraintConfiguration()
-                                                        .setTenantIdentifiersBlacklist(asList("xxxx")))
-                        .setTables(asList(new TableEntry().setName("users")
-                                                .setRlsPolicy(new RLSPolicy()
-                                                                .setName("rls_pol")
-                                                                .setNameForFunctionThatChecksIfRecordExistsInTable("does_users_record_exists")
-                                                                .setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "biging").build()))))
+                            .setGrantee("db-user")
+                            .setValidTenantValueConstraint(new ValidTenantValueConstraintConfiguration()
+                            .setTenantIdentifiersBlacklist(asList("xxxx")))
+                            .setTables(asList(new TableEntry().setName("users")
+                            .setRlsPolicy(new RLSPolicy()
+                            .setName("rls_pol")
+                            .setNameForFunctionThatChecksIfRecordExistsInTable("does_users_record_exists")
+                            .setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "biging").build())))),
+                    new SharedSchemaContextConfiguration().setDefaultSchema("public")
+                            .setGrantee("postgres-user")
+                            .setForceRowLevelSecurityForTableOwner(true)
+                            .setDefaultTenantIdColumn("tenant")
+                            .setCurrentTenantIdentifierAsDefaultValueForTenantColumnInAllTables(true)
+                            .setValidTenantValueConstraint(new ValidTenantValueConstraintConfiguration()
+                            .setTenantIdentifiersBlacklist(asList("INVALID_Ten", "John_1234_Doe")))
+                            .setTables(asList(new TableEntry().setName("users_info")
+                            .setRlsPolicy(new RLSPolicy()
+                            .setName("users_info_rls")
+                            .setNameForFunctionThatChecksIfRecordExistsInTable("does_users_record_exists")
+                            .setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("id", "biging").build())),
+                            new TableEntry().setName("tweet")
+                                    .setRlsPolicy(new RLSPolicy()
+                                    .setName("tweet_rls_policy")
+                                    .setNameForFunctionThatChecksIfRecordExistsInTable("does_tweet_record_exists")
+                                    .setPrimaryKeyColumnsNameToTypeMap(mapBuilder().put("uuid", "UUID").build())))
+                            )
             ]
     }
 }
