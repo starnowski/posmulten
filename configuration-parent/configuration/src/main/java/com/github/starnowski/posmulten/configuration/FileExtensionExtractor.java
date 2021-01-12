b376995401dab9f7bc5615a5032253055be71133
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-13 00:47:33 +0100

    #158 - Added implementation for the FileExtensionExtractor type. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/FileExtensionExtractor.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/FileExtensionExtractor.java
@@ -4,6 +4,13 @@ public class FileExtensionExtractor {
 
     public String extract(String filePath)
     {
-        return null;
+        return filePath == null || filePath.trim().isEmpty() ? null : extractExtensionFromNonEmptyValue(filePath);
+    }
+
+    private String extractExtensionFromNonEmptyValue(String filePath)
+    {
+        int i = filePath.lastIndexOf(".");
+        String extension = i > 0 && i < filePath.length() ? filePath.substring(i + 1) : null;
+        return extension == null || extension.trim().isEmpty() ? null : extension;
     }
 }
