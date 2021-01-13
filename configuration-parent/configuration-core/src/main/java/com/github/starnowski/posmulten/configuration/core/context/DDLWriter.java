747dbe8a7cf08eb657cd566d0ccdefecc3213ba5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 00:28:45 +0100

    #158 - Added the DDLWriter type. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/context/DDLWriter.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.configuration.core.context;
+
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+
+public class DDLWriter {
+
+    public void saveCreteScripts(String filePath, ISharedSchemaContext sharedSchemaContext)
+    {
+        //TODO
+    }
+
+    public void saveDropScripts(String filePath, ISharedSchemaContext sharedSchemaContext)
+    {
+        //TODO
+    }
+}
