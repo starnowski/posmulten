21529d33b8c0e1675e0e3a450571ac30bbce1c45
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-13 02:05:13 +0100

    #158 - Added TODO comments. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactoryResolver.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactoryResolver.java
@@ -2,11 +2,26 @@ package com.github.starnowski.posmulten.configuration;
 
 import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactory;
 
+import static com.github.starnowski.posmulten.configuration.DefaultSharedSchemaContextBuilderFactoryResolverContext.builder;
+
 public class DefaultSharedSchemaContextBuilderFactoryResolver {
+
+    private final DefaultSharedSchemaContextBuilderFactoryResolverContext context;
+
+    public DefaultSharedSchemaContextBuilderFactoryResolver() {
+        this.context = builder().build();
+    }
+
+    public DefaultSharedSchemaContextBuilderFactoryResolver(DefaultSharedSchemaContextBuilderFactoryResolverContext context) {
+        this.context = context;
+    }
 
     public IDefaultSharedSchemaContextBuilderFactory resolve(String filePath)
     {
         //TODO
+//        context.getDefaultSharedSchemaContextBuilderFactorySupplierClasspathSearcher()
+//        context.getSuppliers()
+//        context.getDefaultSharedSchemaContextBuilderFactorySupplierResolver()
         return null;
     }
 }
