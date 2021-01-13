ccd76f5288669475190753c7c1cb0a58ce042073
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 00:53:00 +0100

    #158 - Added tests for the DefaultSharedSchemaContextBuilderFactoryResolver type. [skip travis]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/context/DDLWriterTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/context/DDLWriterTest.groovy
@@ -1,22 +1,57 @@
 package com.github.starnowski.posmulten.configuration.core.context
 
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static java.util.stream.Collectors.toList
 
 class DDLWriterTest extends Specification {
 
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder()
 
+    def tested = new DDLWriter()
+
+    @Unroll
     def "should save all DDL statements that creates shared schema strategy to file"()
     {
+        given:
+            def tmpFile = tempFolder.newFile("output.sql")
+            def context = Mock(ISharedSchemaContext)
+            List<SQLDefinition> mockedSQLDefinitions = ddlStatementsEntries.stream().map({it ->
+                def mockedDefinition = Mock(SQLDefinition)
+                mockedDefinition.getCreateScript() >> it.getCreateScript()
+                mockedDefinition.getDropScript() >> it.getDropScript()
+                mockedDefinition
+            }).collect(toList())
+            context.getSqlDefinitions() >> mockedSQLDefinitions
+
+        when:
+            tested.saveCreteScripts(tmpFile.getAbsolutePath(), context)
+
+        then:
+            List<String> actualFileLines = returnFileLines(tmpFile)
+            expectedFileLines == actualFileLines
 
         where:
             ddlStatementsEntries                                                                        ||  expectedFileLines
             [te("create some ...", "drop this record"), te("CREATE RLS_POLICY", "REVOKE Policy")]       ||  ["create some ...", "CREATE RLS_POLICY"]
             [te("Alter table etc;", "drop this record"), te("CREATE RLS_POLICY", "DROP some object")]   ||  ["Alter table etc;", "CREATE RLS_POLICY"]
             [te("grant privilege to object ", "revoke privilege")]                                      ||  ["grant privilege to object "]
+    }
+
+    List<String> returnFileLines(File file)
+    {
+        Scanner scanner = new Scanner(tmpFile)
+        List<String> lines = new ArrayList<>()
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine())
+        }
+        lines
     }
 
     static TestEntry te(String createScript, String dropScript)
