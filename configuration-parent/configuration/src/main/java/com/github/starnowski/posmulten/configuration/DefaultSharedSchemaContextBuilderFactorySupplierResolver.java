f94be6aafe5684133ccd7339f24a17192bacdf3a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-13 01:06:36 +0100

    #158 - Added implementation for the DefaultSharedSchemaContextBuilderFactorySupplierResolver type. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolver.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactorySupplierResolver.java
@@ -3,15 +3,31 @@ package com.github.starnowski.posmulten.configuration;
 import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactorySupplier;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import static java.util.Comparator.comparingInt;
 
 public class DefaultSharedSchemaContextBuilderFactorySupplierResolver {
 
+    private final FileExtensionExtractor fileExtensionExtractor = new FileExtensionExtractor();
+
     public IDefaultSharedSchemaContextBuilderFactorySupplier resolveSupplierBasedOnPriorityForFile(String file, Set<IDefaultSharedSchemaContextBuilderFactorySupplier> suppliers) {
-        //TODO
+        String fileExtension = fileExtensionExtractor.extract(file);
         Comparator<IDefaultSharedSchemaContextBuilderFactorySupplier> priorityComparator = comparingInt(IDefaultSharedSchemaContextBuilderFactorySupplier::getPriority);
-        return suppliers.stream().max(priorityComparator).orElse(null);
+        return suppliers.stream().filter(supplier -> extensionIsSupportedBySupplier(fileExtension, supplier.getSupportedFileExtensions())).max(priorityComparator).orElse(null);
+    }
+
+    private boolean extensionIsSupportedBySupplier(String fileExtension, List<String> supportedFileExtensions)
+    {
+        return supportedFileExtensions == null ? false : supportedFileExtensions.stream().anyMatch(supportedFileExtension -> extensionIsEqual(fileExtension, supportedFileExtension));
+    }
+
+    private boolean extensionIsEqual(String fileExtension, String supportedExtension)
+    {
+        fileExtension = fileExtension == null ? null : fileExtension.toLowerCase();
+        supportedExtension = supportedExtension == null ? null : supportedExtension.toLowerCase();
+        return Objects.equals(fileExtension, supportedExtension);
     }
 }
