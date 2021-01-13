c63dd04f10c2d045851f8f765e7e8a58e0546f7d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-13 01:50:56 +0100

    #158 - Added basic types. [skip travis]

---
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DefaultSharedSchemaContextBuilderFactoryResolver.java
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.configuration;
+
+import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactory;
+
+public class DefaultSharedSchemaContextBuilderFactoryResolver {
+
+    public IDefaultSharedSchemaContextBuilderFactory resolve(String filePath)
+    {
+        //TODO
+        return null;
+    }
+}
