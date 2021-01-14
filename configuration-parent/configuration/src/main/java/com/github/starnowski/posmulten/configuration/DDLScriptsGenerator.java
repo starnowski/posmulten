7d7dcc2271d950e07813bbb18c9a30a12ef58241
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 02:14:05 +0100

    #158 - Added DDLScriptsGenerator type. [skip travis]

---
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
@@ -0,0 +1,9 @@
+package com.github.starnowski.posmulten.configuration;
+
+public class DDLScriptsGenerator {
+
+    public void generate(String configurationFilePath, String createScripsFilePath, String dropScripsFilePath)
+    {
+        //TODO
+    }
+}
