84a3c846c29c4971e49686eb0941a2cfd9ac1145
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 20:50:34 +0100

    #158 - Added implementation for the DDLScriptsGenerator type. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
@@ -1,6 +1,12 @@
 package com.github.starnowski.posmulten.configuration;
 
 import com.github.starnowski.posmulten.configuration.core.context.DDLWriter;
+import com.github.starnowski.posmulten.configuration.core.context.IDefaultSharedSchemaContextBuilderFactory;
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+
+import java.io.IOException;
 
 public class DDLScriptsGenerator {
 
@@ -17,8 +23,11 @@ public class DDLScriptsGenerator {
     private final DefaultSharedSchemaContextBuilderFactoryResolver defaultSharedSchemaContextBuilderFactoryResolver;
     private final DDLWriter ddlWriter;
 
-    public void generate(String configurationFilePath, String createScripsFilePath, String dropScripsFilePath)
-    {
-        //TODO
+    public void generate(String configurationFilePath, String createScripsFilePath, String dropScripsFilePath) throws SharedSchemaContextBuilderException, IOException {
+        IDefaultSharedSchemaContextBuilderFactory factory = defaultSharedSchemaContextBuilderFactoryResolver.resolve(configurationFilePath);
+        DefaultSharedSchemaContextBuilder builder = factory.build(configurationFilePath);
+        ISharedSchemaContext context = builder.build();
+        ddlWriter.saveCreteScripts(createScripsFilePath, context);
+        ddlWriter.saveDropScripts(dropScripsFilePath, context);
     }
 }
