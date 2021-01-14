be33c9dba13907288e8f01d82d2e3a6d1c4665de
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 20:56:44 +0100

    #158 - Added tests for the DDLScriptsGenerator type. [skip travis]

--- configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DDLScriptsGeneratorTest.groovy
+++ configuration-parent/configuration/src/test/groovy/com/github/starnowski/posmulten/configuration/DDLScriptsGeneratorTest.groovy
@@ -35,4 +35,58 @@ class DDLScriptsGeneratorTest extends Specification {
             "C:\\some\\path\\config.yml"    |   "Z:\\create-shared-schema.sql"  |   ".\\drop-shared-schema.sql"
             "dir/cofing.xml"                |   "create-schema.sql"             |   "drop-schema.sql"
     }
+
+    @Unroll
+    def "should create DDL scripts for file #configFilePath and save creation script in file #creationScriptPath but do not save dropping script when file is not specified"()
+    {
+        given:
+            def defaultSharedSchemaContextBuilderFactoryResolver = Mock(DefaultSharedSchemaContextBuilderFactoryResolver)
+            def ddlWriter = Mock(DDLWriter)
+            def tested = new DDLScriptsGenerator(defaultSharedSchemaContextBuilderFactoryResolver, ddlWriter)
+            IDefaultSharedSchemaContextBuilderFactory defaultSharedSchemaContextBuilderFactory = Mock(IDefaultSharedSchemaContextBuilderFactory)
+            DefaultSharedSchemaContextBuilder builder = Mock(DefaultSharedSchemaContextBuilder)
+            ISharedSchemaContext context = Mock(ISharedSchemaContext)
+
+        when:
+            tested.generate(configFilePath, creationScriptPath, null)
+
+        then:
+            1 * defaultSharedSchemaContextBuilderFactoryResolver.resolve(configFilePath) >> defaultSharedSchemaContextBuilderFactory
+            1 * defaultSharedSchemaContextBuilderFactory.build(configFilePath) >> builder
+            1 * builder.build() >> context
+            1 * ddlWriter.saveCreteScripts(creationScriptPath, context)
+            0 * ddlWriter.saveDropScripts(_, context)
+
+        where:
+            configFilePath                  |   creationScriptPath
+            "C:\\some\\path\\config.yml"    |   "Z:\\create-shared-schema.sql"
+            "dir/cofing.xml"                |   "create-schema.sql"
+    }
+
+    @Unroll
+    def "should create DDL scripts for file #configFilePath and save dropping script in file #droppingScriptPath but do not save creation script when file is not specified"()
+    {
+        given:
+            def defaultSharedSchemaContextBuilderFactoryResolver = Mock(DefaultSharedSchemaContextBuilderFactoryResolver)
+            def ddlWriter = Mock(DDLWriter)
+            def tested = new DDLScriptsGenerator(defaultSharedSchemaContextBuilderFactoryResolver, ddlWriter)
+            IDefaultSharedSchemaContextBuilderFactory defaultSharedSchemaContextBuilderFactory = Mock(IDefaultSharedSchemaContextBuilderFactory)
+            DefaultSharedSchemaContextBuilder builder = Mock(DefaultSharedSchemaContextBuilder)
+            ISharedSchemaContext context = Mock(ISharedSchemaContext)
+
+        when:
+            tested.generate(configFilePath, null, droppingScriptPath)
+
+        then:
+            1 * defaultSharedSchemaContextBuilderFactoryResolver.resolve(configFilePath) >> defaultSharedSchemaContextBuilderFactory
+            1 * defaultSharedSchemaContextBuilderFactory.build(configFilePath) >> builder
+            1 * builder.build() >> context
+            0 * ddlWriter.saveCreteScripts(_, context)
+            1 * ddlWriter.saveDropScripts(droppingScriptPath, context)
+
+        where:
+            configFilePath                  |   droppingScriptPath
+            "C:\\some\\path\\config.yml"    |   ".\\drop-shared-schema.sql"
+            "dir/cofing.xml"                |   "drop-schema.sql"
+    }
 }
