2609012667a63cc6f79d449c65dac39fa9e51d8c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 20:03:32 +0100

    #158 - Added DDLScriptsGenerator type. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
@@ -1,6 +1,21 @@
 package com.github.starnowski.posmulten.configuration;
 
+import com.github.starnowski.posmulten.configuration.core.context.DDLWriter;
+
 public class DDLScriptsGenerator {
+
+    public DDLScriptsGenerator()
+    {
+        this(new DefaultSharedSchemaContextBuilderFactoryResolver(), new DDLWriter());
+    }
+
+    public DDLScriptsGenerator(DefaultSharedSchemaContextBuilderFactoryResolver defaultSharedSchemaContextBuilderFactoryResolver, DDLWriter ddlWriter) {
+        this.defaultSharedSchemaContextBuilderFactoryResolver = defaultSharedSchemaContextBuilderFactoryResolver;
+        this.ddlWriter = ddlWriter;
+    }
+
+    private final DefaultSharedSchemaContextBuilderFactoryResolver defaultSharedSchemaContextBuilderFactoryResolver;
+    private final DDLWriter ddlWriter;
 
     public void generate(String configurationFilePath, String createScripsFilePath, String dropScripsFilePath)
     {
