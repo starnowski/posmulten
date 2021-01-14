854981ea3373f80f879588e5d045e3f1884c28de
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-14 01:49:58 +0100

    #158 - Added implementation for the saveDropScripts(String filePath, ISharedSchemaContext sharedSchemaContext) method [skip travis]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/context/DDLWriter.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/context/DDLWriter.java
@@ -6,6 +6,7 @@ import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaCont
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
 import static java.nio.file.Files.write;
@@ -18,8 +19,11 @@ public class DDLWriter {
         write(Paths.get(new File(filePath).toURI()), lines);
     }
 
-    public void saveDropScripts(String filePath, ISharedSchemaContext sharedSchemaContext)
-    {
-        //TODO
+    public void saveDropScripts(String filePath, ISharedSchemaContext sharedSchemaContext) throws IOException {
+        //Save DDL statements in reverse order
+        LinkedList<SQLDefinition> stack = new LinkedList<>();
+        sharedSchemaContext.getSqlDefinitions().forEach(stack::push);
+        List<String> lines = stack.stream().map(SQLDefinition::getDropScript).collect(toList());
+        write(Paths.get(new File(filePath).toURI()), lines);
     }
 }
