3ab876f0ecca0603d13943eeafde7018576e5edc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-15 21:44:33 +0100

    #157 - Added base abstract class for exceptions. [skip travis]

---
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/exceptions/InvalidConfigurationException.java
@@ -0,0 +1,19 @@
+package com.github.starnowski.posmulten.configuration.core.exceptions;
+
+public abstract class InvalidConfigurationException extends Exception {
+
+    public InvalidConfigurationException() {
+    }
+
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+
+    public InvalidConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidConfigurationException(Throwable cause) {
+        super(cause);
+    }
+}
