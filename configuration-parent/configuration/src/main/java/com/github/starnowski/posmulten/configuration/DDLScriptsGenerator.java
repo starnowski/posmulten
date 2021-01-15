af271b02b103e9837401a996ace1642c3be25d20
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-15 01:11:31 +0100

    #158 - Added logger. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/DDLScriptsGenerator.java
@@ -5,9 +5,12 @@ import com.github.starnowski.posmulten.configuration.core.context.IDefaultShared
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import lombok.extern.java.Log;
 
 import java.io.IOException;
+import java.util.logging.Level;
 
+@Log
 public class DDLScriptsGenerator {
 
     public DDLScriptsGenerator() {
@@ -23,13 +26,16 @@ public class DDLScriptsGenerator {
     private final DDLWriter ddlWriter;
 
     public void generate(String configurationFilePath, String createScripsFilePath, String dropScripsFilePath) throws SharedSchemaContextBuilderException, IOException {
+        log.log(Level.INFO, "Generate DDL statements based on file: {0}", new Object[]{configurationFilePath});
         IDefaultSharedSchemaContextBuilderFactory factory = defaultSharedSchemaContextBuilderFactoryResolver.resolve(configurationFilePath);
         DefaultSharedSchemaContextBuilder builder = factory.build(configurationFilePath);
         ISharedSchemaContext context = builder.build();
         if (createScripsFilePath != null) {
+            log.log(Level.INFO, "Saving DDL statements that creates the shared schema strategy to {0}", createScripsFilePath);
             ddlWriter.saveCreteScripts(createScripsFilePath, context);
         }
         if (dropScripsFilePath != null) {
+            log.log(Level.INFO, "Saving DDL statements that drop the shared schema strategy to {0}", dropScripsFilePath);
             ddlWriter.saveDropScripts(dropScripsFilePath, context);
         }
     }
