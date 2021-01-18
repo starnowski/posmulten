ad1519230f1bae478839b7a26e11ad03dbf0bcd5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-18 18:55:08 +0100

    #157 - Added tests cases for many-to-many tables. [skip travis]

--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest.groovy
@@ -11,6 +11,7 @@ import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.
 import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.INVALID_NESTED_NODE_EMPTY_LIST_FILE_PATH
 import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.INVALID_ROOT_NODE_BLANK_FIELDS_FILE_PATH
 import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.ONLY_MANDATORY_FIELDS_FILE_PATH
+import static com.github.starnowski.posmulten.configuration.yaml.TestProperties.MANY_TO_MANY_TABLES_FILE_PATH
 
 class YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest extends AbstractSpecification {
 
@@ -32,7 +33,7 @@ class YamlConfigurationDefaultSharedSchemaContextBuilderFactoryTest extends Abst
             !builder.build().getSqlDefinitions().isEmpty()
 
         where:
-            filePath << [ALL_FIELDS_FILE_PATH, ONLY_MANDATORY_FIELDS_FILE_PATH]
+            filePath << [ALL_FIELDS_FILE_PATH, ONLY_MANDATORY_FIELDS_FILE_PATH, MANY_TO_MANY_TABLES_FILE_PATH]
     }
 
     @Unroll
--- configuration-parent/configuration-yaml-interpreter/src/test/java/com/github/starnowski/posmulten/configuration/yaml/TestProperties.java
+++ configuration-parent/configuration-yaml-interpreter/src/test/java/com/github/starnowski/posmulten/configuration/yaml/TestProperties.java
@@ -4,6 +4,7 @@ public class TestProperties {
 
     public static final String ALL_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/all-fields.yaml";
     public static final String ONLY_MANDATORY_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/only-mandatory-fields.yaml";
+    public static final String MANY_TO_MANY_TABLES_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/many-to-many-tables.yaml";
     public static final String INVALID_ROOT_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-root-node-blank-fields.yaml";
     public static final String INVALID_NESTED_NODE_BLANK_FIELDS_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-blank-fields.yaml";
     public static final String INVALID_NESTED_NODE_EMPTY_LIST_FILE_PATH = "/com/github/starnowski/posmulten/configuration/yaml/invalid-nested-node-empty-list.yaml";
