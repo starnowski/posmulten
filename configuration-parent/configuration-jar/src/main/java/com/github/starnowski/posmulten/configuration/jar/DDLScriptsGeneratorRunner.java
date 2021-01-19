40f12570f113560db306643d28bf33d272d89429
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-19 18:32:54 +0100

    #157 - Reading project properties in main method.

--- configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/DDLScriptsGeneratorRunner.java
+++ configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/DDLScriptsGeneratorRunner.java
@@ -6,24 +6,34 @@ import com.github.starnowski.posmulten.postgresql.core.context.exceptions.Shared
 import lombok.extern.java.Log;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Level;
 
 @Log
 public class DDLScriptsGeneratorRunner {
 
     public static void main(String[] args) throws SharedSchemaContextBuilderException, IOException {
-        String configFilePath = System.getProperty("posmulten.configuration.config.file.path");
-        String createScriptPath = System.getProperty("posmulten.configuration.create.script.path");
-        String dropScriptPath = System.getProperty("posmulten.configuration.drop.script.path");
-        DDLScriptsGenerator ddlScriptsGenerator = new DDLScriptsGenerator();
-        try {
-            ddlScriptsGenerator.generate(configFilePath, createScriptPath, dropScriptPath);
-        } catch (InvalidConfigurationException e) {
-            log.log(Level.SEVERE, "Posmulten invalid configuration");
-            e.getErrorMessages().forEach(message ->
-                    log.log(Level.SEVERE, "Configuration error: {0}", message)
-            );
-            System.exit(1);
+        if (Boolean.TRUE.equals(Boolean.valueOf(System.getProperty("posmulten.configuration.config.version.print")))) {
+            DDLScriptsGeneratorRunner runner = new DDLScriptsGeneratorRunner();
+            java.io.InputStream is = runner.getClass().getResourceAsStream("configuration-jar.properties");
+            java.util.Properties p = new Properties();
+            p.load(is);
+            String version = p.getProperty("configuration.jar.project.version");
+            System.out.print(version);
+        } else {
+            String configFilePath = System.getProperty("posmulten.configuration.config.file.path");
+            String createScriptPath = System.getProperty("posmulten.configuration.create.script.path");
+            String dropScriptPath = System.getProperty("posmulten.configuration.drop.script.path");
+            DDLScriptsGenerator ddlScriptsGenerator = new DDLScriptsGenerator();
+            try {
+                ddlScriptsGenerator.generate(configFilePath, createScriptPath, dropScriptPath);
+            } catch (InvalidConfigurationException e) {
+                log.log(Level.SEVERE, "Posmulten invalid configuration");
+                e.getErrorMessages().forEach(message ->
+                        log.log(Level.SEVERE, "Configuration error: {0}", message)
+                );
+                System.exit(1);
+            }
         }
     }
 }
