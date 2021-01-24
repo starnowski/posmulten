6fa20c193239f78549fca226af61d0531758236b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-24 15:44:13 +0100

    #182 - Added implementations. [skip travis]

--- configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/NoDefaultSharedSchemaContextBuilderFactorySupplierException.java
+++ configuration-parent/configuration/src/main/java/com/github/starnowski/posmulten/configuration/NoDefaultSharedSchemaContextBuilderFactorySupplierException.java
@@ -1,16 +1,18 @@
 package com.github.starnowski.posmulten.configuration;
 
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+
+import static java.util.stream.Collectors.joining;
 
 public class NoDefaultSharedSchemaContextBuilderFactorySupplierException extends Exception {
 
-    private final String filePath;
-    private final Set<String> supportedFileExtensions;
-
     public NoDefaultSharedSchemaContextBuilderFactorySupplierException(String filePath, Set<String> supportedFileExtensions) {
-        this.filePath = filePath;
-        this.supportedFileExtensions = Collections.unmodifiableSet(supportedFileExtensions == null ? new HashSet<>() : supportedFileExtensions);
+        super(prepareMessage(filePath, supportedFileExtensions));
+    }
+
+    private static String prepareMessage(String filePath, Set<String> supportedFileExtensions) {
+        return String.format("No supplier was found, able to handle file %1$s. Supported file extensions: %2$s", filePath, Optional.ofNullable(supportedFileExtensions).orElse(new HashSet<>()).stream().sorted().collect(joining(", ")));
     }
 }
