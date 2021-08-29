4c8f962c2208e2b4b0868a11741ab7fc1dce7354
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-08-29 22:52:19 +0200

    Feature/203 merge to master (#207)
    * #203 - Fixed Travis deployment. [skip travis]
    
    * #203 - Removed info about bintray repository.
    
    * #203 - Added missing maven-gpg-plugin plugin.

--- deploy/release.sh
+++ deploy/release.sh
@@ -11,6 +11,6 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     # Decrypting key files
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
-    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
     exit $?
 fi
