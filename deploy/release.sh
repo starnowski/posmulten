7d1949628d5fe5fe26d8617ae9b73cedfa3977ac
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-08-29 20:21:40 +0200

    Feature/203 nexus deploy (#205)
    * #203 - Added notes and settings file. [skip travis][skip cli]
    
    * #203 - Added maven wrapper.
    
    * #203 - Added maven wrapper.
    
    * #203 - Added notes [skip travis][skip ci]
    
    * #203 - Setting maven central repository.
    
    * #203 - Removed codacy action. [skip travis]

--- deploy/release.sh
+++ deploy/release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIRNAME="$(dirname $0)"
+
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     echo "Deploying tag $TRAVIS_TAG"
 
@@ -9,6 +11,6 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     # Decrypting key files
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
