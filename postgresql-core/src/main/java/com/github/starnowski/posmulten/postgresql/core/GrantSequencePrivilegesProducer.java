ada594896baea3f50ee99e04842fa51ecf785c5a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-08 23:01:08 +0200

    #65 - Added implementation for the GrantSequencePrivilegesProducer component. [skip travis] [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSequencePrivilegesProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/GrantSequencePrivilegesProducer.java
@@ -1,43 +1,47 @@
 /**
- *     Posmulten library is an open-source project for the generation
- *     of SQL DDL statements that make it easy for implementation of
- *     Shared Schema Multi-tenancy strategy via the Row Security
- *     Policies in the Postgres database.
- *
- *     Copyright (C) 2020  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posmulten library is an open-source project for the generation
+ * of SQL DDL statements that make it easy for implementation of
+ * Shared Schema Multi-tenancy strategy via the Row Security
+ * Policies in the Postgres database.
+ * <p>
+ * Copyright (C) 2020  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posmulten.postgresql.core;
 
 import com.github.starnowski.posmulten.postgresql.core.common.DefaultSQLDefinition;
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
 
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+
 public class GrantSequencePrivilegesProducer {
 
     public SQLDefinition produce(String schema, String sequence, String user) {
         validateParameters(schema, sequence, user);
-        return new DefaultSQLDefinition(prepareCreateScript(schema, sequence, user), prepareDropScript(schema, sequence, user));
+        return new DefaultSQLDefinition(prepareCreateScript(schema, sequence, user), prepareDropScript(schema, sequence, user), prepareCheckingStatements(schema, sequence, user));
     }
 
     private String prepareDropScript(String schema, String sequence, String user) {
         StringBuilder sb = new StringBuilder();
         sb.append("REVOKE ALL PRIVILEGES ON SEQUENCE ");
-        if (schema == null)
-        {
+        if (schema == null) {
             sb.append("public");
         } else {
             sb.append(schema);
@@ -55,8 +59,7 @@ public class GrantSequencePrivilegesProducer {
     private String prepareCreateScript(String schema, String sequence, String user) {
         StringBuilder sb = new StringBuilder();
         sb.append("GRANT ALL PRIVILEGES ON SEQUENCE ");
-        if (schema == null)
-        {
+        if (schema == null) {
             sb.append("public");
         } else {
             sb.append(schema);
@@ -71,25 +74,25 @@ public class GrantSequencePrivilegesProducer {
         return sb.toString();
     }
 
+    private List<String> prepareCheckingStatements(String schema, String sequence, String user) {
+        String pattern = "SELECT COUNT(1) FROM  information_schema.usage_privileges WHERE  grantee = '%1s' AND object_schema = '%2s' AND object_name = '%3s' AND object_type = 'SEQUENCE' AND privilege_type = 'USAGE';";
+        return singletonList(format(pattern, user, schema, sequence));
+    }
+
     private void validateParameters(String schema, String sequence, String user) {
-        if (schema != null && schema.trim().isEmpty())
-        {
+        if (schema != null && schema.trim().isEmpty()) {
             throw new IllegalArgumentException("schema cannot be blank");
         }
-        if (user == null)
-        {
+        if (user == null) {
             throw new IllegalArgumentException("user cannot be null");
         }
-        if (user.trim().isEmpty())
-        {
+        if (user.trim().isEmpty()) {
             throw new IllegalArgumentException("user cannot be blank");
         }
-        if (sequence == null)
-        {
+        if (sequence == null) {
             throw new IllegalArgumentException("sequence cannot be null");
         }
-        if (sequence.trim().isEmpty())
-        {
+        if (sequence.trim().isEmpty()) {
             throw new IllegalArgumentException("sequence cannot be blank");
         }
     }
