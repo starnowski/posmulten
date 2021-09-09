eab26b47b247e7e2f4fa33aea18a5235d58f18c4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-09 20:49:16 +0200

    #65 - Fixed implementation for the RLSPolicyProducer component. [skip travis]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/RLSPolicyProducer.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/rls/RLSPolicyProducer.java
@@ -140,7 +140,7 @@ public class RLSPolicyProducer {
         String table = parameters.getPolicyTable();
         String schema = parameters.getPolicySchema();
         String schemaName = schema == null ? "public" : schema;
-        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s'", name, table, schemaName);
+        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s';", name, table, schemaName);
     }
 
     private String prepareStatementThatChecksIfPolicyWithSpecifiedCmdExists(RLSPolicyProducerParameters parameters) {
@@ -166,7 +166,7 @@ public class RLSPolicyProducer {
             case DELETE:
                 cmd = "d";
         }
-        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s' AND pg.polcmd = '%4$s'", name, table, schemaName, cmd);
+        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s' AND pg.polcmd = '%4$s';", name, table, schemaName, cmd);
     }
 
     private String prepareStatementThatChecksIfPolicyForGranteeExists(RLSPolicyProducerParameters parameters) {
@@ -175,7 +175,7 @@ public class RLSPolicyProducer {
         String schema = parameters.getPolicySchema();
         String grantee = parameters.getGrantee();
         String schemaName = schema == null ? "public" : schema;
-        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn, pg_roles ro WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s' AND ro.oid = ANY(pg.polroles) AND ro.rolname = '%4$s'", name, table, schemaName, grantee);
+        return format("SELECT COUNT(1) FROM pg_catalog.pg_policy pg, pg_class pc, pg_catalog.pg_namespace pn, pg_roles ro WHERE pg.polrelid = pc.oid AND pc.relnamespace = pn.oid AND pg.polname = '%1$s' AND pc.relname = '%2$s' AND pn.nspname = '%3$s' AND ro.oid = ANY(pg.polroles) AND ro.rolname = '%4$s';", name, table, schemaName, grantee);
     }
 
     private boolean isCheckWithExpressionShouldBeApplied(RLSPolicyProducerParameters parameters) {
