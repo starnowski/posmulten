0072076d663776ce7c02be4fd6107fb1ffdb175f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-09 21:11:56 +0200

    #137 - Added IdentifierLengthValidator validator (not yet implemented!) [skip travis] [skip ci]

---
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IdentifierLengthValidator.java
@@ -0,0 +1,13 @@
+package com.github.starnowski.posmulten.postgresql.core.context;
+
+public class IdentifierLengthValidator extends AbstractIdentifierValidator{
+    @Override
+    public void init(SharedSchemaContextRequest sharedSchemaContextRequest) {
+
+    }
+
+    @Override
+    public ValidationResult validate(String identifier) {
+        return null;
+    }
+}
