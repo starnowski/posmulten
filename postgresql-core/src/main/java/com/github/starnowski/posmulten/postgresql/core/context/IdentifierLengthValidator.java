d9ec714d6d0b3c578e2a550ded8c1ba9324b4bde
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-11 19:03:34 +0200

    #137 - Added implementation for validator initialization. [skip travis][skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IdentifierLengthValidator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/IdentifierLengthValidator.java
@@ -19,13 +19,19 @@ public class IdentifierLengthValidator extends AbstractIdentifierValidator {
 
     @Override
     public void init(SharedSchemaContextRequest sharedSchemaContextRequest) throws InvalidSharedSchemaContextRequestException {
-        if (sharedSchemaContextRequest.getIdentifierMinLength() != null && sharedSchemaContextRequest.getIdentifierMinLength() <= 0) {
-            throw new InvalidSharedSchemaContextRequestException("The identifierMinLength property value can not be less or equal to zero");
+        if (sharedSchemaContextRequest.getIdentifierMinLength() != null) {
+            if (sharedSchemaContextRequest.getIdentifierMinLength() <= 0) {
+                throw new InvalidSharedSchemaContextRequestException("The identifierMinLength property value can not be less or equal to zero");
+            }
+            this.identifierMinLength = sharedSchemaContextRequest.getIdentifierMinLength();
         } else {
             this.identifierMinLength = 1;
         }
-        if (sharedSchemaContextRequest.getIdentifierMaxLength() != null && sharedSchemaContextRequest.getIdentifierMaxLength() <= 0) {
-            throw new InvalidSharedSchemaContextRequestException("The identifierMaxLength property value can not be less or equal to zero");
+        if (sharedSchemaContextRequest.getIdentifierMaxLength() != null) {
+            if (sharedSchemaContextRequest.getIdentifierMaxLength() <= 0) {
+                throw new InvalidSharedSchemaContextRequestException("The identifierMaxLength property value can not be less or equal to zero");
+            }
+            this.identifierMaxLength = sharedSchemaContextRequest.getIdentifierMaxLength();
         } else {
             this.identifierMaxLength = DEFAULT_MAXIMUM_NUMBER_OF_IDENTIFIER_CHARACTERS;
         }
