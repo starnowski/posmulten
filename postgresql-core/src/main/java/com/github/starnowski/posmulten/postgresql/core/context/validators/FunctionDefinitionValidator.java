a55e63a5557ba2f0b506722d79e35ec224616e8c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-12-22 18:20:59 +0100

    #137 - Added implementation. [skip travis] [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/FunctionDefinitionValidator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/validators/FunctionDefinitionValidator.java
@@ -1,13 +1,30 @@
 package com.github.starnowski.posmulten.postgresql.core.context.validators;
 
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+import com.github.starnowski.posmulten.postgresql.core.common.function.IFunctionDefinition;
+import com.github.starnowski.posmulten.postgresql.core.context.IIdentifierValidator;
 import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
 
 import java.util.List;
 
-public class FunctionDefinitionValidator implements ISQLDefinitionsValidator{
+public class FunctionDefinitionValidator implements ISQLDefinitionsValidator {
+
+    private final List<IIdentifierValidator> iIdentifierValidators;
+
+    public FunctionDefinitionValidator(List<IIdentifierValidator> iIdentifierValidators) {
+        this.iIdentifierValidators = iIdentifierValidators;
+    }
+
     @Override
     public void validate(List<SQLDefinition> sqlDefinitions) throws SharedSchemaContextBuilderException {
-        //TODO
+        for (SQLDefinition definition : sqlDefinitions) {
+            if (definition instanceof IFunctionDefinition) {
+                String functionReference = ((IFunctionDefinition) definition).getFunctionReference();
+                String functionName = functionReference.indexOf(".") == -1 ? functionReference : functionReference.substring(functionReference.indexOf(".") + 1);
+                for (IIdentifierValidator identifierValidator : iIdentifierValidators) {
+                    identifierValidator.validate(functionName);
+                }
+            }
+        }
     }
 }
