e6dfb5bf8c2ef713154c5a9f4d492f936719c260
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-01 17:50:12 +0100

    #218 - Added implementation for the CurrentTenantIdPropertyTypeEnricher type. [skip travis] [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/CurrentTenantIdPropertyTypeEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/CurrentTenantIdPropertyTypeEnricher.java
@@ -2,11 +2,11 @@ package com.github.starnowski.posmulten.postgresql.core.context.enrichers;
 
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
-import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
 
 public class CurrentTenantIdPropertyTypeEnricher implements ISharedSchemaContextEnricher {
     @Override
-    public ISharedSchemaContext enrich(ISharedSchemaContext context, SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
-        return null;
+    public ISharedSchemaContext enrich(ISharedSchemaContext context, SharedSchemaContextRequest request) {
+        context.setCurrentTenantIdPropertyType(request.getCurrentTenantIdPropertyType());
+        return context;
     }
 }
