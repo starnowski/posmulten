c134ca852948a369ccbf309a669afdea2876abbe
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-16 12:09:19 +0100

    #228 - Temporary commented code to test signing artifacts

--- deploy/release.sh
+++ deploy/release.sh
@@ -12,6 +12,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
     gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
-    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
+#    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
+    "${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true -P maven-central-deploy
     exit $?
 fi
