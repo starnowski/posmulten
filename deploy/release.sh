bb5750860c75b544207ba4944c7c050e615b9216
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-16 12:14:58 +0100

    #228 - Fixed the release.sh script

--- deploy/release.sh
+++ deploy/release.sh
@@ -2,17 +2,13 @@
 
 DIRNAME="$(dirname $0)"
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    echo "Deploying tag $TRAVIS_TAG"
+# Setting gpg directory path
+export GPG_DIR="$(dirname $0)"
 
-    # Setting gpg directory path
-    export GPG_DIR="$(dirname $0)"
-
-    # Decrypting key files
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
-    gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
-#    "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
-    "${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true -P maven-central-deploy
-    exit $?
-fi
+# Decrypting key files
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
+#"${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
+"${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true -P maven-central-deploy
+exit $?
