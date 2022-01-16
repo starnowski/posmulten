c88a97176ad1c0b85ff86f8d334c51b95326d330
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-16 11:35:00 +0100

    #228 - Setting md5 algorithm

--- deploy/release.sh
+++ deploy/release.sh
@@ -9,8 +9,9 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     export GPG_DIR="$(dirname $0)"
 
     # Decrypting key files
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+    gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
     "${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
     exit $?
 fi
