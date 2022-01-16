d66c7a184aff340de2e52fb2cca43c71d728036d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-16 12:20:36 +0100

    #228 - Uncommented code

--- deploy/release.sh
+++ deploy/release.sh
@@ -9,6 +9,5 @@ export GPG_DIR="$(dirname $0)"
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
 gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
-#"${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
-"${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true -P maven-central-deploy
+"${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
 exit $?
