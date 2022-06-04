5e5dcc88d5562fd4c7d63d1112a1f65927839813
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-06-04 16:38:51 +0200

    #252 Added tests case for correct execution order. [skip ci]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest.groovy
@@ -3,6 +3,8 @@ package com.github.starnowski.posmulten.postgresql.core.context
 import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
 import spock.lang.Specification
 
+import static com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING
+import static com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairDefaultPosition.AT_END
 import static java.util.stream.Collectors.toList
 
 class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specification {
@@ -13,7 +15,7 @@ class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specific
         given:
             def tested = new DefaultSharedSchemaContextBuilder()
             def sqlDefinitions = [Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition)]
-            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [CustomSQLDefinitionPairDefaultPosition.AT_END, CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING ]
+            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [AT_END, AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), AT_BEGINNING ]
             def expectedPositions = positionProviders.stream().map({ it -> it.getPosition() }).collect(toList())
             def lastBulderReference = tested
 
@@ -34,7 +36,7 @@ class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specific
         given:
             def tested = new DefaultSharedSchemaContextBuilder()
             def creationScripts = ["SELECT 89", "SELECT 15", "insert into some_table blablal;", "SELECT 500", "Alter TabLe"]
-            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [CustomSQLDefinitionPairDefaultPosition.AT_END, CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING ]
+            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [AT_END, AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), AT_BEGINNING ]
             def expectedPositions = positionProviders.stream().map({ it -> it.getPosition() }).collect(toList())
             def lastBulderReference = tested
 
@@ -58,7 +60,7 @@ class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specific
             def tested = new DefaultSharedSchemaContextBuilder()
             def creationScripts = ["SELECT 113", "SELECT 88", "insert into some_table values;", "SELECT 500", "Alter TabLe"]
             def dropScripts = ["DELETE", "SELECT 57", "delete FROM _xtable;", "DROP TABLE", "Alter TabLe"]
-            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [CustomSQLDefinitionPairDefaultPosition.AT_END, CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING ]
+            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [AT_END, AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), AT_BEGINNING ]
             def expectedPositions = positionProviders.stream().map({ it -> it.getPosition() }).collect(toList())
             def lastBulderReference = tested
 
@@ -83,7 +85,7 @@ class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specific
             def creationScripts = ["SELECT 113", "SELECT 88", "insert into some_table values;", "SELECT 500", "Alter TabLe"]
             def dropScripts = ["DELETE", "SELECT 57", "delete FROM _xtable;", "DROP TABLE", "Alter TabLe"]
             def checkingStatements = [["SELECT 144"], ["SELECT 1 from tab"], ["SELECT 43", "SELECT 88", "SELECT 91"], ["SELECT 776"], ["SELECT 13", "SELECT 1 FROM table"]]
-            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [CustomSQLDefinitionPairDefaultPosition.AT_END, CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING ]
+            List<CustomSQLDefinitionPairPositionProvider> positionProviders = [AT_END, AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), AT_BEGINNING ]
             def expectedPositions = positionProviders.stream().map({ it -> it.getPosition() }).collect(toList())
             def lastBulderReference = tested
 
@@ -100,6 +102,48 @@ class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specific
 
         and: "reference to builder should be the same"
             lastBulderReference == tested
+    }
+
+    def "should add custom sql definition in correct position"(){
+        given:
+            def tested = new DefaultSharedSchemaContextBuilder()
+            tested.setGrantee("john_doe")
+            def creation1 = "creat1"
+            def creation2 = "creat2"
+            def creation3 = "creat3"
+            def creation4 = "creat4"
+            def drop1 = "creat1"
+            def drop2 = "creat2"
+            def drop3 = "creat3"
+            def drop4 = "creat4"
+            def checkingStatements1 = ["SELECT 13", "SELECT 1 FROM table"]
+            def checkingStatements2 = ["SELECT 54 FROM table"]
+            def checkingStatements3 = ["SELECT 88"]
+            def checkingStatements4 = ["SELECT 46", "SELECT 1 FROM non_table"]
+
+            tested.addCustomSQLDefinition(AT_END, creation1, drop1, checkingStatements1)
+            tested.addCustomSQLDefinition(AT_BEGINNING, creation2, drop2, checkingStatements2)
+            tested.addCustomSQLDefinition(AT_BEGINNING, creation3, drop3, checkingStatements3)
+            tested.addCustomSQLDefinition(AT_END, creation4, drop4, checkingStatements4)
+
+        when:
+            def context = tested.build()
+
+        then:
+            context.getSqlDefinitions()[0].createScript == creation2
+            context.getSqlDefinitions()[0].dropScript == drop2
+            context.getSqlDefinitions()[0].checkingStatements == checkingStatements2
+            context.getSqlDefinitions()[2].createScript == creation3
+            context.getSqlDefinitions()[2].dropScript == drop3
+            context.getSqlDefinitions()[2].checkingStatements == checkingStatements3
+
+            def lastIndex = context.getSqlDefinitions().size() - 1
+            context.getSqlDefinitions()[lastIndex - 1].createScript == creation1
+            context.getSqlDefinitions()[lastIndex - 1].dropScript == drop1
+            context.getSqlDefinitions()[lastIndex - 1].checkingStatements == checkingStatements1
+            context.getSqlDefinitions()[lastIndex].createScript == creation4
+            context.getSqlDefinitions()[lastIndex].dropScript == drop4
+            context.getSqlDefinitions()[lastIndex].checkingStatements == checkingStatements4
     }
 
     private static class TestCustomSQLDefinitionPairPositionProvider implements CustomSQLDefinitionPairPositionProvider {
