7778f2606ceaed740b85de83911b223031cac037
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-06-04 21:48:22 +0200

    #252 Added test case (not yet finished!) [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/AbstractCustomSQLDefinitionsEnricher.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/enrichers/AbstractCustomSQLDefinitionsEnricher.java
@@ -5,6 +5,8 @@ import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaCont
 import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest;
 import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
 
+import java.util.Objects;
+
 public abstract class AbstractCustomSQLDefinitionsEnricher implements ISharedSchemaContextEnricher {
 
     private final CustomSQLDefinitionPairPositionProvider customSQLDefinitionPairPositionProvider;
@@ -15,7 +17,7 @@ public abstract class AbstractCustomSQLDefinitionsEnricher implements ISharedSch
 
     @Override
     public ISharedSchemaContext enrich(ISharedSchemaContext context, SharedSchemaContextRequest request) throws SharedSchemaContextBuilderException {
-        request.getCustomSQLDefinitionPairs().stream().filter(customSQLDefinitionPair -> customSQLDefinitionPair.getPosition().equals(customSQLDefinitionPairPositionProvider.getPosition()))
+        request.getCustomSQLDefinitionPairs().stream().filter(customSQLDefinitionPair -> Objects.equals(customSQLDefinitionPair.getPosition(), customSQLDefinitionPairPositionProvider.getPosition()))
                 .map(pair -> pair.getSqlDefinition())
                 .forEach(sqlDefinition -> context.addSQLDefinition(sqlDefinition));
         return context;
--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/AbstractCustomSQLDefinitionsEnricherTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/enrichers/AbstractCustomSQLDefinitionsEnricherTest.groovy
@@ -1,10 +1,61 @@
 package com.github.starnowski.posmulten.postgresql.core.context.enrichers
 
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
+import com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairPositionProvider
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext
+import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContext
+import com.github.starnowski.posmulten.postgresql.core.util.Pair
 import spock.lang.Specification
 
-abstract class AbstractCustomSQLDefinitionsEnricherTest extends Specification {
+abstract class AbstractCustomSQLDefinitionsEnricherTest<T extends AbstractCustomSQLDefinitionsEnricher> extends Specification {
+
+    def "should select correct custom definitions"(){
+        given:
+            DefaultSharedSchemaContextBuilder builder = new DefaultSharedSchemaContextBuilder()
+            builder.setGrantee("XXX")
+            def testData = getTestData()
+            testData.forEach({it -> builder.addCustomSQLDefinition(it.getKey(), it.getValue())})
+            def request = builder.getSharedSchemaContextRequestCopy()
+            ISharedSchemaContext context = new SharedSchemaContext()
+            T tested = getTestedObject()
+
+        when:
+            def result = tested.enrich(context, request)
+
+        then:
+            result.getSqlDefinitions().createScript == getExpectedCreationScripts()
+
+        and: "test data should contains values that should be ignored"
+            testData.containsAll(getIgnoredCreationScripts())
+
+        and: "collection of ignored values can not be empty"
+            !getIgnoredCreationScripts().isEmpty()
+    }
+
+    abstract T getTestedObject()
+
+    abstract List<Pair<ConstantCustomSQLDefinitionPairPositionProvider, SQLDefinition>> getTestData()
 
     abstract List<String> getExpectedCreationScripts()
 
-    abstract List<String> getIngoredCreationScripts()
+    abstract List<String> getIgnoredCreationScripts()
+
+    protected static pp(String position) {
+        return new ConstantCustomSQLDefinitionPairPositionProvider(position)
+    }
+
+    protected static class ConstantCustomSQLDefinitionPairPositionProvider implements CustomSQLDefinitionPairPositionProvider {
+
+        private String position
+
+        protected ConstantCustomSQLDefinitionPairPositionProvider(String position){
+            this.position = position
+        }
+
+        @Override
+        String getPosition() {
+            position
+        }
+    }
 }
