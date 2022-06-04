07aca004d700c618f74044a2e8a91f6d4c6cc52e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-06-04 12:58:37 +0200

    #252 Tests code refactoring. [skip ci]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest.groovy
@@ -7,22 +7,25 @@ import static java.util.stream.Collectors.toList
 
 class DefaultSharedSchemaContextBuilderCustomSQLDefinitionsTest extends Specification {
 
-
     def "should add custom SQL definitions with their positions "() {
         given:
             def tested = new DefaultSharedSchemaContextBuilder()
             def sqlDefinitions = [Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition), Mock(SQLDefinition)]
             List<CustomSQLDefinitionPairPositionProvider> positionProviders = [CustomSQLDefinitionPairDefaultPosition.AT_END, CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING, new TestCustomSQLDefinitionPairPositionProvider("xxx"), new TestCustomSQLDefinitionPairPositionProvider("test1"), CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING ]
             def expectedPositions = positionProviders.stream().map({ it -> it.getPosition() }).collect(toList())
+            def lastBulderReference = tested
 
         when:
             for (int i = 0; i < sqlDefinitions.size(); i++) {
-                tested = tested.addCustomSQLDefinition(positionProviders[i], sqlDefinitions[i])
+                lastBulderReference = lastBulderReference.addCustomSQLDefinition(positionProviders[i], sqlDefinitions[i])
             }
 
         then:
             tested.sharedSchemaContextRequestCopy.customSQLDefinitionPairs.position == expectedPositions
             tested.sharedSchemaContextRequestCopy.customSQLDefinitionPairs.sqlDefinition == sqlDefinitions
+
+        and: "reference to builder should be the same"
+            lastBulderReference == tested
     }
 
     private static class TestCustomSQLDefinitionPairPositionProvider implements CustomSQLDefinitionPairPositionProvider {
