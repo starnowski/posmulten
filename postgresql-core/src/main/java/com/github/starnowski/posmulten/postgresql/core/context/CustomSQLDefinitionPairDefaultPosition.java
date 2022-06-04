715a0caf052d9b024cc66f10b96d60ad32b859e9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-06-05 01:29:42 +0200

    #252 Added documentation (not yet finished!) [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/CustomSQLDefinitionPairDefaultPosition.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/CustomSQLDefinitionPairDefaultPosition.java
@@ -1,7 +1,16 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+/**
+ * Default implementation of {@link CustomSQLDefinitionPairPositionProvider} type.
+ */
 public enum CustomSQLDefinitionPairDefaultPosition implements CustomSQLDefinitionPairPositionProvider {
+    /**
+     * Custom definition is being added before all definitions created by builder
+     */
     AT_BEGINNING,
+    /**
+     * Custom definition is being added after all definitions created by builder
+     */
     AT_END;
 
     @Override
--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/CustomSQLDefinitionPairPositionProvider.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/CustomSQLDefinitionPairPositionProvider.java
@@ -1,6 +1,20 @@
 package com.github.starnowski.posmulten.postgresql.core.context;
 
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
+
+import java.util.List;
+
+/**
+ * Defines position for custom SQL definition passed to query builder.
+ * @see DefaultSharedSchemaContextBuilder#addCustomSQLDefinition(CustomSQLDefinitionPairPositionProvider, String)
+ * @see DefaultSharedSchemaContextBuilder#addCustomSQLDefinition(CustomSQLDefinitionPairPositionProvider, String, String)
+ * @see DefaultSharedSchemaContextBuilder#addCustomSQLDefinition(CustomSQLDefinitionPairPositionProvider, String, String, List)
+ * @see DefaultSharedSchemaContextBuilder#addCustomSQLDefinition(CustomSQLDefinitionPairPositionProvider, SQLDefinition)
+ */
 public interface CustomSQLDefinitionPairPositionProvider {
 
+    /**
+     * @return position for custom SQL definition
+     */
     String getPosition();
 }
