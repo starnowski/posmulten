ec2a7adba75405d7e74c964720df9419300366d9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-12-20 14:40:18 +0100

    #245 - Added test case [skip ci]

--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricherTest.groovy
@@ -47,35 +47,35 @@ class RLSPolicyConfigurationEnricherTest extends AbstractBaseTest {
     def "should set builder component with rls policy for table name '#tableName' in schema #tableSchema, rls policy name '#rlsPolicyName', tenant column '#tenantColumn', primaryKeyColumnsNameToTypeMap '#primaryKeyColumnsNameToTypeMap', createTenantColumnForTable '#createTenantColumnForTable', nameForFunctionThatChecksIfRecordExistsInTable '#nameForFunctionThatChecksIfRecordExistsInTable', validTenantValueConstraintName '#validTenantValueConstraintName', skipAddingOfTenantColumnDefaultValue '#skipAddingOfTenantColumnDefaultValue'"()
     {
         given:
-        def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
-        def entry = new TableEntry()
-                .setName(tableName)
-                .setSchema(Optional.ofNullable(tableSchema))
-                .setRlsPolicy(new RLSPolicy()
-                        .setName(rlsPolicyName)
-                        .setTenantColumn(tenantColumn)
-                        .setPrimaryKeyDefinition(new PrimaryKeyDefinition()
-                                .setPrimaryKeyColumnsNameToTypeMap(primaryKeyColumnsNameToTypeMap)
-                                .setNameForFunctionThatChecksIfRecordExistsInTable(nameForFunctionThatChecksIfRecordExistsInTable))
-                        .setCreateTenantColumnForTable(createTenantColumnForTable)
-                        .setValidTenantValueConstraintName(validTenantValueConstraintName)
-                        .setSkipAddingOfTenantColumnDefaultValue(skipAddingOfTenantColumnDefaultValue))
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+            def entry = new TableEntry()
+                    .setName(tableName)
+                    .setSchema(Optional.ofNullable(tableSchema))
+                    .setRlsPolicy(new RLSPolicy()
+                            .setName(rlsPolicyName)
+                            .setTenantColumn(tenantColumn)
+                            .setPrimaryKeyDefinition(new PrimaryKeyDefinition()
+                                    .setPrimaryKeyColumnsNameToTypeMap(primaryKeyColumnsNameToTypeMap)
+                                    .setNameForFunctionThatChecksIfRecordExistsInTable(nameForFunctionThatChecksIfRecordExistsInTable))
+                            .setCreateTenantColumnForTable(createTenantColumnForTable)
+                            .setValidTenantValueConstraintName(validTenantValueConstraintName)
+                            .setSkipAddingOfTenantColumnDefaultValue(skipAddingOfTenantColumnDefaultValue))
 
         when:
-        def result = tested.enrich(builder, entry)
+            def result = tested.enrich(builder, entry)
 
         then:
-        result == builder
-        1 * builder.createRLSPolicyForTable(new TableKey(tableName, tableSchema), primaryKeyColumnsNameToTypeMap, tenantColumn, rlsPolicyName)
-        1 * builder.setNameForFunctionThatChecksIfRecordExistsInTable(new TableKey(tableName, tableSchema), nameForFunctionThatChecksIfRecordExistsInTable)
-        1 * builder.registerCustomValidTenantValueConstraintNameForTable(new TableKey(tableName, tableSchema), validTenantValueConstraintName)
+            result == builder
+            1 * builder.createRLSPolicyForTable(new TableKey(tableName, tableSchema), primaryKeyColumnsNameToTypeMap, tenantColumn, rlsPolicyName)
+            1 * builder.setNameForFunctionThatChecksIfRecordExistsInTable(new TableKey(tableName, tableSchema), nameForFunctionThatChecksIfRecordExistsInTable)
+            1 * builder.registerCustomValidTenantValueConstraintNameForTable(new TableKey(tableName, tableSchema), validTenantValueConstraintName)
 
 
         where:
-        tableName   |   tableSchema         |   rlsPolicyName   |   tenantColumn    |   primaryKeyColumnsNameToTypeMap                                              |   createTenantColumnForTable  |   nameForFunctionThatChecksIfRecordExistsInTable  |   validTenantValueConstraintName  |   skipAddingOfTenantColumnDefaultValue
-        "t1"        |   "some_schema"       |   "rls_pol"       |   "tenant_co"     |   MapBuilder.mapBuilder().put("id", "bigint").build()                         |   false                       |   "is_record_exists"                              |   "table_ten_is_valid_con"        |   null
-        "table1"    |   "PROJECT_TEST"      |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build() |   null                        |   "is_table_exists"                               |   "tenant_table_shoul_be_valid"   |   false
-        "table5"    |   null                |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build() |   null                        |   "is_table_exists"                               |   "tenant_table_shoul_be_valid"   |   false
+            tableName   |   tableSchema         |   rlsPolicyName   |   tenantColumn    |   primaryKeyColumnsNameToTypeMap                                              |   createTenantColumnForTable  |   nameForFunctionThatChecksIfRecordExistsInTable  |   validTenantValueConstraintName  |   skipAddingOfTenantColumnDefaultValue
+            "t1"        |   "some_schema"       |   "rls_pol"       |   "tenant_co"     |   MapBuilder.mapBuilder().put("id", "bigint").build()                         |   false                       |   "is_record_exists"                              |   "table_ten_is_valid_con"        |   null
+            "table1"    |   "PROJECT_TEST"      |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build() |   null                        |   "is_table_exists"                               |   "tenant_table_shoul_be_valid"   |   false
+            "table5"    |   null                |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build() |   null                        |   "is_table_exists"                               |   "tenant_table_shoul_be_valid"   |   false
     }
 
     @Unroll
@@ -103,6 +103,35 @@ class RLSPolicyConfigurationEnricherTest extends AbstractBaseTest {
             tableName   |   rlsPolicyName   |   tenantColumn    |   primaryKeyColumnsNameToTypeMap
             "t1"        |   "rls_pol"       |   "tenant_co"     |   MapBuilder.mapBuilder().put("user_id", "int").build()
             "table1"    |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build()
+    }
+
+    @Unroll
+    def "should invoke createTenantColumnForTable method when property createTenantColumnForTable is set with the 'true' value for table name '#tableName' in schema #tableSchema, rls policy name '#rlsPolicyName', tenant column '#tenantColumn', primaryKeyColumnsNameToTypeMap '#primaryKeyColumnsNameToTypeMap'"()
+    {
+        given:
+        def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+        def entry = new TableEntry().setName(tableName)
+                .setSchema(Optional.ofNullable(tableSchema))
+                .setRlsPolicy(new RLSPolicy()
+                        .setName(rlsPolicyName)
+                        .setTenantColumn(tenantColumn)
+                        .setPrimaryKeyDefinition(new PrimaryKeyDefinition()
+                                .setPrimaryKeyColumnsNameToTypeMap(primaryKeyColumnsNameToTypeMap))
+                        .setCreateTenantColumnForTable(true))
+
+        when:
+        def result = tested.enrich(builder, entry)
+
+        then:
+        result == builder
+        1 * builder.createRLSPolicyForTable(new TableKey(tableName, tableSchema), primaryKeyColumnsNameToTypeMap, tenantColumn, rlsPolicyName)
+        1 * builder.createTenantColumnForTable(new TableKey(tableName, tableSchema))
+
+        where:
+        tableName   |   tableSchema |   rlsPolicyName   |   tenantColumn    |   primaryKeyColumnsNameToTypeMap
+        "t1"        |   "xxx"       |   "rls_pol"       |   "tenant_co"     |   MapBuilder.mapBuilder().put("user_id", "int").build()
+        "table1"    |   "yyy"       |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build()
+        "table5"    |   null        |   "table_rls_pol" |   "ten_id"        |   MapBuilder.mapBuilder().put("tab_id", "bigint").put("uuid", "UUID").build()
     }
 
     @Unroll
