a03fd54cf382284251af6ec84c919c83fa51d9a7
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-12-20 15:40:40 +0100

    #245 - Code refactoring [skip ci]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/ForeignKeyConfigurationEnricher.java
@@ -3,9 +3,11 @@ package com.github.starnowski.posmulten.configuration.core;
 import com.github.starnowski.posmulten.configuration.core.model.ForeignKeyConfiguration;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 
+import java.util.Optional;
+
 public class ForeignKeyConfigurationEnricher {
 
-    public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, String tableName, ForeignKeyConfiguration foreignKeyConfiguration) {
+    public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, String tableName, Optional<String> tableSchema, ForeignKeyConfiguration foreignKeyConfiguration) {
         return builder.createSameTenantConstraintForForeignKey(tableName, foreignKeyConfiguration.getTableName(), foreignKeyConfiguration.getForeignKeyPrimaryKeyColumnsMappings(), foreignKeyConfiguration.getConstraintName());
     }
 }
