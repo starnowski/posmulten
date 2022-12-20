6de79ec7bee55ea06c4661698523460014bded1c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-12-20 14:33:02 +0100

    #245 - Added implementation [skip ci]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/RLSPolicyConfigurationEnricher.java
@@ -3,22 +3,34 @@ package com.github.starnowski.posmulten.configuration.core;
 import com.github.starnowski.posmulten.configuration.core.model.RLSPolicy;
 import com.github.starnowski.posmulten.configuration.core.model.TableEntry;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+import com.github.starnowski.posmulten.postgresql.core.context.TableKey;
 
 public class RLSPolicyConfigurationEnricher implements ITableEntryEnricher {
     @Override
     public DefaultSharedSchemaContextBuilder enrich(DefaultSharedSchemaContextBuilder builder, TableEntry tableEntry) {
         if (tableEntry != null && tableEntry.getRlsPolicy() != null) {
             RLSPolicy rlsPolicy = tableEntry.getRlsPolicy();
-            builder.createRLSPolicyForTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyDefinition() == null ? null : rlsPolicy.getPrimaryKeyDefinition().getPrimaryKeyColumnsNameToTypeMap(), rlsPolicy.getTenantColumn(), rlsPolicy.getName());
-
+            if (tableEntry.getSchema() == null) {
+                builder.createRLSPolicyForTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyDefinition() == null ? null : rlsPolicy.getPrimaryKeyDefinition().getPrimaryKeyColumnsNameToTypeMap(), rlsPolicy.getTenantColumn(), rlsPolicy.getName());
+            } else {
+                builder.createRLSPolicyForTable(new TableKey(tableEntry.getName(), tableEntry.getSchema().orElse(null)), rlsPolicy.getPrimaryKeyDefinition() == null ? null : rlsPolicy.getPrimaryKeyDefinition().getPrimaryKeyColumnsNameToTypeMap(), rlsPolicy.getTenantColumn(), rlsPolicy.getName());
+            }
             if (Boolean.TRUE.equals(rlsPolicy.getCreateTenantColumnForTable())) {
                 builder.createTenantColumnForTable(tableEntry.getName());
             }
             if (rlsPolicy.getPrimaryKeyDefinition() != null && rlsPolicy.getPrimaryKeyDefinition().getNameForFunctionThatChecksIfRecordExistsInTable() != null) {
-                builder.setNameForFunctionThatChecksIfRecordExistsInTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyDefinition().getNameForFunctionThatChecksIfRecordExistsInTable());
+                if (tableEntry.getSchema() == null) {
+                    builder.setNameForFunctionThatChecksIfRecordExistsInTable(tableEntry.getName(), rlsPolicy.getPrimaryKeyDefinition().getNameForFunctionThatChecksIfRecordExistsInTable());
+                } else {
+                    builder.setNameForFunctionThatChecksIfRecordExistsInTable(new TableKey(tableEntry.getName(), tableEntry.getSchema().orElse(null)), rlsPolicy.getPrimaryKeyDefinition().getNameForFunctionThatChecksIfRecordExistsInTable());
+                }
             }
             if (rlsPolicy.getValidTenantValueConstraintName() != null) {
-                builder.registerCustomValidTenantValueConstraintNameForTable(tableEntry.getName(), rlsPolicy.getValidTenantValueConstraintName());
+                if (tableEntry.getSchema() == null) {
+                    builder.registerCustomValidTenantValueConstraintNameForTable(tableEntry.getName(), rlsPolicy.getValidTenantValueConstraintName());
+                } else {
+                    builder.registerCustomValidTenantValueConstraintNameForTable(new TableKey(tableEntry.getName(), tableEntry.getSchema().orElse(null)), rlsPolicy.getValidTenantValueConstraintName());
+                }
             }
             if (Boolean.TRUE.equals(rlsPolicy.getSkipAddingOfTenantColumnDefaultValue())) {
                 builder.skipAddingOfTenantColumnDefaultValueForTable(tableEntry.getName());
