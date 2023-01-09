1161187b8ba382adfc375dad1354cfeb2389dc9a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-01-10 00:27:07 +0100

    #256 - Added entry class for custom SQL definition [skip ci]

---
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/model/CustomDefinitionEntry.java
@@ -0,0 +1,39 @@
+package com.github.starnowski.posmulten.configuration.yaml.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomDefinitionEntry {
+
+    @JsonProperty(value = "position", required = true)
+    @NotBlank
+    private CustomDefinitionPosition position;
+    @JsonProperty(value = "creation_script")
+    private String creationScript;
+    @JsonProperty(value = "drop_script")
+    private String dropScript;
+    @JsonProperty(value = "custom_position")
+    private String customPosition;
+    @JsonProperty(value = "validation_scripts")
+    private List<String> validationScripts;
+
+    public static enum CustomDefinitionPosition {
+        AT_END,
+        AT_BEGINNING,
+        CUSTOM
+    }
+}
