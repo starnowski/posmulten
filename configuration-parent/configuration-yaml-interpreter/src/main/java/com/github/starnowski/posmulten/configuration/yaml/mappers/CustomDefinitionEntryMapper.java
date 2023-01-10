57c449e3d1f9e09eb21a65b7affefdfc0e402d4a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-01-10 22:32:31 +0100

    #256 - Code refactoring [skip ci]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/CustomDefinitionEntryMapper.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/CustomDefinitionEntryMapper.java
@@ -6,13 +6,13 @@ import com.github.starnowski.posmulten.configuration.yaml.model.CustomDefinition
 public class CustomDefinitionEntryMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry, CustomDefinitionEntry> {
     @Override
     public CustomDefinitionEntry map(com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry input) {
-        return new CustomDefinitionEntry().setCreationScript(input.getCreationScript()).setDropScript(input.getDropScript())
+        return input == null ? null : new CustomDefinitionEntry().setCreationScript(input.getCreationScript()).setDropScript(input.getDropScript())
                 .setPosition(input.getPosition()).setCustomPosition(input.getCustomPosition()).setValidationScripts(input.getValidationScripts());
     }
 
     @Override
     public com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry unmap(CustomDefinitionEntry output) {
-        return new com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry().setCreationScript(output.getCreationScript())
+        return output == null ? null : new com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry().setCreationScript(output.getCreationScript())
                 .setDropScript(output.getDropScript()).setPosition(output.getPosition()).setCustomPosition(output.getCustomPosition())
                 .setValidationScripts(output.getValidationScripts());
     }
