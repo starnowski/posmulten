d6e0f42301e635fe8029a8550a27426e1917df2e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-01-11 09:30:59 +0100

    #256 - Added implementation [skip ci]

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntryEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntryEnricher.java
@@ -2,11 +2,33 @@ package com.github.starnowski.posmulten.configuration.core;
 
 import com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry;
 import com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairDefaultPosition;
+import com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairPositionProvider;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CustomDefinitionEntryEnricher {
+
+    private final static Map<String, CustomSQLDefinitionPairPositionProvider> defaultPositionsMap = Stream.of(CustomSQLDefinitionPairDefaultPosition.values()).collect(Collectors.toMap(Enum::name, e -> e));
+
     public void enrich(DefaultSharedSchemaContextBuilder builder, CustomDefinitionEntry cd) {
-        CustomSQLDefinitionPairDefaultPosition defaultPosition = CustomSQLDefinitionPairDefaultPosition.valueOf(cd.getPosition().name());
-        builder.addCustomSQLDefinition(defaultPosition == null ? () -> cd.getCustomPosition() : defaultPosition, cd.getCreationScript(), cd.getDropScript(), cd.getValidationScripts());
+        CustomSQLDefinitionPairPositionProvider provider = defaultPositionsMap.containsKey(cd.getPosition().name()) ? defaultPositionsMap.get(cd.getPosition().name()) : new CustomPositionProvider(cd.getCustomPosition());
+        builder.addCustomSQLDefinition(provider, cd.getCreationScript(), cd.getDropScript(), cd.getValidationScripts());
+    }
+
+    private static final class CustomPositionProvider implements CustomSQLDefinitionPairPositionProvider {
+
+        private final String position;
+
+        public CustomPositionProvider(String position) {
+            this.position = position;
+        }
+
+        @Override
+        public String getPosition() {
+            return this.position;
+        }
     }
 }
--- configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntryEnricherTest.groovy
+++ configuration-parent/configuration-core/src/test/groovy/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntryEnricherTest.groovy
@@ -2,10 +2,12 @@ package com.github.starnowski.posmulten.configuration.core
 
 import com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry
 import com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairDefaultPosition
+import com.github.starnowski.posmulten.postgresql.core.context.CustomSQLDefinitionPairPositionProvider
 import spock.lang.Unroll
 
 import static com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry.CustomDefinitionPosition.AT_BEGINNING
 import static com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry.CustomDefinitionPosition.AT_END
+import static com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry.CustomDefinitionPosition.CUSTOM
 
 
 class CustomDefinitionEntryEnricherTest extends AbstractBaseTest {
@@ -28,5 +30,30 @@ class CustomDefinitionEntryEnricherTest extends AbstractBaseTest {
             new CustomDefinitionEntry().setPosition(AT_END).setCreationScript("X1").setDropScript("X2")       || CustomSQLDefinitionPairDefaultPosition.AT_END | "X1" | "X2" | null
             new CustomDefinitionEntry().setPosition(AT_BEGINNING).setCreationScript("G").setDropScript("H") || CustomSQLDefinitionPairDefaultPosition.AT_BEGINNING  | "G" | "H" | null
             new CustomDefinitionEntry().setPosition(AT_END).setCreationScript("X1").setValidationScripts(["14", "select 1"]) || CustomSQLDefinitionPairDefaultPosition.AT_END    | "X1" | null | ["14", "select 1"]
+    }
+
+    @Unroll
+    def "should set correct custom definition with custom position '#expectedPosition', creation script #cs, drop script #ds and validation scripts #vs"(){
+        given:
+            def builder = prepareBuilderMockWithZeroExpectationOfMethodsInvocation()
+            CustomSQLDefinitionPairPositionProvider passedCustomPositionProvider = (CustomSQLDefinitionPairPositionProvider)null
+            definition.setPosition(CUSTOM)
+
+        when:
+            tested.enrich(builder, definition)
+
+        then:
+            1 * builder.addCustomSQLDefinition(_, cs, ds, vs) >> {
+                parameters ->
+                    passedCustomPositionProvider = (CustomSQLDefinitionPairPositionProvider)parameters[0]
+                    builder
+            }
+            passedCustomPositionProvider.getPosition() == expectedPosition
+
+        where:
+            definition                                                                                      || expectedPosition | cs   |   ds |   vs
+            new CustomDefinitionEntry().setCreationScript("X1").setDropScript("X2").setCustomPosition("Some where near")                         || "Some where near" | "X1" | "X2" | null
+            new CustomDefinitionEntry().setCreationScript("G").setDropScript("H").setCustomPosition("-1")                             || "-1"  | "G" | "H" | null
+            new CustomDefinitionEntry().setCreationScript("X1").setValidationScripts(["14", "select 1"]).setCustomPosition("37")      || "37"    | "X1" | null | ["14", "select 1"]
     }
 }
