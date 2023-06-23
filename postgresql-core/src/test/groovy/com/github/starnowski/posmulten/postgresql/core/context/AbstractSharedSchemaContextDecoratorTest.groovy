241927148aff1300e6d2c66ad74145b7b97b342e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-23 21:05:34 +0200

    #283 - Added test case [skip ci]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/AbstractSharedSchemaContextDecoratorTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/AbstractSharedSchemaContextDecoratorTest.groovy
@@ -63,6 +63,33 @@ abstract class AbstractSharedSchemaContextDecoratorTest<T extends AbstractShared
             results.get(1).getDropScript() == expectedStatement2
     }
 
+    def "GetSqlDefinitions - getCheckingStatements()"() {
+        given:
+            def val1 = "Value2"
+            def val2 = "Some template}}"
+            def testStatement1 = "Drop fun( " + getFirstTemplateVariable() + "and second part " + getSecondTemplateVariable() + "end" + val2 + "end"
+            def testStatement2 = "nnnn tab( " + getFirstTemplateVariable() + "and second part " + getSecondTemplateVariable() + "end" + getFirstTemplateVariable()
+            def testStatement3 = "adfafdzcvzvc tab( " + getFirstTemplateVariable() + "and second part " + getSecondTemplateVariable() + "end" + getFirstTemplateVariable()
+            def expectedStatement1 = "Drop fun( " + val1 + "and second part " + val2 + "end" + val2 + "end"
+            def expectedStatement2 = "nnnn tab( " + val1 + "and second part " + val2 + "end" + val1
+            def expectedStatement3 = "adfafdzcvzvc tab( " + val1 + "and second part " + val2 + "end" + val1
+            ISharedSchemaContext sharedSchemaContext = Mock(ISharedSchemaContext)
+            def tested = prepareTestedObject(sharedSchemaContext, val1, val2)
+            SQLDefinition def1 = Mock(SQLDefinition)
+            SQLDefinition def2 = Mock(SQLDefinition)
+            def1.getCheckingStatements() >> [testStatement1, testStatement2]
+            def2.getCheckingStatements() >> [testStatement3]
+
+        when:
+            def results = tested.getSqlDefinitions()
+
+        then:
+            1 * sharedSchemaContext.getSqlDefinitions() >> [def1, def2]
+            results.size() == 2
+            results.get(0).getCheckingStatements() == [expectedStatement1, expectedStatement2]
+            results.get(1).getCheckingStatements() == [expectedStatement3]
+    }
+
     def "GetTenantHasAuthoritiesFunctionInvocationFactory"() {
         given:
             def val1 = "eeesdf"
