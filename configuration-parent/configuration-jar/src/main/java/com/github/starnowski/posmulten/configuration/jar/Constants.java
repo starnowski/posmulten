2f4e1432e15b78bcfeb7919b715177945f17a186
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-26 19:13:25 +0200

    #283 - Added tests case [skip ci]

--- configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/Constants.java
+++ configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String VALIDATION_STATEMENTS_PATH_PROPERTY = "posmulten.configuration.validation.statements.path";
     public static final String PRINT_PROJECT_VERSION_PROPERTY = "posmulten.configuration.config.version.print";
     public static final String PROJECT_VERSION_PROPERTY = "configuration.jar.project.version";
+    public static final String CONTEXT_DECORATOR_REPLACECHARACTERSMAP_PROPERTY = "posmulten.configuration.config.context.decorator.replaceCharactersMap";
+    public static final String CONTEXT_DECORATOR_REPLACECHARACTERSMAP_SEPARATOR_PROPERTY = "posmulten.configuration.config.context.decorator.replaceCharactersMap.separator";
 }
--- configuration-parent/configuration-jar/src/test/groovy/com/github/starnowski/posmulten/configuration/jar/SystemPropertiesDefaultDecoratorContextSupplierTest.groovy
+++ configuration-parent/configuration-jar/src/test/groovy/com/github/starnowski/posmulten/configuration/jar/SystemPropertiesDefaultDecoratorContextSupplierTest.groovy
@@ -3,6 +3,7 @@ package com.github.starnowski.posmulten.configuration.jar
 import com.github.starnowski.posmulten.configuration.SystemPropertyReader
 import spock.lang.Specification
 import spock.lang.Unroll
+import static com.github.starnowski.posmulten.configuration.jar.Constants.CONTEXT_DECORATOR_REPLACECHARACTERSMAP_PROPERTY
 
 class SystemPropertiesDefaultDecoratorContextSupplierTest extends Specification {
 
@@ -16,6 +17,14 @@ class SystemPropertiesDefaultDecoratorContextSupplierTest extends Specification 
             def result = tested.get()
 
         then:
+            1 * systemPropertyReader.read(CONTEXT_DECORATOR_REPLACECHARACTERSMAP_PROPERTY) >> systemProperty
+            result.getReplaceCharactersMap() == replaceCharactersMap
 
+        where:
+            systemProperty                                              ||  replaceCharactersMap
+            "dbname=some_public_db"                                     ||  [dbname: "some_public_db"]
+            "dbname=some_public_db,grantee=user"                        ||  [dbname: "some_public_db", grantee: "user"]
+            "{{schema}}=non_public_schema,{{grantee}}=johnDoe"          ||  ["{{schema}}": "non_public_schema", "{{grantee}}": "johnDoe"]
+            "\$schema=non_public_schema,-->user<--=johnDoe,ro;le=admin" ||  ["\$schema": "non_public_schema", "-->user<--": "johnDoe", "ro;le": "admin"]
     }
 }
