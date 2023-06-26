046980b8d248eaa4ce6251d75a985789811817bd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-26 19:36:16 +0200

    #283 - Added implementation [skip ci]

--- configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/SystemPropertiesDefaultDecoratorContextSupplier.java
+++ configuration-parent/configuration-jar/src/main/java/com/github/starnowski/posmulten/configuration/jar/SystemPropertiesDefaultDecoratorContextSupplier.java
@@ -3,7 +3,17 @@ package com.github.starnowski.posmulten.configuration.jar;
 import com.github.starnowski.posmulten.configuration.SystemPropertyReader;
 import com.github.starnowski.posmulten.postgresql.core.context.decorator.DefaultDecoratorContext;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.github.starnowski.posmulten.configuration.jar.Constants.CONTEXT_DECORATOR_REPLACECHARACTERSMAP_PROPERTY;
+
 public class SystemPropertiesDefaultDecoratorContextSupplier {
+
+    private static String REPLACECHARACTERSMAP_PAIR_PATTERN = "(.*)=(.*)";
+    private static Pattern replaceCharactersMapPattern = Pattern.compile(REPLACECHARACTERSMAP_PAIR_PATTERN);
 
     private final SystemPropertyReader systemPropertyReader;
 
@@ -17,6 +27,19 @@ public class SystemPropertiesDefaultDecoratorContextSupplier {
 
     public DefaultDecoratorContext get()
     {
-        return null;
+        DefaultDecoratorContext.DefaultDecoratorContextBuilder decoratorContextBuilder = DefaultDecoratorContext.builder();
+        String patternsValue = systemPropertyReader.read(CONTEXT_DECORATOR_REPLACECHARACTERSMAP_PROPERTY);
+        if (patternsValue != null) {
+            String[] patterns = patternsValue.split(",");
+            Map<String, String> replaceCharactersMap = new HashMap<>();
+            for (String patternPari : patterns) {
+                Matcher m = replaceCharactersMapPattern.matcher(patternPari);
+                if (m.find()) {
+                    replaceCharactersMap.put(m.group(1), m.group(2));
+                }
+            }
+            decoratorContextBuilder.withReplaceCharactersMap(replaceCharactersMap);
+        }
+        return decoratorContextBuilder.build();
     }
 }
