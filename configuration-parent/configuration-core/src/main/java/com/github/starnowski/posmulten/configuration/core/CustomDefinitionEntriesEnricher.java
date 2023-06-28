fc337f47596a3c421f77d97b6963b6101fe58dc2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-28 19:08:14 +0200

    #283 - Added licence header to configuration-core

--- configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntriesEnricher.java
+++ configuration-parent/configuration-core/src/main/java/com/github/starnowski/posmulten/configuration/core/CustomDefinitionEntriesEnricher.java
@@ -1,3 +1,26 @@
+/**
+ *     Posmulten library is an open-source project for the generation
+ *     of SQL DDL statements that make it easy for implementation of
+ *     Shared Schema Multi-tenancy strategy via the Row Security
+ *     Policies in the Postgres database.
+ *
+ *     Copyright (C) 2020  Szymon Tarnowski
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ */
 package com.github.starnowski.posmulten.configuration.core;
 
 import com.github.starnowski.posmulten.configuration.core.model.CustomDefinitionEntry;
