f41cf3f4486cbf4ce4104264469e5f9530599a37
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-26 20:46:22 +0200

    #295 - Added comparator tests cases [skip ci]

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/comparable/DefaultSharedSchemaContextComparatorTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/context/comparable/DefaultSharedSchemaContextComparatorTest.groovy
@@ -40,5 +40,39 @@ class DefaultSharedSchemaContextComparatorTest extends Specification {
         where:
             left | right ||  onlyOnLeft || onlyOnRight
             ["x1", "x23", "cdasdf", "com one"]      | ["x1", "cdasdf", "this is new",  "com one"] || ["x23"] || ["this is new"]
+            ["x1", "x23", "cdasdf", "com one"]      | ["x1", "cdasdf", "com one"] || ["x23"] || []
+            ["x1", "cdasdf", "com one"]      | ["x1", "cdasdf", "this is new",  "com one"] || [] || ["this is new"]
+    }
+
+    @Unroll
+    def "should return empty list as differences for creation scripts when there is no differences between definitions for creation scripts #collection"(){
+        given:
+            SharedSchemaContextComparator tested = new DefaultSharedSchemaContextComparator()
+            ISharedSchemaContext leftContext = Mock(ISharedSchemaContext)
+            ISharedSchemaContext rightContext = Mock(ISharedSchemaContext)
+            leftContext.getSqlDefinitions() >> {
+                collection.stream().map({
+                    def defMock = Mock(SQLDefinition)
+                    defMock.getCreateScript() >> it
+                    defMock
+                }).collect(Collectors.toList())
+            }
+            rightContext.getSqlDefinitions() >> {
+                collection.stream().map({
+                    def defMock = Mock(SQLDefinition)
+                    defMock.getCreateScript() >> it
+                    defMock
+                }).collect(Collectors.toList())
+            }
+
+        when:
+            SharedSchemaContextComparator.SharedSchemaContextComparableResults result = tested.diff(leftContext, rightContext)
+
+        then:
+            result.getCreationScriptsDifferences().getExistedOnlyOnLeft().isEmpty()
+            result.getCreationScriptsDifferences().getExistedOnlyOnRight().isEmpty()
+
+        where:
+            collection << [["x1", "cdasdf", "this is new",  "com one"]]
     }
 }
