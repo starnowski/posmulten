862da5d9af742b41091e2cefd8bc3cf4dfa427ef
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-26 20:31:07 +0200

    #295 - Added comparator implementation [skip ci]

--- postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/comparable/DefaultSharedSchemaContextComparator.java
+++ postgresql-core/src/main/java/com/github/starnowski/posmulten/postgresql/core/context/comparable/DefaultSharedSchemaContextComparator.java
@@ -2,9 +2,13 @@ package com.github.starnowski.posmulten.postgresql.core.context.comparable;
 
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 
+import java.util.stream.Collectors;
+
 public class DefaultSharedSchemaContextComparator implements SharedSchemaContextComparator {
     @Override
     public SharedSchemaContextComparableResults diff(ISharedSchemaContext left, ISharedSchemaContext right) {
-        return null;
+        return new SharedSchemaContextComparableResults(
+                new ComparableResult(left.getSqlDefinitions().stream().map(def -> def.getCreateScript()).filter(cre -> right.getSqlDefinitions().stream().map(def1 -> def1.getCreateScript()).noneMatch(cre1 -> cre.equals(cre1))).collect(Collectors.toList())
+                        , right.getSqlDefinitions().stream().map(def -> def.getCreateScript()).filter(cre -> left.getSqlDefinitions().stream().map(def1 -> def1.getCreateScript()).noneMatch(cre1 -> cre.equals(cre1))).collect(Collectors.toList())), null, null);
     }
 }
