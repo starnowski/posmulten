7e21758ef864e34a549a831d4b496729bdd33987
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-01 17:02:23 +0100

    #295 - Added factory object [skip ci]

---
+++ openwebstart/src/main/java/com/github/starnowski/posmulten/openwebstart/YamlSharedSchemaContextFactory.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posmulten.openwebstart;
+
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+
+public class YamlSharedSchemaContextFactory {
+
+    public ISharedSchemaContext build(String yaml) {
+        //TODO
+        return null;
+    }
+}
