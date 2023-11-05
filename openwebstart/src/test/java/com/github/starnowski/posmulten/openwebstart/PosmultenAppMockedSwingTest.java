bd155820941c739d5fc8ccb35687efcc9043950a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-05 22:54:48 +0100

    #295 - Setting -100,-100 location for window for tests purposes on Linux OS

--- openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
+++ openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
@@ -58,7 +58,11 @@ class PosmultenAppMockedSwingTest {
         PosmultenApp frame = GuiActionRunner.execute(() -> new PosmultenApp(factory));
         window = new FrameFixture(frame);
         window.show(); // shows the frame to test
-        frame.setLocation(-1, -1);
+        String osName = System.getProperty("os.name");
+        if (osName != null && osName.toLowerCase().contains("linux")) {
+            //Hack to fix issue for ubuntu and xvfb : org.assertj.swing.exception.ActionFailedException: The component to click is out of the boundaries of the screen
+            frame.setLocation(-100, -100);
+        }
     }
 
     @Test
