3340f66ce57e0f330a62413e6dc0b3764e5caeea
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-05 16:00:20 +0100

    #295 - Added first implementation [skip ci]

--- openwebstart/src/main/java/com/github/starnowski/posmulten/openwebstart/PosmultenApp.java
+++ openwebstart/src/main/java/com/github/starnowski/posmulten/openwebstart/PosmultenApp.java
@@ -1,10 +1,15 @@
 package com.github.starnowski.posmulten.openwebstart;
 
+import com.github.starnowski.posmulten.configuration.core.exceptions.InvalidConfigurationException;
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+import com.github.starnowski.posmulten.postgresql.core.context.decorator.DefaultDecoratorContext;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
 import net.miginfocom.layout.AC;
 import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.util.stream.Collectors;
 
 public class PosmultenApp extends JFrame {
     private final YamlSharedSchemaContextFactory factory;
@@ -34,7 +39,14 @@ public class PosmultenApp extends JFrame {
         submitButton.addActionListener(e -> {
             System.out.println("actionPerformed : " + e.getActionCommand());
             String inputCode = inputTextArea.getText();
-            outputTextArea1.setText(inputCode);
+            try {
+                ISharedSchemaContext context = factory.build(inputCode, DefaultDecoratorContext.builder().build());
+                outputTextArea1.setText(context.getSqlDefinitions().stream().map(definition -> definition.getCreateScript()).collect(Collectors.joining("\n")));
+            } catch (InvalidConfigurationException ex) {
+                throw new RuntimeException(ex);
+            } catch (SharedSchemaContextBuilderException ex) {
+                throw new RuntimeException(ex);
+            }
             outputTextArea2.setText(inputCode);
             outputTextArea3.setText(inputCode);
         });
