5b0a47ad007fff73d7333aad197b9ee17403100b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-09 19:42:05 +0100

    #295 - Fixed pipeline with using method "getMovedComponent"

--- openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
+++ openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
@@ -208,7 +208,7 @@ class PosmultenAppMockedSwingTest {
         List<SQLDefinition> definitions = asList(sqlDef("DEF 1", null), sqlDef("ALTER DEFINIT and Function", null));
         Mockito.when(context.getSqlDefinitions()).thenReturn(definitions);
         getMovedComponent(window.textBox(CONFIGURATION_TEXTFIELD_NAME)).enterText(yaml);
-        getComponentWithDefaultSettings(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME)).check();
+        getMovedComponent(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME)).check();
         //Add parameter index 0
         addParameter(0, "{{some_key}}", "value1");
         addParameter(1, "url", "http://host");
@@ -237,7 +237,7 @@ class PosmultenAppMockedSwingTest {
         List<SQLDefinition> definitions = asList(sqlDef("DEF 1", null), sqlDef("ALTER DEFINIT and Function", null));
         Mockito.when(context.getSqlDefinitions()).thenReturn(definitions);
         getMovedComponent(window.textBox(CONFIGURATION_TEXTFIELD_NAME)).enterText(yaml);
-        getComponentWithDefaultSettings(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME)).check();
+        getMovedComponent(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME)).check();
         //Add parameter index 0
         addParameter(0, "{{some_key}}", "value1");
         addParameter(1, "url", "http://host");
