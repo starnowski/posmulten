57103c8ed539d657fb6653cd6613d98222eea8cf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-09 20:21:27 +0100

    #295 - Setting check

--- openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
+++ openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
@@ -78,9 +78,6 @@ class PosmultenAppMockedSwingTest {
         window = new FrameFixture(tested);
         window.show(); // shows the frame to test
         tested.setLocation(0, 0);
-        if (isRunningOnVirtualScreen) {
-            window.maximize();
-        }
     }
 
     @Test
@@ -208,7 +205,8 @@ class PosmultenAppMockedSwingTest {
         List<SQLDefinition> definitions = asList(sqlDef("DEF 1", null), sqlDef("ALTER DEFINIT and Function", null));
         Mockito.when(context.getSqlDefinitions()).thenReturn(definitions);
         getMovedComponent(window.textBox(CONFIGURATION_TEXTFIELD_NAME)).enterText(yaml);
-        selectCheckBox(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME), true);
+        window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME).check();
+//        selectCheckBox(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME), true);
         //Add parameter index 0
         addParameter(0, "{{some_key}}", "value1");
         addParameter(1, "url", "http://host");
@@ -237,7 +235,8 @@ class PosmultenAppMockedSwingTest {
         List<SQLDefinition> definitions = asList(sqlDef("DEF 1", null), sqlDef("ALTER DEFINIT and Function", null));
         Mockito.when(context.getSqlDefinitions()).thenReturn(definitions);
         getMovedComponent(window.textBox(CONFIGURATION_TEXTFIELD_NAME)).enterText(yaml);
-        selectCheckBox(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME), true);
+        window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME).check();
+//        selectCheckBox(window.checkBox(DISPLAY_PARAMETERS_CHECK_BOX_NAME), true);
         //Add parameter index 0
         addParameter(0, "{{some_key}}", "value1");
         addParameter(1, "url", "http://host");
