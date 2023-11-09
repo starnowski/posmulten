4452c49c5dfa6dafad487311681b57ffe558cf74
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-09 20:15:35 +0100

    #295 - maximize window

--- openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
+++ openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
@@ -78,6 +78,9 @@ class PosmultenAppMockedSwingTest {
         window = new FrameFixture(tested);
         window.show(); // shows the frame to test
         tested.setLocation(0, 0);
+        if (isRunningOnVirtualScreen) {
+            window.maximize();
+        }
     }
 
     @Test
