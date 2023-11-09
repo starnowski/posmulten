822a85f1d832b8f5ad7c4206c22780fe157d5097
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-11-09 18:39:04 +0100

    #295 - Setting java xvfbRunningTests

--- openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
+++ openwebstart/src/test/java/com/github/starnowski/posmulten/openwebstart/PosmultenAppMockedSwingTest.java
@@ -50,8 +50,8 @@ class PosmultenAppMockedSwingTest {
     }
 
     private static boolean isRunningOnVirtualScreen() {
-        String display = System.getenv("XVFB_RUNNING_TESTS");
-        return display != null && display.trim().isEmpty();
+        String display = System.getProperty("xvfbRunningTests");
+        return display != null && !display.trim().isEmpty() && Boolean.getBoolean(display.trim());
     }
 
     private JTextComponentFixture findTextComponentFixtureByName(String name) {
