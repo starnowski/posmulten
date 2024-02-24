258647047825d4ebfc825f62917ab4023a8eeefc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-02-24 12:34:47 +0100

    #301 - Added usage of StepWise annotation in the tests

--- postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForeignKeyConstraintStatementProducerItTest.groovy
+++ postgresql-core/src/test/groovy/com/github/starnowski/posmulten/postgresql/core/ForeignKeyConstraintStatementProducerItTest.groovy
@@ -1,15 +1,18 @@
 package com.github.starnowski.posmulten.postgresql.core
 
 import com.github.starnowski.posmulten.postgresql.core.context.TableKey
+import com.github.starnowski.posmulten.postgresql.core.util.SqlUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
 import spock.lang.Specification
+import spock.lang.Stepwise
 import spock.lang.Unroll
 
 import static com.github.starnowski.posmulten.postgresql.test.utils.TestUtils.isConstraintExists
 import static org.junit.Assert.assertEquals
 
+@Stepwise
 @SpringBootTest(classes = [TestApplication.class])
 class ForeignKeyConstraintStatementProducerItTest extends Specification {
 
@@ -17,41 +20,66 @@ class ForeignKeyConstraintStatementProducerItTest extends Specification {
 
     @Autowired
     JdbcTemplate jdbcTemplate
+    @Autowired
+    SqlUtils sqlUtils
 
     def definition
-    def schema
-    def table
-    def constraintName
 
     @Unroll
     def "should return statement that adds '#testConstraintName' constraint to table (#testTable) and schema (#testSchema) with condition '#conditionStatement'"()
     {
         given:
-            schema = testSchema
-            table = testTable
-            constraintName = testConstraintName
-            assertEquals(false, isConstraintExists(jdbcTemplate, schema, table, constraintName))
+            System.out.println("FIRST FEATURE")
+            assertEquals(false, isConstraintExists(jdbcTemplate, testSchema, testTable, testConstraintName))
             def parameters = DefaultForeignKeyConstraintStatementParameters.builder()
-                    .withConstraintName(constraintName)
-                    .withTableName(table)
-                    .withTableSchema(schema)
+                    .withConstraintName(testConstraintName)
+                    .withTableName(testTable)
+                    .withTableSchema(testSchema)
                     .withReferenceTableKey(new TableKey(referenceTable, referenceSchema))
                     .withForeignKeyColumnMappings(foreignKeyColumnMappings).build()
+            definition = tested.produce(parameters)
 
         when:
-            definition = tested.produce(parameters)
             jdbcTemplate.execute(definition.getCreateScript())
 
         then:
-            isConstraintExists(jdbcTemplate, schema, table, constraintName)
+            isConstraintExists(jdbcTemplate, testSchema, testTable, testConstraintName)
+
+        and: "and all checking statement should pass"
+            sqlUtils.assertAllCheckingStatementsArePassing(definition)
 
         where:
             testConstraintName          |   testSchema                                      | testTable     |   referenceSchema                                     |   referenceTable  |   foreignKeyColumnMappings
             "f_key_users"               |   "non_public_schema_with_composite_key"          | "posts"       |   "non_public_schema_with_composite_key"              |   "users"         |   [tenant_id : "tenant_id", user_id: "id"]
+            "f_key_users_groups_to_users"               |   "non_public_schema_with_composite_key"          | "users_groups"       |   "non_public_schema_with_composite_key"              |   "users"         |   [tenant_id : "tenant_id", user_id: "id"]
     }
 
-    def cleanup() {
-        jdbcTemplate.execute(definition.getDropScript())
-        assertEquals(false, isConstraintExists(jdbcTemplate, schema, table, constraintName))
+    @Unroll
+    def "should drop constraint #testConstraintName for schema #testSchema and table #testTable based on correct drop statement"(){
+        given:
+            System.out.println("SECOND FEATURE")
+            assertEquals(true, isConstraintExists(jdbcTemplate, testSchema, testTable, testConstraintName))
+            def parameters = DefaultForeignKeyConstraintStatementParameters.builder()
+                    .withConstraintName(testConstraintName)
+                    .withTableName(testTable)
+                    .withTableSchema(testSchema)
+                    .withReferenceTableKey(new TableKey(referenceTable, referenceSchema))
+                    .withForeignKeyColumnMappings(foreignKeyColumnMappings).build()
+            definition = tested.produce(parameters)
+
+        when:
+            jdbcTemplate.execute(definition.getDropScript())
+
+        then:
+            !isConstraintExists(jdbcTemplate, testSchema, testTable, testConstraintName)
+
+        and: "and all checking statement should return zero as result"
+            sqlUtils.assertAllResultForCheckingStatementsAreEqualZero(definition)
+
+        where:
+            testConstraintName          |   testSchema                                      | testTable     |   referenceSchema                                     |   referenceTable  |   foreignKeyColumnMappings
+            "f_key_users"               |   "non_public_schema_with_composite_key"          | "posts"       |   "non_public_schema_with_composite_key"              |   "users"         |   [tenant_id : "tenant_id", user_id: "id"]
+            "f_key_users_groups_to_users"               |   "non_public_schema_with_composite_key"          | "users_groups"       |   "non_public_schema_with_composite_key"              |   "users"         |   [tenant_id : "tenant_id", user_id: "id"]
     }
+
 }
