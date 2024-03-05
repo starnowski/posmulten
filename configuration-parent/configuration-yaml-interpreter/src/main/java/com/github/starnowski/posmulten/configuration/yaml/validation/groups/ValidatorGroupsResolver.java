97daea3221e13100f4c80720e9e01f04c6c561ae
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-03-05 09:24:07 +0100

    #301 - Added validation group resolvers (not yet finished!) [skip ci]

--- configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/validation/groups/ValidatorGroupsResolver.java
+++ configuration-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/validation/groups/ValidatorGroupsResolver.java
@@ -2,9 +2,24 @@ package com.github.starnowski.posmulten.configuration.yaml.validation.groups;
 
 import com.github.starnowski.posmulten.configuration.yaml.model.SharedSchemaContextConfiguration;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ValidatorGroupsResolver {
+
+    private final List<ValidatorGroupResolver> validatorGroupResolvers;
+
+    public ValidatorGroupsResolver() {
+        this(Arrays.asList(new NameForFunctionThatChecksIfRecordExistsInTableNotBlankGroupResolver()));
+    }
+
+    public ValidatorGroupsResolver(List<ValidatorGroupResolver> validatorGroupResolvers) {
+        this.validatorGroupResolvers = validatorGroupResolvers;
+    }
+
+    List<ValidatorGroupResolver> getValidatorGroupResolvers() {
+        return validatorGroupResolvers;
+    }
 
     List<Class> resolveForSharedSchemaContextConfiguration(SharedSchemaContextConfiguration sharedSchemaContextConfiguration) {
         //ValidatorGroupResolver
--- configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/validation/groups/ValidatorGroupsResolverTest.groovy
+++ configuration-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/validation/groups/ValidatorGroupsResolverTest.groovy
@@ -2,5 +2,19 @@ package com.github.starnowski.posmulten.configuration.yaml.validation.groups
 
 import spock.lang.Specification
 
+import java.util.stream.Collectors
+
 class ValidatorGroupsResolverTest extends Specification {
+
+    def"should have expected list of validator groups resolvers"()
+    {
+        given:
+            def tested = new ValidatorGroupsResolver()
+
+        when:
+            def results = tested.getValidatorGroupResolvers()
+
+        then:
+            results.stream().map {it -> it.getClass()}.collect(Collectors.toList()) == [NameForFunctionThatChecksIfRecordExistsInTableNotBlankGroupResolver.class]
+    }
 }
