21dc5110e8f87c042fbff76c4fcb90ea0502db28
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-08-11 14:14:02 +0200

    #294 - Code refactoring

---
+++ configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-interpreter-core/src/main/java/com/github/starnowski/posmulten/configuration/yaml/core/mappers/AbstractRLSPolicyMapper.java
@@ -0,0 +1,58 @@
+/**
+ * Posmulten library is an open-source project for the generation
+ * of SQL DDL statements that make it easy for implementation of
+ * Shared Schema Multi-tenancy strategy via the Row Security
+ * Policies in the Postgres database.
+ * <p>
+ * Copyright (C) 2020  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package com.github.starnowski.posmulten.configuration.yaml.core.mappers;
+
+import com.github.starnowski.posmulten.configuration.yaml.core.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.core.model.AbstractPrimaryKeyDefinition;
+import com.github.starnowski.posmulten.configuration.yaml.core.model.AbstractRLSPolicy;
+import com.github.starnowski.posmulten.configuration.yaml.core.model.AbstractStringWrapperWithNotBlankValue;
+
+public abstract class AbstractRLSPolicyMapper<S extends AbstractStringWrapperWithNotBlankValue, PK extends AbstractPrimaryKeyDefinition, T extends AbstractRLSPolicy<T, S, PK>> extends AbstractConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.RLSPolicy, T> {
+
+    @Override
+    public T map(com.github.starnowski.posmulten.configuration.core.model.RLSPolicy input) {
+        return input == null ? null : createNewInstanceOfOutput()
+                .setName(input.getName())
+                .setCreateTenantColumnForTable(input.getCreateTenantColumnForTable())
+                .setPrimaryKeyDefinition(getPrimaryKeyDefinitionMapper().map(input.getPrimaryKeyDefinition()))
+                .setSkipAddingOfTenantColumnDefaultValue(input.getSkipAddingOfTenantColumnDefaultValue())
+                .setTenantColumn(input.getTenantColumn() == null ? null : createNewStringWrapperWithNotBlankValue(input.getTenantColumn()))
+                .setValidTenantValueConstraintName(input.getValidTenantValueConstraintName() == null ? null : createNewStringWrapperWithNotBlankValue(input.getValidTenantValueConstraintName()));
+    }
+
+    @Override
+    public com.github.starnowski.posmulten.configuration.core.model.RLSPolicy unmap(T output) {
+        return output == null ? null : new com.github.starnowski.posmulten.configuration.core.model.RLSPolicy()
+                .setName(output.getName())
+                .setCreateTenantColumnForTable(output.getCreateTenantColumnForTable())
+                .setPrimaryKeyDefinition(getPrimaryKeyDefinitionMapper().unmap(output.getPrimaryKeyDefinition()))
+                .setSkipAddingOfTenantColumnDefaultValue(output.getSkipAddingOfTenantColumnDefaultValue())
+                .setTenantColumn(output.getTenantColumn() == null ? null : output.getTenantColumn().getValue())
+                .setValidTenantValueConstraintName(output.getValidTenantValueConstraintName() == null ? null : output.getValidTenantValueConstraintName().getValue());
+    }
+
+    abstract protected S createNewStringWrapperWithNotBlankValue(String value);
+
+    abstract protected IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.PrimaryKeyDefinition, PK> getPrimaryKeyDefinitionMapper();
+}
--- configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapper.java
+++ configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-interpreter/src/main/java/com/github/starnowski/posmulten/configuration/yaml/mappers/RLSPolicyMapper.java
@@ -1,55 +1,50 @@
 /**
- *     Posmulten library is an open-source project for the generation
- *     of SQL DDL statements that make it easy for implementation of
- *     Shared Schema Multi-tenancy strategy via the Row Security
- *     Policies in the Postgres database.
- *
- *     Copyright (C) 2020  Szymon Tarnowski
- *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
+ * Posmulten library is an open-source project for the generation
+ * of SQL DDL statements that make it easy for implementation of
+ * Shared Schema Multi-tenancy strategy via the Row Security
+ * Policies in the Postgres database.
+ * <p>
+ * Copyright (C) 2020  Szymon Tarnowski
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 package com.github.starnowski.posmulten.configuration.yaml.mappers;
 
+import com.github.starnowski.posmulten.configuration.core.model.PrimaryKeyDefinition;
 import com.github.starnowski.posmulten.configuration.yaml.core.IConfigurationMapper;
+import com.github.starnowski.posmulten.configuration.yaml.core.mappers.AbstractRLSPolicyMapper;
 import com.github.starnowski.posmulten.configuration.yaml.model.RLSPolicy;
 import com.github.starnowski.posmulten.configuration.yaml.model.StringWrapperWithNotBlankValue;
 
-public class RLSPolicyMapper implements IConfigurationMapper<com.github.starnowski.posmulten.configuration.core.model.RLSPolicy, RLSPolicy> {
+public class RLSPolicyMapper extends AbstractRLSPolicyMapper<StringWrapperWithNotBlankValue, com.github.starnowski.posmulten.configuration.yaml.model.PrimaryKeyDefinition, RLSPolicy> {
 
     private final PrimaryKeyDefinitionMapper primaryKeyDefinitionMapper = new PrimaryKeyDefinitionMapper();
 
     @Override
-    public RLSPolicy map(com.github.starnowski.posmulten.configuration.core.model.RLSPolicy input) {
-        return input == null ? null : new RLSPolicy()
-                .setName(input.getName())
-                .setCreateTenantColumnForTable(input.getCreateTenantColumnForTable())
-                .setPrimaryKeyDefinition(primaryKeyDefinitionMapper.map(input.getPrimaryKeyDefinition()))
-                .setSkipAddingOfTenantColumnDefaultValue(input.getSkipAddingOfTenantColumnDefaultValue())
-                .setTenantColumn(input.getTenantColumn() == null ? null : new StringWrapperWithNotBlankValue(input.getTenantColumn()))
-                .setValidTenantValueConstraintName(input.getValidTenantValueConstraintName() == null ? null : new StringWrapperWithNotBlankValue(input.getValidTenantValueConstraintName()));
+    protected StringWrapperWithNotBlankValue createNewStringWrapperWithNotBlankValue(String value) {
+        return new StringWrapperWithNotBlankValue(value);
     }
 
     @Override
-    public com.github.starnowski.posmulten.configuration.core.model.RLSPolicy unmap(RLSPolicy output) {
-        return output == null ? null : new com.github.starnowski.posmulten.configuration.core.model.RLSPolicy()
-                .setName(output.getName())
-                .setCreateTenantColumnForTable(output.getCreateTenantColumnForTable())
-                .setPrimaryKeyDefinition(primaryKeyDefinitionMapper.unmap(output.getPrimaryKeyDefinition()))
-                .setSkipAddingOfTenantColumnDefaultValue(output.getSkipAddingOfTenantColumnDefaultValue())
-                .setTenantColumn(output.getTenantColumn() == null ? null : output.getTenantColumn().getValue())
-                .setValidTenantValueConstraintName(output.getValidTenantValueConstraintName() == null ? null : output.getValidTenantValueConstraintName().getValue());
+    protected IConfigurationMapper<PrimaryKeyDefinition, com.github.starnowski.posmulten.configuration.yaml.model.PrimaryKeyDefinition> getPrimaryKeyDefinitionMapper() {
+        return primaryKeyDefinitionMapper;
+    }
+
+    @Override
+    protected RLSPolicy createNewInstanceOfOutput() {
+        return new RLSPolicy();
     }
 }
