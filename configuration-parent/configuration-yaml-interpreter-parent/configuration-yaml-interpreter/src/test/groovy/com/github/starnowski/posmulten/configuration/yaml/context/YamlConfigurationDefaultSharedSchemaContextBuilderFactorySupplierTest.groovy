2a6008a356bb2fca73b781e5919ffab355aac0a1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-08-11 20:34:54 +0200

    #294 - Added tests classes [skip ci]

--- configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest.groovy
+++ configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/context/YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest.groovy
@@ -1,42 +1,18 @@
 package com.github.starnowski.posmulten.configuration.yaml.context
 
-import spock.lang.Specification
+import com.github.starnowski.posmulten.configuration.common.yaml.context.AbstractYamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest
 
-import static java.util.Arrays.asList
-
-class YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest extends Specification {
+class YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest extends AbstractYamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplierTest<YamlConfigurationDefaultSharedSchemaContextBuilderFactory, YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier> {
 
     def tested = new YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier()
 
-    def "should return priority with value zero"()
-    {
-        expect:
-            tested.getPriority() == 0
+    @Override
+    protected YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier getYamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier() {
+        new YamlConfigurationDefaultSharedSchemaContextBuilderFactorySupplier()
     }
 
-    def "should return extensions related to YAML language"()
-    {
-        given:
-            def expectedExtensions = asList("yaml", "yml")
-
-        when:
-            def results = tested.getSupportedFileExtensions()
-
-        then:
-            results
-            results.size() == expectedExtensions.size()
-            results.containsAll(expectedExtensions)
-    }
-
-    def "should return factory supplier"()
-    {
-        when:
-            def supplier = tested.getFactorySupplier()
-
-        then:
-            supplier
-
-        and: "supplier creates object of correct type"
-            YamlConfigurationDefaultSharedSchemaContextBuilderFactory.class.equals(supplier.get().getClass())
+    @Override
+    protected Class<YamlConfigurationDefaultSharedSchemaContextBuilderFactory> getYamlConfigurationDefaultSharedSchemaContextBuilderFactoryClass() {
+        YamlConfigurationDefaultSharedSchemaContextBuilderFactory.class
     }
 }
