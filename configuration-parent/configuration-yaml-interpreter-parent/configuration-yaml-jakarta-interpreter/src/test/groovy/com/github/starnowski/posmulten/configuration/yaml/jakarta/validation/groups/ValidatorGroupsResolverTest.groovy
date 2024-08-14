395bead64d6edaa44a24e2b386dfbacc6039b5ca
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-08-14 18:11:33 +0200

    #294 - Added group tests

---
+++ configuration-parent/configuration-yaml-interpreter-parent/configuration-yaml-jakarta-interpreter/src/test/groovy/com/github/starnowski/posmulten/configuration/yaml/jakarta/validation/groups/ValidatorGroupsResolverTest.groovy
@@ -0,0 +1,43 @@
+package com.github.starnowski.posmulten.configuration.yaml.jakarta.validation.groups
+
+import com.github.starnowski.posmulten.configuration.yaml.jakarta.model.SharedSchemaContextConfiguration
+import spock.lang.Specification
+
+import java.util.stream.Collectors
+
+class ValidatorGroupsResolverTest extends Specification {
+
+    def"should have expected list of validator groups resolvers"()
+    {
+        given:
+            def tested = new ValidatorGroupsResolver()
+
+        when:
+            def results = tested.getValidatorGroupResolvers()
+
+        then:
+            results.stream().map {it -> it.getClass()}.collect(Collectors.toList()) == [NameForFunctionThatChecksIfRecordExistsInTableNotBlankGroupResolver.class]
+    }
+
+    def"should return expected list of classed based on result from combined list of validator groups resolvers"()
+    {
+        given:
+            def vgr1 = Mock(ValidatorGroupResolver)
+            def vgr2 = Mock(ValidatorGroupResolver)
+            def tested = new ValidatorGroupsResolver([vgr1, vgr2])
+            def sharedSchemaContextConfiguration = new SharedSchemaContextConfiguration()
+            def validatorGroupResolverContext = Mock(ValidatorGroupResolver.ValidatorGroupResolverContext)
+
+        when:
+            def results = tested.resolveForSharedSchemaContextConfiguration(sharedSchemaContextConfiguration, validatorGroupResolverContext)
+
+        then:
+            1 * vgr1.resolveForSharedSchemaContextConfiguration(sharedSchemaContextConfiguration, validatorGroupResolverContext) >> ValidationClass1
+            1 * vgr2.resolveForSharedSchemaContextConfiguration(sharedSchemaContextConfiguration, validatorGroupResolverContext) >> ValidationClass2
+            results == [ValidationClass1, ValidationClass2]
+    }
+
+    class ValidationClass1 {}
+
+    class ValidationClass2 {}
+}
